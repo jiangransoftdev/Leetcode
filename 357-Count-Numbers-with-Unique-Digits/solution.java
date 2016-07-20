10f6d20a2fca917c6ebcac39b028466d53a3ae4b
jiangran910126 <jiangran0126@gmail.com>
2016-07-20 02:11:02 -0500

    [357. Count Numbers with Unique Digits][Accepted]Dynamic programming

---
+++ 357-Count-Numbers-with-Unique-Digits/solution.java
@@ -0,0 +1,13 @@
+public class Solution {
+    public int countNumbersWithUniqueDigits(int n) {
+        if(n==0) return 1;
+        if(n==1) return 10;
+        int factor=9,res=10;
+        for(int i=2;i<=n;i++){
+            factor*=9-i+2;
+            res=res+factor;
+        }
+            
+        return res;
+    }
+}
