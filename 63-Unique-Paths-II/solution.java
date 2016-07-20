98badf72deb52cfbadd7443058ae2945b34bb3b3
jiangran910126 <jiangran0126@gmail.com>
2016-07-20 02:14:01 -0500

    [63. Unique Paths II][Accepted]Dynamic Programming

---
+++ 63-Unique-Paths-II/solution.java
@@ -0,0 +1,19 @@
+public class Solution {
+    public int uniquePathsWithObstacles(int[][] obstacleGrid) {
+        int m=obstacleGrid.length;
+        if(m==0) return 0;
+        int n=obstacleGrid[0].length;
+        int[][] dp=new int[m+1][n+1];
+        if(obstacleGrid[0][0]==1) return 0;
+        dp[1][1]=1;
+        for(int i=1;i<=m;i++){
+            for(int j=1;j<=n;j++){
+                if(i==1&&j==1) continue;
+                if(obstacleGrid[i-1][j-1]==1)
+                    dp[i][j]=0;
+                else dp[i][j]=dp[i-1][j]+dp[i][j-1];
+            }
+        }
+        return dp[m][n];
+    }
+}
