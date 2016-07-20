6b8acb10e3cbe1dc0ef6caa29adf1182a7b1ed17
jiangran910126 <jiangran0126@gmail.com>
2016-07-20 14:23:59 -0500

    [343. Integer Break][Accepted]Dynamic Programming

---
+++ 343-Integer-Break/solution.java
@@ -0,0 +1,19 @@
+public class Solution {
+    public int integerBreak(int n) {
+        if(n==2) return 1;
+        if(n==3) return 2;
+        if(n==4) return 4;
+        if(n==5) return 6;
+        if(n==6) return 9;
+        int[] dp=new int[n+1];
+        dp[1]=1;
+        dp[2]=1;
+        dp[3]=2;
+        dp[4]=4;
+        dp[5]=6;
+        dp[6]=9;
+        for(int i=7;i<=n;i++)
+            dp[i]=dp[i-3]*3;
+        return dp[n];
+    }
+}
