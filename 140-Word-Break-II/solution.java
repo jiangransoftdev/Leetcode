a02ee505f1411118b08ea8b413659d3cbf9f433f
jiangran910126 <jiangran0126@gmail.com>
2016-07-20 01:59:28 -0500

    [140. Word Break II][Accepted]Memorized Searching+Backtracking

---
+++ 140-Word-Break-II/solution.java
@@ -0,0 +1,24 @@
+public class Solution {
+    Map<String,List<String>> map=new HashMap<>();
+    public List<String> wordBreak(String s, Set<String> wordDict) {
+        List<String> res=new ArrayList<>();
+        if(s.length()==0) return res;
+        if(map.containsKey(s)) return map.get(s);
+        for(int i=1;i<=s.length();i++){
+            String sub=s.substring(0,i);
+            if(wordDict.contains(sub)){
+                List<String> list=wordBreak(s.substring(i),wordDict);
+                if(list.size()==0){
+                    if(i==s.length())
+                        res.add(sub);
+                }
+                else{
+                    for(String str:list)
+                        res.add(sub+" "+str);
+                }
+            }
+        }
+        map.put(s,res);
+        return res;
+    }
+}
