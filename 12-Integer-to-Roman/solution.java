afa173e08d9eebb86c03a53b4edd4691b84e635e
jiangran910126 <jiangran0126@gmail.com>
2016-07-21 01:48:12 -0500

    [12. Integer to Roman][Accepted]

---
+++ 12-Integer-to-Roman/solution.java
@@ -0,0 +1,15 @@
+public class Solution {
+    public String intToRoman(int num) {
+        if(num==0) return "";
+        int[] value={1000,900,500,400,100,90,50,40,10,9,5,4,1};
+        String[] str={"M","CM","D","CD","C","XC","L","XL","X","IX","V","IV","I"};
+        StringBuilder sb=new StringBuilder();
+        for(int i=0;i<value.length;i++){
+        	while(num>=value[i]){
+        		num-=value[i];
+        		sb.append(str[i]);
+        	}
+        }
+        return sb.toString();
+    }
+}
