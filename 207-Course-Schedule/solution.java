37606b88d37169a13babf41c85f3b45d4d0df299
jiangran910126 <jiangran0126@gmail.com>
2016-07-21 15:35:21 -0500

    [207. Course Schedule][Accepted]

--- 207-Course-Schedule/solution.java
+++ 207-Course-Schedule/solution.java
@@ -1,26 +1,25 @@
 public class Solution {
     public boolean canFinish(int numCourses, int[][] prerequisites) {
-        List[] graph=new ArrayList[numCourses];
+        List[] list=new ArrayList[numCourses];
         for(int i=0;i<numCourses;i++)
-        	graph[i]=new ArrayList();
-        for(int i=0;i<prerequisites.length;i++)
-        	graph[prerequisites[i][1]].add(prerequisites[i][0]);
+            list[i]=new ArrayList();
+        for(int i=0;i<prerequisites.length;i++){
+            list[prerequisites[i][0]].add(prerequisites[i][1]);
+        }
         boolean[] visit=new boolean[numCourses];
-        for(int i=0;i<numCourses;i++)
-        	if(!dfs(graph,visit,i))
-        		return false;
+        for(int i=0;i<numCourses;i++){
+            if(!helper(list,i,visit))
+                return false;
+        }
         return true;
     }
-public boolean dfs(List[] graph,boolean[] visit,int i){
-	if(visit[i])
-		return false;
-	else
-		visit[i]=true;
-	for(int j=0;j<graph[i].size();j++){
-		if(!dfs(graph,visit,(int)graph[i].get(j)))
-			return false;
-	}
-	visit[i]=false;
-	return true;
-}
+    public boolean helper(List[] list,int course,boolean[] visit){
+        if(visit[course]) return false;
+        visit[course]=true;
+        for(int j=0;j<list[course].size();j++){
+            if(!helper(list,(int)list[course].get(j),visit)) return false;
+        }
+        visit[course]=false;
+        return true;
+    }
 }
