4eb6d844ce3ac384c30260701615811d3cd3787a
jiangran910126 <jiangran0126@gmail.com>
2016-07-22 02:07:53 -0500

    [204. Count Primes][Accepted]

---
+++ 204-Count-Primes/solution.java
@@ -0,0 +1,18 @@
+public class Solution {
+    public int countPrimes(int n) {
+        if(n<=2) return 0;
+        int count=1;
+        boolean[] isNotPrime=new boolean[n+1];
+        for(int i=3;i*i<=n;i+=2){
+            if(isNotPrime[i]) continue;
+            for(int j=i*i;j<=n;j+=2*i){
+                isNotPrime[j]=true;
+            }
+        }
+        for(int i=3;i<n;i+=2){
+            if(!isNotPrime[i])
+                count++;
+        }
+        return count;
+    }
+}
