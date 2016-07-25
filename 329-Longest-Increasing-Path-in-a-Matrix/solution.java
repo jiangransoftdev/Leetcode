4d193f74682d277559bcd20e9101444453a070a8
jiangran910126 <jiangran0126@gmail.com>
2016-07-25 14:36:55 -0500

    [329. Longest Increasing Path in a Matrix][Accepted]

---
+++ 329-Longest-Increasing-Path-in-a-Matrix/solution.java
@@ -0,0 +1,30 @@
+public class Solution {
+    Map<Integer,Integer> map=new HashMap<>();
+    public int longestIncreasingPath(int[][] matrix) {
+        int m=matrix.length;
+        if(m==0) return 0;
+        int n=matrix[0].length;
+        boolean[][] visit=new boolean[m][n];
+        int res=0;
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+                res=Math.max(res,explore(matrix,i,j,visit));
+            }
+        }
+        return res;
+    }
+    public int explore(int[][] matrix,int i,int j,boolean[][] visit){
+        if(visit[i][j]) return 0;
+        if(map.containsKey(i*matrix[0].length+j)) return map.get(i*matrix[0].length+j);
+        visit[i][j]=true;
+        int count=0;
+        if(i>0&&matrix[i-1][j]>matrix[i][j]) count=Math.max(count,explore(matrix,i-1,j,visit));
+        if(i<matrix.length-1&&matrix[i+1][j]>matrix[i][j]) count=Math.max(count,explore(matrix,i+1,j,visit));
+        if(j>0&&matrix[i][j-1]>matrix[i][j]) count=Math.max(count,explore(matrix,i,j-1,visit));
+        if(j<matrix[0].length-1&&matrix[i][j+1]>matrix[i][j]) count=Math.max(count,explore(matrix,i,j+1,visit));
+        count++;
+        visit[i][j]=false;
+        map.put(i*matrix[0].length+j,count);
+        return count;
+    }
+}
