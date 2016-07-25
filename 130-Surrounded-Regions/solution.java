2d35427bb5e1e8f08a823f0dcb61c52fca2416d6
jiangran910126 <jiangran0126@gmail.com>
2016-07-25 01:23:00 -0500

    [130. Surrounded Regions][Accepted]

--- 130-Surrounded-Regions/solution.java
+++ 130-Surrounded-Regions/solution.java
@@ -3,32 +3,31 @@ public class Solution {
         int m=board.length;
         if(m==0) return;
         int n=board[0].length;
-        Stack<Integer> s=new Stack<>();
         for(int i=0;i<m;i++){
             if(board[i][0]=='O')
-                s.push(i*n);
+                explore(board,i,0);
             if(board[i][n-1]=='O')
-                s.push(i*n+n-1);
+                explore(board,i,n-1);
         }
         for(int i=0;i<n;i++){
             if(board[0][i]=='O')
-                s.push(i);
+                explore(board,0,i);
             if(board[m-1][i]=='O')
-                s.push((m-1)*n+i);
-        }
-        while(!s.isEmpty()){
-            int index=s.pop(),x=index/n, y=index%n;
-            board[x][y]='Y';
-            if(x>0&&board[x-1][y]=='O') s.push(index-n);
-            if(x<m-1&&board[x+1][y]=='O') s.push(index+n);
-            if(y>0&&board[x][y-1]=='O') s.push(index-1);
-            if(y<n-1&&board[x][y+1]=='O') s.push(index+1);
+                explore(board,m-1,i);
         }
         for(int i=0;i<m;i++){
             for(int j=0;j<n;j++){
+                 if(board[i][j]=='O') board[i][j]='X';
                 if(board[i][j]=='Y') board[i][j]='O';
-                else if(board[i][j]=='O') board[i][j]='X';
             }
         }
+    }
+    public void explore(char[][] board,int i,int j){
+        if(board[i][j]=='O')
+            board[i][j]='Y';
+        if(i>1&&board[i-1][j]=='O') explore(board,i-1,j);
+        if(i<board.length-2&&board[i+1][j]=='O') explore(board,i+1,j);
+        if(j>1&&board[i][j-1]=='O') explore(board,i,j-1);
+        if(j<board[0].length-2&&board[i][j+1]=='O') explore(board,i,j+1);
     }
 }
