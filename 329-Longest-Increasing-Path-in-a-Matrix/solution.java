997c1be55880df29a4ee830e959f598df28e4754
jiangran910126 <jiangran0126@gmail.com>
2016-07-25 14:52:51 -0500

    [329. Longest Increasing Path in a Matrix][Accepted]

--- 329-Longest-Increasing-Path-in-a-Matrix/solution.java
+++ 329-Longest-Increasing-Path-in-a-Matrix/solution.java
@@ -4,24 +4,24 @@ public class Solution {
         int m=matrix.length;
         if(m==0) return 0;
         int n=matrix[0].length;
-        boolean[][] visit=new boolean[m][n];
+        // boolean[][] visit=new boolean[m][n];
         int res=0;
         for(int i=0;i<m;i++){
             for(int j=0;j<n;j++){
-                res=Math.max(res,explore(matrix,i,j,visit));
+                res=Math.max(res,explore(matrix,i,j));
             }
         }
         return res;
     }
-    public int explore(int[][] matrix,int i,int j,boolean[][] visit){
+    public int explore(int[][] matrix,int i,int j){
         if(map.containsKey(i*matrix[0].length+j)) return map.get(i*matrix[0].length+j);
-                if(visit[i][j]) return 0;
-        visit[i][j]=true;
+        // if(visit[i][j]) return 0;
+        // visit[i][j]=true;
         int count=0;
-        if(i>0&&matrix[i-1][j]>matrix[i][j]) count=Math.max(count,explore(matrix,i-1,j,visit));
-        if(i<matrix.length-1&&matrix[i+1][j]>matrix[i][j]) count=Math.max(count,explore(matrix,i+1,j,visit));
-        if(j>0&&matrix[i][j-1]>matrix[i][j]) count=Math.max(count,explore(matrix,i,j-1,visit));
-        if(j<matrix[0].length-1&&matrix[i][j+1]>matrix[i][j]) count=Math.max(count,explore(matrix,i,j+1,visit));
+        if(i>0&&matrix[i-1][j]>matrix[i][j]) count=Math.max(count,explore(matrix,i-1,j));
+        if(i<matrix.length-1&&matrix[i+1][j]>matrix[i][j]) count=Math.max(count,explore(matrix,i+1,j));
+        if(j>0&&matrix[i][j-1]>matrix[i][j]) count=Math.max(count,explore(matrix,i,j-1));
+        if(j<matrix[0].length-1&&matrix[i][j+1]>matrix[i][j]) count=Math.max(count,explore(matrix,i,j+1));
         count++;
         // visit[i][j]=false;
         map.put(i*matrix[0].length+j,count);
