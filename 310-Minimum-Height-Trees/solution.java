87c80f7cf6525e2c89d98409ebb695f06dd24613
jiangran910126 <jiangran0126@gmail.com>
2016-07-26 15:57:19 -0500

    [310. Minimum Height Trees][Accepted]

---
+++ 310-Minimum-Height-Trees/solution.java
@@ -0,0 +1,45 @@
+public class Solution {
+    public List<Integer> findMinHeightTrees(int n, int[][] edges) {
+        List<Integer> res=new ArrayList<>();
+        if(edges.length==0) {
+            res.add(0);
+            return res;
+        }
+        List[] graph=new ArrayList[n];
+        boolean[] visit=new boolean[n];
+        int[] d=new int[n];
+        for(int i=0;i<n;i++)
+            graph[i]=new ArrayList();
+        for(int i=0;i<edges.length;i++){
+            graph[edges[i][0]].add(edges[i][1]);
+            graph[edges[i][1]].add(edges[i][0]);
+            d[edges[i][0]]++;
+            d[edges[i][1]]++;
+        }
+        Queue<Integer> q=new LinkedList<>();
+        for(int i=0;i<n;i++){
+            if(d[i]==1){
+                q.offer(i);
+                visit[i]=true;
+            }
+        }
+        while(!q.isEmpty()){
+            int size=q.size();
+            res.clear();
+            for(int i=0;i<size;i++){
+                int node=q.poll();
+                res.add(node);
+                for(int j=0;j<graph[node].size();j++){
+                    if(!visit[(int)graph[node].get(j)]){
+                        d[(int)graph[node].get(j)]--;
+                        if(d[(int)graph[node].get(j)]==1){
+                            q.offer((int)graph[node].get(j));
+                            visit[(int)graph[node].get(j)]=true;
+                        }
+                    }
+                }
+            }
+        }
+        return res;
+    }
+}
