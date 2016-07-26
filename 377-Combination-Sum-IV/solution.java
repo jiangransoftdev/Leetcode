09a3623630c6d6a8331f6aeee130a2160237b5a6
jiangran910126 <jiangran0126@gmail.com>
2016-07-26 16:15:11 -0500

    [377. Combination Sum IV][Accepted]

--- 377-Combination-Sum-IV/solution.java
+++ 377-Combination-Sum-IV/solution.java
@@ -1,14 +1,13 @@
 public class Solution {
     public int combinationSum4(int[] nums, int target) {
-        int[] comb = new int[target + 1];
-    comb[0] = 1;
-    for (int i = 1; i < comb.length; i++) {
-        for (int j = 0; j < nums.length; j++) {
-            if (i - nums[j] >= 0) {
-                comb[i] += comb[i - nums[j]];
-            }
-        }
-    }
-    return comb[target];
+       int[] dp=new int[target+1];
+       dp[0]=1;
+       for(int i=1;i<=target;i++){
+           for(int j=0;j<nums.length;j++){
+               if(i>=nums[j])
+                    dp[i]+=dp[i-nums[j]];
+           }
+       }
+       return dp[target];
     }
 }
