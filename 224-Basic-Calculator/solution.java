ab608a1d1cf9b8450c4514fa83b7d08160147bd3
jiangran910126 <jiangran0126@gmail.com>
2016-07-27 00:20:12 -0500

    [224. Basic Calculator][Accepted]

---
+++ 224-Basic-Calculator/solution.java
@@ -0,0 +1,35 @@
+public class Solution {
+    public int calculate(String s) {
+        if(s.length()==0) return 0;
+        int res=0;
+        int co=1;
+        int num=0;
+        Stack<Integer> sta=new Stack<>(); 
+        for(int i=0;i<s.length();i++){
+            if(s.charAt(i)>=48&&s.charAt(i)<=57){
+                num=s.charAt(i)-'0';
+                while(i+1<s.length()&&s.charAt(i+1)>=48&&s.charAt(i+1)<=57){
+                    num=num*10+s.charAt(i+1)-'0';
+                    i++;
+                }
+                res+=num*co;
+            }
+            else if(s.charAt(i)=='+'){
+                co=1;
+            }
+            else if(s.charAt(i)=='-'){
+                co=-1;
+            }
+            else if(s.charAt(i)=='('){
+                sta.push(res);
+                sta.push(co);
+                res=0;
+                co=1;
+            }
+            else if(s.charAt(i)==')'){
+                res=res*sta.pop()+sta.pop();
+            }
+        }
+        return res;
+    }
+}
