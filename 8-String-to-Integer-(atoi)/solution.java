5728b871337ff6a687c3f2e9d38f7879497c0777
jiangran910126 <jiangran0126@gmail.com>
2016-07-28 02:23:17 -0500

    [8. String to Integer (atoi)][Accepted]

---
+++ 8-String-to-Integer-(atoi)/solution.java
@@ -0,0 +1,31 @@
+public class Solution {
+    public int myAtoi(String str) {
+        long res=0;
+	int countm=0,countp=0;
+        for(int i=0;i<str.length();i++){
+        	if(!(str.charAt(i)>='0'&&str.charAt(i)<='9')&&str.charAt(i)!=' '&&str.charAt(i)!='-'&&str.charAt(i)!='+')
+        		break;
+        	else{
+        		if(str.charAt(i)==' ')
+        			continue;
+        		else{
+        			if(str.charAt(i)=='-')
+        			    countm++;
+        			else if(str.charAt(i)=='+')
+        			    countp++;
+        			else{
+        			    if(countm==1&&countp==1) return 0;
+        			    res=res*10+(str.charAt(i)-'0');
+        			    if(res>(long)Integer.MAX_VALUE)
+        			        return (countm==1)?Integer.MIN_VALUE:Integer.MAX_VALUE;
+        			}
+        			if(i+1<str.length()&&(!(str.charAt(i+1)>='0'&&str.charAt(i+1)<='9')||str.charAt(i+1)==' '))
+        			    break;
+        			if(countm>1||countp>1)
+            			return 0;
+        		}
+        	}
+        }
+    return (countm==1)?(int)res*-1:(int)res;
+    }
+}
