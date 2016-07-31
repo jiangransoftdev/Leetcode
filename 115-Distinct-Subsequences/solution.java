716e80d55f51b35b39c25c516bf065586d67e6ef
jiangran910126 <jiangran0126@gmail.com>
2016-07-31 17:10:03 -0500

    [115. Distinct Subsequences][Accepted]

--- 115-Distinct-Subsequences/solution.java
+++ 115-Distinct-Subsequences/solution.java
@@ -1,16 +1,14 @@
 public class Solution {
     public int numDistinct(String s, String t) {
         if(s==null&&t==null) return 0;
-        int[][] dp=new int[s.length()+1][t.length()+1];
-        for(int i=0;i<=s.length();i++)
-    	dp[i][0]=1;
-        for(int j=1;j<=t.length();j++){
-            for(int i=1;i<=s.length();i++){
+        int[] dp=new int[t.length()+1];
+    	dp[0]=1;
+        for(int i=1;i<=s.length();i++){
+            for(int j=t.length();j>=1;j--){
                 if(s.charAt(i-1)==t.charAt(j-1))
-                    dp[i][j]=dp[i-1][j]+dp[i-1][j-1];
-                else dp[i][j]=dp[i-1][j];
+                    dp[j]+=dp[j-1];
             }
         }
-        return dp[s.length()][t.length()];
+        return dp[t.length()];
     }
 }
