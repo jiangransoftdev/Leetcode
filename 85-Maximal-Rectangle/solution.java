6d4f651a84e71f79f8fb64872490880f7d7f71a1
jiangran910126 <jiangran0126@gmail.com>
2016-08-01 16:03:07 -0500

    [85. Maximal Rectangle][Accepted]

---
+++ 85-Maximal-Rectangle/solution.java
@@ -0,0 +1,37 @@
+public class Solution {
+    public int maximalRectangle(char[][] matrix) {
+        int m=matrix.length;
+        if(m==0) return 0;
+        int n=matrix[0].length;
+        int[] left=new int[n];
+        int[] right=new int[n];
+        int[] height=new int[n];
+        int max=0;
+        Arrays.fill(right,n);
+        for(int i=0;i<m;i++){
+            int curleft=0,curright=n;
+            for(int j=0;j<n;j++){
+                if(matrix[i][j]=='1'){
+                    height[j]++;
+                    left[j]=Math.max(curleft,left[j]);
+                }
+                else{
+                    height[j]=0;
+                    left[j]=0;
+                    curleft=j+1;
+                }
+            }
+            for(int j=n-1;j>=0;j--){
+                if(matrix[i][j]=='1')
+                    right[j]=Math.min(curright,right[j]);
+                else{
+                    right[j]=n;
+                    curright=j;
+                }
+            }
+            for(int j=0;j<n;j++)
+                max=Math.max(max,height[j]*(right[j]-left[j]));
+        }
+        return max;
+    }
+}
