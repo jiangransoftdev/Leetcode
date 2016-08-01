e8474955f956f9f6c8e89ad7731ef131b62d710f
jiangran910126 <jiangran0126@gmail.com>
2016-08-01 17:49:43 -0500

    [220. Contains Duplicate III][Accepted]

---
+++ 220-Contains-Duplicate-III/solution.java
@@ -0,0 +1,15 @@
+public class Solution {
+    public boolean containsNearbyAlmostDuplicate(int[] nums, int k, int t) {
+        if(nums.length==0) return false;
+        TreeSet<Long> set=new TreeSet<>();
+        for(int i=0;i<nums.length;i++){
+            Long floor=set.floor((long)nums[i]+t);
+            if(floor!=null&&floor>=(long)nums[i]-t)
+                return true;
+            set.add((long)nums[i]);
+            if(i>=k)
+                set.remove((long)nums[i-k]);
+        }
+        return false;
+    }
+}
