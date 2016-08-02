250811922f777f902e273f36e0323c454dda341b
jiangran910126 <jiangran0126@gmail.com>
2016-08-02 00:01:16 -0500

    [10. Regular Expression Matching][Accepted]

--- 10-Regular-Expression-Matching/solution.java
+++ 10-Regular-Expression-Matching/solution.java
@@ -1,32 +1,26 @@
 public class Solution {
     public boolean isMatch(String s, String p) {
-        if (s == null || p == null) {
-        return false;
-    }
-    boolean[][] dp = new boolean[s.length()+1][p.length()+1];
-    dp[0][0] = true;
-    for (int i = 0; i < p.length(); i++) {
-        if (p.charAt(i) == '*' && dp[0][i-1]) {
-            dp[0][i+1] = true;
-        }
-    }
-    for (int i = 0 ; i < s.length(); i++) {
-        for (int j = 0; j < p.length(); j++) {
-            if (p.charAt(j) == '.') {
-                dp[i+1][j+1] = dp[i][j];
-            }
-            if (p.charAt(j) == s.charAt(i)) {
-                dp[i+1][j+1] = dp[i][j];
-            }
-            if (p.charAt(j) == '*') {
-                if (p.charAt(j-1) != s.charAt(i) && p.charAt(j-1) != '.') {
-                    dp[i+1][j+1] = dp[i+1][j-1];
-                } else {
-                    dp[i+1][j+1] = (dp[i+1][j] || dp[i][j+1] || dp[i+1][j-1]);
+        if(s==null||p==null) return false;
+        boolean[][] dp=new boolean[s.length()+1][p.length()+1];
+        dp[0][0]=true;
+        for(int i=0;i<p.length();i++)
+            if(p.charAt(i)=='*'&&dp[0][i-1])
+                dp[0][i+1]=true;
+        for(int i=1;i<=s.length();i++){
+            for(int j=1;j<=p.length();j++){
+                if(s.charAt(i-1)==p.charAt(j-1))
+                    dp[i][j]=dp[i-1][j-1];
+                else if(p.charAt(j-1)=='.')
+                    dp[i][j]=dp[i-1][j-1];
+                else if(p.charAt(j-1)=='*'){
+                    if(p.charAt(j-2)!=s.charAt(i-1)&&p.charAt(j-2)!='.')
+                        dp[i][j]=dp[i][j-2];
+                    else{
+                        dp[i][j]=dp[i][j-1]||dp[i-1][j]||dp[i][j-2];
+                    }
                 }
             }
         }
-    }
-    return dp[s.length()][p.length()];
+        return dp[s.length()][p.length()];
     }
 }
