1e24a446b3491aaf8e0bca6894c1ce2aaec7fa89
jiangran910126 <jiangran0126@gmail.com>
2016-08-04 18:02:17 -0500

    [324. Wiggle Sort II][Accepted]

---
+++ 324-Wiggle-Sort-II/solution.java
@@ -0,0 +1,15 @@
+public class Solution {
+    public void wiggleSort(int[] nums) {
+        Arrays.sort(nums);
+        int[] tmp=new int[nums.length];
+        int mid=(nums.length+1)/2-1,index=0;
+        for(int i=0;i<=mid;i++){
+            tmp[index]=nums[mid-i];
+            if(index+1<nums.length)
+                tmp[index+1]=nums[nums.length-1-i];
+            index+=2;
+        }
+        for(int i=0;i<nums.length;i++) 
+            nums[i]=tmp[i];
+    }
+}
