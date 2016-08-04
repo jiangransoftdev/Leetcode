723cdac4d16d39e70f7c61ba4f46713bcc230255
jiangran910126 <jiangran0126@gmail.com>
2016-08-04 00:49:15 -0500

    [295. Find Median from Data Stream][Accepted]

---
+++ 295-Find-Median-from-Data-Stream/solution.java
@@ -0,0 +1,27 @@
+public class MedianFinder {
+    PriorityQueue[] q={new PriorityQueue(),new PriorityQueue(Collections.reverseOrder())};
+    boolean even=true;
+    // Adds a number into the data structure.
+    public void addNum(int num) {
+       if(even){
+           q[1].offer(num);
+           q[0].add(q[1].poll());
+       }
+       else{
+           q[0].offer(num);
+           q[1].add(q[0].poll());
+       }
+       even=!even;
+    }
+
+    // Returns the median of current data stream
+    public double findMedian() {
+        if(!even) return (int)q[0].peek();
+        else return ((int)q[0].peek()+(int)q[1].peek())/2.0;
+    }
+};
+
+// Your MedianFinder object will be instantiated and called as such:
+// MedianFinder mf = new MedianFinder();
+// mf.addNum(1);
+// mf.findMedian();
