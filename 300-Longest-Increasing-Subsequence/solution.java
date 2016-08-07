46482948bb0d593bee349989937067f3a8c39827
jiangran910126 <jiangran0126@gmail.com>
2016-08-07 15:54:00 -0500

    [300. Longest Increasing Subsequence][Accepted]

---
+++ 300-Longest-Increasing-Subsequence/solution.java
@@ -0,0 +1,16 @@
+public class Solution {
+    public int lengthOfLIS(int[] nums) {
+        if(nums.length==0) return 0;
+        int[] dp=new int[nums.length+1];
+        int len=0;
+        for(int i=0;i<nums.length;i++){
+            int index=Arrays.binarySearch(dp,0,len,nums[i]);
+            if(index<0)
+                index=-(index+1);
+            dp[index]=nums[i];
+            if(index==len)
+                len++;
+        }
+        return len;
+    }
+}
