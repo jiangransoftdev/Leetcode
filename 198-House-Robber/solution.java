0982db5bca0928a559f4225c4c9beba9d5ee4913
jiangran910126 <jiangran0126@gmail.com>
2016-08-06 23:34:55 -0500

    [198. House Robber][Accepted]

--- 198-House-Robber/solution.java
+++ 198-House-Robber/solution.java
@@ -3,9 +3,8 @@ public class Solution {
         if(nums.length==0) return 0;
         int total=0,rob=0,unrob=0;
         for(int i=0;i<nums.length;i++){
-            int tmp=unrob;
-            unrob=Math.max(unrob,rob);
-            rob=tmp+nums[i];
+            rob=unrob+nums[i];
+            unrob=total;
             total=Math.max(unrob,rob);
         }
         return total;
