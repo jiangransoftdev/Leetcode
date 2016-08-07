0ac045513b23d78e6482c42c533416095bb57f27
jiangran910126 <jiangran0126@gmail.com>
2016-08-06 23:40:55 -0500

    [342. Power of Four][Accepted]

---
+++ 342-Power-of-Four/solution.java
@@ -0,0 +1,7 @@
+public class Solution {
+    public boolean isPowerOfFour(int num) {
+        if(num<=0) return false;
+        if(num==1) return true;
+        return 1073741824%num==0&&(num%10==4||num%10==6);
+    }
+}
