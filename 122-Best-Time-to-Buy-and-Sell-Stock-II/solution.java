3b2a9bea7162a37591b8ca2650881538190712ac
jiangran910126 <jiangran0126@gmail.com>
2016-08-07 14:28:49 -0500

    [122. Best Time to Buy and Sell Stock II][Accepted]

---
+++ 122-Best-Time-to-Buy-and-Sell-Stock-II/solution.java
@@ -0,0 +1,11 @@
+public class Solution {
+    public int maxProfit(int[] prices) {
+        if(prices.length==0) return 0;
+        int profit=0;
+        for(int i=1;i<prices.length;i++){
+            if(prices[i]-prices[i-1]>0)
+                profit+=prices[i]-prices[i-1];
+        }
+        return profit;
+    }
+}
