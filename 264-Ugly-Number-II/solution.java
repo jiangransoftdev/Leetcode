4a6b84192fd3647783f336e344f4579b8a60554f
jiangran910126 <jiangran0126@gmail.com>
2016-08-08 01:03:26 -0500

    [264. Ugly Number II][Accepted]

--- 264-Ugly-Number-II/solution.java
+++ 264-Ugly-Number-II/solution.java
@@ -1,13 +1,13 @@
 public class Solution {
     public int nthUglyNumber(int n) {
-        int p2=0,p3=0,p5=0;
-        int[] res=new int[n+1];
+        int f2=0,f3=0,f5=0;
+        int[] res=new int[n];
         res[0]=1;
         for(int i=1;i<n;i++){
-            res[i]=Math.min(res[p2]*2,Math.min(res[p3]*3,res[p5]*5));
-            if(res[i]==res[p2]*2) p2++;
-            if(res[i]==res[p3]*3) p3++;
-            if(res[i]==res[p5]*5) p5++;
+            res[i]=Math.min(res[f2]*2,Math.min(res[f3]*3,res[f5]*5));
+            if(res[i]==res[f2]*2) f2++;
+            if(res[i]==res[f3]*3) f3++;
+            if(res[i]==res[f5]*5) f5++;
         }
         return res[n-1];
     }
