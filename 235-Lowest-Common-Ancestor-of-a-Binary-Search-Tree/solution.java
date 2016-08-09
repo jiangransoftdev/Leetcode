fb1f922e18f9d4f241918c3f4b4aa5560df82f67
jiangran910126 <jiangran0126@gmail.com>
2016-08-08 22:10:55 -0500

    [235. Lowest Common Ancestor of a Binary Search Tree][Accepted]

--- 235-Lowest-Common-Ancestor-of-a-Binary-Search-Tree/solution.java
+++ 235-Lowest-Common-Ancestor-of-a-Binary-Search-Tree/solution.java
@@ -10,10 +10,11 @@
 public class Solution {
     public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
         if(root==null) return null;
-        if(root==p||root==q) return root;
-        TreeNode left=lowestCommonAncestor(root.left,p,q);
-        TreeNode right=lowestCommonAncestor(root.right,p,q);
-        if(left!=null&&right!=null) return root;
-        else return left==null?right:left;
+        if(root.val>p.val&&root.val>q.val)
+            root=lowestCommonAncestor(root.left,p,q);
+        else if(root.val<p.val&&root.val<q.val)
+            root=lowestCommonAncestor(root.right,p,q);
+        else return root;
+        return root;
     }
 }
