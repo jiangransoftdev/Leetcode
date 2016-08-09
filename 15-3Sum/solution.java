514f97f1b19ae24fc382bee5ad4458c4ef1463e9
jiangran910126 <jiangran0126@gmail.com>
2016-08-09 00:23:13 -0500

    [15. 3Sum][Accepted]

--- 15-3Sum/solution.java
+++ 15-3Sum/solution.java
@@ -6,13 +6,9 @@ public class Solution {
             if(i>0&&nums[i]==nums[i-1]) continue;
             int left=i+1,right=nums.length-1;
             while(left<right){
-                List<Integer> solu=new ArrayList<>();
                 int sum=nums[i]+nums[left]+nums[right];
                 if(sum==0){
-                    solu.add(nums[i]);
-                    solu.add(nums[left]);
-                    solu.add(nums[right]);
-                    res.add(solu);
+                    res.add(Arrays.asList(nums[i],nums[left],nums[right]));
                     while(left<right&&nums[left]==nums[left+1]) left++;
                     while(left<right&&nums[right]==nums[right-1]) right--;
                     left++;
