0ea434bb4a81b645173db12a6c1c809bbc048364
jiangran910126 <jiangran0126@gmail.com>
2016-08-09 15:53:09 -0500

    [211. Add and Search Word - Data structure design][Accepted]

--- 211-Add-and-Search-Word---Data-structure-design/solution.java
+++ 211-Add-and-Search-Word---Data-structure-design/solution.java
@@ -2,26 +2,23 @@ class TrieNode{
     TrieNode[] child;
     boolean isEnd;
     public TrieNode(){
-        this.child=new TrieNode[26];
+        child=new TrieNode[26];
         isEnd=false;
     }
 }
 public class WordDictionary {
-    TrieNode root;
-    boolean res;
+    private TrieNode root;
     public WordDictionary(){
         root=new TrieNode();
-        res=false;
     }
     // Adds a word into the data structure.
     public void addWord(String word) {
         TrieNode p=root;
         for(int i=0;i<word.length();i++){
-            int k=word.charAt(i)-'a';
-            if(p.child[k]==null){
-                p.child[k]=new TrieNode();
-            }
-            p=p.child[k];
+            int index=word.charAt(i)-'a';
+            if(p.child[index]==null)
+                p.child[index]=new TrieNode();
+            p=p.child[index];
         }
         p.isEnd=true;
     }
@@ -30,27 +27,21 @@ public class WordDictionary {
     // contain the dot character '.' to represent any one letter.
     public boolean search(String word) {
         TrieNode p=root;
-        return searchNode(word,0,p);
+        return helper(word,0,p);
     }
-    public boolean searchNode(String word,int index,TrieNode p){
-        if(index==word.length()){
-            if(p!=null)
-                if(p.isEnd) return true;
-            return false;
-        }
+    public boolean helper(String word,int index,TrieNode p){
+        boolean res=false;
         if(p==null) return false;
-        if(word.charAt(index)=='.'){
-            for(int i=0;i<26;i++){
-                res=searchNode(word,index+1,p.child[i]);
-                if(res) return res;
-            }
-        }
-        else{
-            int k=word.charAt(index)-'a';
-            if(p.child[k]!=null)
-                res=searchNode(word,index+1,p.child[k]);
+        if(index==word.length()){
+            if(p.isEnd) return true;
             else return false;
         }
+        if(word.charAt(index)=='.'){
+            for(int i=0;i<26;i++)
+                res|=helper(word,index+1,p.child[i]);
+        }
+        else
+            res|=helper(word,index+1,p.child[word.charAt(index)-'a']);
         return res;
     }
 }
