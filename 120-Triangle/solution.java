0c9414362af57403909d85aee726653abdaf2b1a
jiangran910126 <jiangran0126@gmail.com>
2016-08-09 17:02:42 -0500

    [120. Triangle][Accepted]

--- 120-Triangle/solution.java
+++ 120-Triangle/solution.java
@@ -1,11 +1,11 @@
 public class Solution {
     public int minimumTotal(List<List<Integer>> triangle) {
         int[] dp=new int[triangle.size()];
-        for(int i=0;i<triangle.get(triangle.size()-1).size();i++) dp[i]=triangle.get(triangle.size()-1).get(i);
-        for(int layer=triangle.size()-2;layer>=0;layer--){
-            for(int i=0;i<=layer;i++){
-                dp[i]=Math.min(dp[i],dp[i+1])+triangle.get(layer).get(i);
-            }
+        for(int i=0;i<triangle.get(triangle.size()-1).size();i++)
+            dp[i]=triangle.get(triangle.size()-1).get(i);
+        for(int i=triangle.size()-2;i>=0;i--){
+            for(int j=0;j<=i;j++)
+                dp[j]=Math.min(dp[j],dp[j+1])+triangle.get(i).get(j);
         }
         return dp[0];
     }
