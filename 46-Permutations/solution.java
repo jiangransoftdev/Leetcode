bb4bde2684a5b7c6b30bae73da5d18706789704c
jiangran910126 <jiangran0126@gmail.com>
2016-08-09 23:29:14 -0500

    [46. Permutations][Accepted]

---
+++ 46-Permutations/solution.java
@@ -0,0 +1,24 @@
+public class Solution {
+    public List<List<Integer>> permute(int[] nums) {
+        List<List<Integer>> res=new ArrayList<>();
+        if(nums.length==0) return res;
+        helper(nums,0,res);
+        return res;
+    }
+    public void helper(int[] nums,int index,List<List<Integer>> res){
+        if(index==nums.length){
+            res.add(new ArrayList<Integer>(){{ for (int i : nums) add(i); }});
+            return;
+        }
+        for(int i=index;i<nums.length;i++){
+            swap(nums,index,i);
+            helper(nums,index+1,res);
+            swap(nums,index,i);
+        }
+    }
+    public void swap(int[] nums,int i,int j){
+        int tmp=nums[i];
+        nums[i]=nums[j];
+        nums[j]=tmp;
+    }
+}
