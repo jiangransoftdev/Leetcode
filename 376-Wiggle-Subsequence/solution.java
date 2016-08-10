bf35357f6a8bc20d3f1949c73b1e75cc3f782d15
jiangran910126 <jiangran0126@gmail.com>
2016-08-10 15:17:16 -0500

    [376. Wiggle Subsequence][Accepted]

--- 376-Wiggle-Subsequence/solution.java
+++ 376-Wiggle-Subsequence/solution.java
@@ -1,12 +1,12 @@
 public class Solution {
     public int wiggleMaxLength(int[] nums) {
-        int presign=0,len=1;
         if(nums.length<=1) return nums.length;
+        int pre=0,len=1;
         for(int i=1;i<nums.length;i++){
             int sign=nums[i]-nums[i-1];
-            if(sign==0||sign*presign>0) continue;
+            if(sign==0||sign*pre>0) continue;
             else{
-                presign=sign;
+                pre=sign;
                 len++;
             }
         }
