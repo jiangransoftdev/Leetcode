48d91de6417ac5803b2a0acd41db446bb524c8be
jiangran910126 <jiangran0126@gmail.com>
2016-08-10 15:22:21 -0500

    [377. Combination Sum IV][Accepted]

--- 377-Combination-Sum-IV/solution.java
+++ 377-Combination-Sum-IV/solution.java
@@ -4,7 +4,7 @@ public class Solution {
         dp[0]=1;
         for(int i=1;i<=target;i++){
             for(int j=0;j<nums.length;j++){
-                if(i>=nums[j])
+                if(i-nums[j]>=0)
                     dp[i]+=dp[i-nums[j]];
             }
         }
