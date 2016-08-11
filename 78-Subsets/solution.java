b78fc0130fef257fd271389c4969713ddbd275ed
jiangran910126 <jiangran0126@gmail.com>
2016-08-11 16:57:46 -0500

    [78. Subsets][Accepted]

---
+++ 78-Subsets/solution.java
@@ -0,0 +1,16 @@
+public class Solution {
+    public List<List<Integer>> subsets(int[] nums) {
+        List<List<Integer>> res=new ArrayList<>();
+        if(nums.length==0) return res;
+        helper(nums,0,new ArrayList<>(),res);
+        return res;
+    }
+    public void helper(int[] nums,int index,List<Integer> solu,List<List<Integer>> res){
+        res.add(new ArrayList<>(solu));
+        for(int i=index;i<nums.length;i++){
+            solu.add(nums[i]);
+            helper(nums,i+1,solu,res);
+            solu.remove(solu.size()-1);
+        }
+    }
+}
