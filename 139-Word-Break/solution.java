f11a24b728d219e8cf1c2b918ca1acc515820ccb
jiangran910126 <jiangran0126@gmail.com>
2016-08-11 14:28:59 -0500

    [139. Word Break][Accepted]

--- 139-Word-Break/solution.java
+++ 139-Word-Break/solution.java
@@ -1,20 +1,14 @@
 public class Solution {
     public boolean wordBreak(String s, Set<String> wordDict) {
-        if(s.length()==0) return true;
         boolean[] dp=new boolean[s.length()+1];
         for(int i=1;i<=s.length();i++){
-            String sub=s.substring(0,i);
-            if(wordDict.contains(sub)){
+            if(wordDict.contains(s.substring(0,i)))
                 dp[i]=true;
-            }
             for(int j=1;j<i;j++){
-                if(dp[j]&&wordDict.contains(s.substring(j,i))){
+                if(dp[j]&&wordDict.contains(s.substring(j,i)))
                     dp[i]=true;
-                    break;
-                }
             }
         }
         return dp[s.length()];
     }
-    
 }
