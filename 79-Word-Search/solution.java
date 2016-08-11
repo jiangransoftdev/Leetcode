465fbcab8d5442e41254efd7763926ea373266bb
jiangran910126 <jiangran0126@gmail.com>
2016-08-11 18:18:06 -0500

    [79. Word Search][Accepted]

--- 79-Word-Search/solution.java
+++ 79-Word-Search/solution.java
@@ -1,22 +1,26 @@
 public class Solution {
     public boolean exist(char[][] board, String word) {
+        if(board.length==0) return false;
         int m=board.length;
-        if(m==0) return false;
         int n=board[0].length;
-        boolean res=false;
         for(int i=0;i<m;i++){
-            for(int j=0;j<n;j++)
-                res|=helper(board,m,n,i,j,0,word);
+            for(int j=0;j<n;j++){
+                if(helper(board,i,j,m,n,0,word))
+                    return true;
+            }
         }
-        return res;
+        return false;
     }
-    public boolean helper(char[][] board,int m,int n,int x,int y,int index,String word){
+    public boolean helper(char[][] board,int i,int j,int m,int n,int index,String word){
+        boolean res=false;
         if(index==word.length()) return true;
-        if(x<0||x>=m||y<0||y>=n) return false;
-        if(board[x][y]!=word.charAt(index)) return false;
-        board[x][y]='#';
-        boolean res=helper(board,m,n,x-1,y,index+1,word)||helper(board,m,n,x,y-1,index+1,word)||helper(board,m,n,x+1,y,index+1,word)||helper(board,m,n,x,y+1,index+1,word);
-        board[x][y]=word.charAt(index);
+        if(i<0||i>=m||j<0||j>=n||board[i][j]!=word.charAt(index)) return false;
+        
+        char c=board[i][j];
+        board[i][j]='#';
+        res|=helper(board,i-1,j,m,n,index+1,word)||helper(board,i+1,j,m,n,index+1,word)||helper(board,i,j-1,m,n,index+1,word)||helper(board,i,j+1,m,n,index+1,word);
+        board[i][j]=c;
         return res;
+        
     }
 }
