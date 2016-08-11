5d04d74f267764e6427d52a0aa18324719eb7a5a
jiangran910126 <jiangran0126@gmail.com>
2016-08-10 23:24:39 -0500

    [365. Water and Jug Problem][Accepted]

--- 365-Water-and-Jug-Problem/solution.java
+++ 365-Water-and-Jug-Problem/solution.java
@@ -1,12 +1,12 @@
 public class Solution {
     public boolean canMeasureWater(int x, int y, int z) {
-        int num=gcd(x,y);
-        if(num==0){
-            if(z==0) return true;
-            else return false;
+        int g=gcd(x,y);
+        if(g==0){
+            if(z==x||z==y) return true;
+            return false;
         }
         if(x+y<z) return false;
-        return z%num==0;
+        return z%g==0;
     }
     public int gcd(int x,int y){
         if(y==0) return 0;
