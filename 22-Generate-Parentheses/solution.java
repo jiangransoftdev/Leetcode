4e8e83774303feba1c900d0aafc13e5f2cef4086
jiangran910126 <jiangran0126@gmail.com>
2016-08-11 15:48:15 -0500

    [22. Generate Parentheses][Accepted]

--- 22-Generate-Parentheses/solution.java
+++ 22-Generate-Parentheses/solution.java
@@ -1,21 +1,21 @@
 public class Solution {
     public List<String> generateParenthesis(int n) {
         List<String> res=new ArrayList<>();
-        if(n==0) return res;
         helper(n,0,0,"",res);
         return res;
     }
     public void helper(int n,int l,int r,String solu,List<String> res){
         if(l+r==2*n){
-            if(l==r)
-                res.add(solu);
+            res.add(solu);
             return;
         }
-        if(l>r){
-            helper(n,l+1,r,solu+"(",res);
-            helper(n,l,r+1,solu+")",res);
+        if(l<n){
+            String another=solu+"(";
+            helper(n,l+1,r,another,res);
         }
-        else
-            helper(n,l+1,r,solu+"(",res);
+        if(l>r){
+            String another=solu+")";
+            helper(n,l,r+1,another,res);
+        }
     }
 }
