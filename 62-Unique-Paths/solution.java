cb5fea6e52e52859628000d5cd8531f713ebc99a
jiangran910126 <jiangran0126@gmail.com>
2016-08-12 00:49:30 -0500

    [62. Unique Paths][Accepted]

--- 62-Unique-Paths/solution.java
+++ 62-Unique-Paths/solution.java
@@ -1,13 +1,16 @@
 public class Solution {
     public int uniquePaths(int m, int n) {
-        int[][] dp=new int[m+1][n+1];
-        dp[1][1]=1;
-        for(int i=1;i<=m;i++){
-            for(int j=1;j<=n;j++){
-                if(i==1&&j==1) continue;
+        if(m==0) return 0;
+        int[][] dp=new int[m][n];
+        dp[0][0]=1;
+        for(int i=1;i<m;i++)
+            dp[i][0]=1;
+        for(int i=1;i<n;i++)
+            dp[0][i]=1;
+        for(int i=1;i<m;i++){
+            for(int j=1;j<n;j++)
                 dp[i][j]=dp[i-1][j]+dp[i][j-1];
-            }
         }
-        return dp[m][n];
+        return dp[m-1][n-1];
     }
 }
