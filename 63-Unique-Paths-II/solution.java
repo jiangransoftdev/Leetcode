aa5ab4c272c6dc0ffa355acae5203cc33379e5e0
jiangran910126 <jiangran0126@gmail.com>
2016-08-12 01:15:11 -0500

    [63. Unique Paths II][Accepted]

--- 63-Unique-Paths-II/solution.java
+++ 63-Unique-Paths-II/solution.java
@@ -9,10 +9,10 @@ public class Solution {
         for(int i=1;i<=m;i++){
             for(int j=1;j<=n;j++){
                 if(i==1&&j==1) continue;
-                if(obstacleGrid[i-1][j-1]==1)
-                    dp[i][j]=0;
+                if(obstacleGrid[i-1][j-1]==1) dp[i][j]=0;
                 else dp[i][j]=dp[i-1][j]+dp[i][j-1];
             }
+                
         }
         return dp[m][n];
     }
