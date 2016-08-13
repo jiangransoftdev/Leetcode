7a1e0224971d48db5920516f89e5cfc082148205
jiangran910126 <jiangran0126@gmail.com>
2016-08-13 17:12:10 -0500

    [132. Palindrome Partitioning II][Accepted]

--- 132-Palindrome-Partitioning-II/solution.java
+++ 132-Palindrome-Partitioning-II/solution.java
@@ -1,12 +1,14 @@
 public class Solution {
     public int minCut(String s) {
+        if(s.length()<=1) return 0;
         int l=s.length();
-        boolean[][] p=new boolean[l][l];
         int[] dp=new int[l+1];
-        for(int i=0;i<=l;i++) dp[i]=l-i;
+        boolean[][] p=new boolean[l][l];
+        for(int i=0;i<=l;i++)
+            dp[i]=l-i;
         for(int i=l-1;i>=0;i--){
             for(int j=i;j<l;j++){
-                if(s.charAt(i)==s.charAt(j)&&((j-i)<2||p[i+1][j-1])){
+                if(s.charAt(i)==s.charAt(j)&&(j-i<=2||p[i+1][j-1])){
                     p[i][j]=true;
                     dp[i]=Math.min(dp[i],dp[j+1]+1);
                 }
