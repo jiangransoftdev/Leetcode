022a83eb7d6fcf9d3581d7f5d0c8b9bd8cb974b4
jiangran910126 <jiangran0126@gmail.com>
2016-08-13 21:02:40 -0500

    [75. Sort Colors][Accepted]

---
+++ 75-Sort-Colors/solution.java
@@ -0,0 +1,18 @@
+public class Solution {
+    public void sortColors(int[] nums) {
+        if(nums.length==0) return;
+        int i=0,j=0,k=nums.length-1;
+        while(j<=k){
+            if(nums[j]==0)
+                swap(nums,i++,j++);
+            else if(nums[j]==2)
+                swap(nums,j,k--);
+            else j++;
+        }
+    }
+    public void swap(int[] nums,int i,int j){
+        int tmp=nums[i];
+        nums[i]=nums[j];
+        nums[j]=tmp;
+    }
+}
