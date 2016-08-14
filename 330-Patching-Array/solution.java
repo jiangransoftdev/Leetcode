d56431d56cb2c86aeabb855cc973039fcc677242
jiangran910126 <jiangran0126@gmail.com>
2016-08-13 20:57:30 -0500

    [330. Patching Array][Accepted]

--- 330-Patching-Array/solution.java
+++ 330-Patching-Array/solution.java
@@ -1,17 +1,15 @@
 public class Solution {
     public int minPatches(int[] nums, int n) {
+        int res=0,i=0;
         long sum=1;
-        int addno=0;
-        int i=0;
         while(sum<=n){
-            if(i<nums.length&&nums[i]<=sum){
+            if(i<nums.length&&nums[i]<=sum)
                 sum+=nums[i++];
-            }
             else{
                 sum+=sum;
-                addno++;
+                res++;
             }
         }
-        return addno;
+        return res;
     }
 }
