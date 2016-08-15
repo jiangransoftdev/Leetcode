5798aaf4566fa94210b7ff12857bf03fe31ff663
jiangran910126 <jiangran0126@gmail.com>
2016-08-15 15:54:36 -0500

    [187. Repeated DNA Sequences][Accepted]

--- 187-Repeated-DNA-Sequences/solution.java
+++ 187-Repeated-DNA-Sequences/solution.java
@@ -1,11 +1,11 @@
 public class Solution {
     public List<String> findRepeatedDnaSequences(String s) {
-         Set<String> seen=new HashSet<>(),repeat=new HashSet<>();
-        for(int i=0;i+9<s.length();i++){
-        	String sub=s.substring(i,i+10);
-        	if(!seen.add(sub))
-        		repeat.add(sub);
+        Set<String> set=new HashSet<>();
+        Set<String> res=new HashSet<>();
+        for(int i=0;i+10<=s.length();i++){
+            String sub=s.substring(i,i+10);
+            if(!set.add(sub)) res.add(sub);
         }
-        return new ArrayList(repeat);
+        return new ArrayList(res);
     }
 }
