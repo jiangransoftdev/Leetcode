87ced58ab1fc74fbf657ba6665719ec4476f5e48
jiangran910126 <jiangran0126@gmail.com>
2016-08-15 00:08:41 -0500

    [51. N-Queens][Accepted]

--- 51-N-Queens/solution.java
+++ 51-N-Queens/solution.java
@@ -1,36 +1,34 @@
 public class Solution {
     public List<List<String>> solveNQueens(int n) {
         List<List<String>> res=new ArrayList<>();
-        if(n==0) return res;
-        int[] row=new int[n];
-        helper(n,0,row,res);
+        int[] q=new int[n];
+        helper(n,0,q,res);
         return res;
     }
-    public void helper(int n,int index,int[] row,List<List<String>> res){
+    public void helper(int n,int index,int[] q,List<List<String>> res){
         if(index==n){
             List<String> solu=new ArrayList<>();
             for(int i=0;i<n;i++){
-                StringBuilder sb=new StringBuilder();
+                StringBuilder s=new StringBuilder();
                 for(int j=0;j<n;j++){
-                    if(row[i]==j)
-                        sb.append("Q");
-                    else sb.append(".");
+                    if(q[i]==j)
+                        s.append("Q");
+                    else s.append(".");
                 }
-                solu.add(sb.toString());
+                solu.add(s.toString());
             }
             res.add(solu);
             return;
         }
         for(int i=0;i<n;i++){
-            row[index]=i;
-            if(isValid(index,row))
-                helper(n,index+1,row,res);
+            q[index]=i;
+            if(isValid(q,index))
+                helper(n,index+1,q,res);
         }
     }
-    public boolean isValid(int index,int[] row){
-        for(int i=0;i<index;i++){
-            if(row[i]==row[index]||index-i==Math.abs(row[i]-row[index]))
-                return false;
+    public boolean isValid(int[] q,int n){
+        for(int i=0;i<n;i++){
+            if(q[i]==q[n]||Math.abs(n-i)==Math.abs(q[i]-q[n])) return false;
         }
         return true;
     }
