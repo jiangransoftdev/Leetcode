fae873fa3ab93b5e1398813bea6f1d1e55b3d0b7
jiangran910126 <jiangran0126@gmail.com>
2016-08-14 21:12:30 -0500

    [329. Longest Increasing Path in a Matrix][Accepted]

--- 329-Longest-Increasing-Path-in-a-Matrix/solution.java
+++ 329-Longest-Increasing-Path-in-a-Matrix/solution.java
@@ -1,26 +1,24 @@
 public class Solution {
     public int longestIncreasingPath(int[][] matrix) {
-        int m=matrix.length;
-        if(m==0) return 0;
-        int n=matrix[0].length;
+        if(matrix.length==0) return 0;
+        int m=matrix.length,n=matrix[0].length,res=0;
         int[][] len=new int[m][n];
-        int res=0;
         for(int i=0;i<m;i++){
-            for(int j=0;j<n;j++){
-                res=Math.max(res,explore(matrix,i,j,len,Integer.MIN_VALUE));
-            }
+            for(int j=0;j<n;j++)
+                res=Math.max(res,helper(matrix,i,j,len,Integer.MIN_VALUE));
         }
         return res;
     }
-    public int explore(int[][] matrix,int i,int j,int[][] len,int pre){
-        if(i<0||i>=matrix.length||j<0||j>=matrix[0].length||matrix[i][j]<=pre) return 0;
+    public int helper(int[][] matrix,int i,int j,int[][] len,int pre){
+        if(i<0||i>=matrix.length||j<0||j>=matrix[0].length||matrix[i][j]<=pre)
+            return 0;
         if(len[i][j]>0) return len[i][j];
         int count=0;
         pre=matrix[i][j];
-        count=Math.max(count,explore(matrix,i-1,j,len,pre));
-        count=Math.max(count,explore(matrix,i+1,j,len,pre));
-        count=Math.max(count,explore(matrix,i,j-1,len,pre));
-        count=Math.max(count,explore(matrix,i,j+1,len,pre));
+        count=Math.max(count,helper(matrix,i-1,j,len,pre));
+        count=Math.max(count,helper(matrix,i+1,j,len,pre));
+        count=Math.max(count,helper(matrix,i,j-1,len,pre));
+        count=Math.max(count,helper(matrix,i,j+1,len,pre));
         len[i][j]=++count;
         return count;
     }
