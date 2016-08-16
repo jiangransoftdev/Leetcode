89b26c1c155410813b794cd5349638634d0643b4
jiangran910126 <jiangran0126@gmail.com>
2016-08-15 23:21:12 -0500

    [383. Ransom Note][Accepted]

--- 383-Ransom-Note/solution.java
+++ 383-Ransom-Note/solution.java
@@ -1,28 +1,18 @@
 public class Solution {
     public boolean canConstruct(String ransomNote, String magazine) {
-        if (ransomNote == null || magazine == null || ransomNote.length() > magazine.length())
-return false;
-
-	char[] s1 = ransomNote.toCharArray();
-	char[] s2 = magazine.toCharArray();
-	
-	Arrays.sort(s1);
-	Arrays.sort(s2);
-	
-	int i = 0, j = 0;
-	while (j < s2.length && i < s1.length){
-		if (s1[i] ==  s2[j]){
-			i++;
-			j++;
-		}
-		else if (s1[i] < s2[j]){
-			return false;
-		}
-		else if (s1[i] > s2[j]){
-			j++;
-		}
-	}
-	
-	return i == s1.length;
+        char[] c1=ransomNote.toCharArray();
+        char[] c2=magazine.toCharArray();
+        Arrays.sort(c1);
+        Arrays.sort(c2);
+        int i=0,j=0;
+        while(i<c1.length&&j<c2.length){
+            if(c1[i]==c2[j]){
+                i++;
+                j++;
+            }
+            else if(c1[i]<c2[j]) return false;
+            else if(c1[i]>c2[j]) j++;
+        }
+        return i==c1.length;
     }
 }
