8680712cb4d4fec2e5909ee7022c9ee6ca670c4a
jiangran910126 <jiangran0126@gmail.com>
2016-08-15 22:44:03 -0500

    [89. Gray Code][Accepted]

--- 89-Gray-Code/solution.java
+++ 89-Gray-Code/solution.java
@@ -1,29 +1,12 @@
 public class Solution {
-    public List<Integer> res = new ArrayList<Integer>();
-
-public List<Integer> grayCode(int n) {
-	int[] a = new int[n];
-	if (n == 0) {
-		res.add(0);
-		return res;
-	}
-	cal(0, a, n);
-	return res;
-}
-
-private void cal(int index, int[] bits, int n) {
-	if (index == n) {
-		int sum = 0;
-		sum += bits[0] * (1 << n - 1);
-		for (int i = 1; i < n; i++) {
-			sum += (bits[i] ^ bits[i - 1]) * (1 << (n - i - 1));
-		}
-		res.add(sum);
-	} else {
-		bits[index] = 0;
-		cal(index + 1, bits, n);
-		bits[index] = 1;
-		cal(index + 1, bits, n);
-	}
-}
+    public List<Integer> grayCode(int n) {
+        List<Integer> res=new ArrayList<>();
+        res.add(0);
+        for(int i=0;i<n;i++){
+            int add=1<<i;
+            for(int j=res.size()-1;j>=0;j--)
+                res.add(res.get(j)+add);
+        }
+        return res;
+    }
 }
