5fc1cca621fc478215e7efa1ac0c07a22f98fd38
jiangran910126 <jiangran0126@gmail.com>
2016-08-16 01:45:03 -0500

    [10. Regular Expression Matching][Accepted]

--- 10-Regular-Expression-Matching/solution.java
+++ 10-Regular-Expression-Matching/solution.java
@@ -1,26 +1,23 @@
 public class Solution {
     public boolean isMatch(String s, String p) {
         if(s==null||p==null) return false;
-        boolean[][] dp=new boolean[s.length()+1][p.length()+1];
+        int m=s.length(),n=p.length();
+        boolean[][] dp=new boolean[m+1][n+1];
         dp[0][0]=true;
-        for(int i=0;i<p.length();i++)
+        for(int i=0;i<n;i++){
             if(p.charAt(i)=='*'&&dp[0][i-1])
                 dp[0][i+1]=true;
-        for(int i=1;i<=s.length();i++){
-            for(int j=1;j<=p.length();j++){
-                if(s.charAt(i-1)==p.charAt(j-1))
-                    dp[i][j]=dp[i-1][j-1];
-                else if(p.charAt(j-1)=='.')
+        }
+        for(int i=1;i<=m;i++){
+            for(int j=1;j<=n;j++){
+                if(s.charAt(i-1)==p.charAt(j-1)||p.charAt(j-1)=='.')
                     dp[i][j]=dp[i-1][j-1];
                 else if(p.charAt(j-1)=='*'){
-                    if(p.charAt(j-2)!=s.charAt(i-1)&&p.charAt(j-2)!='.')
-                        dp[i][j]=dp[i][j-2];
-                    else{
-                        dp[i][j]=dp[i][j-1]||dp[i-1][j]||dp[i][j-2];
-                    }
+                    if(p.charAt(j-2)!=s.charAt(i-1)&&p.charAt(j-2)!='.') dp[i][j]=dp[i][j-2];
+                    else dp[i][j]=dp[i][j-2]||dp[i-1][j]||dp[i][j-1];
                 }
             }
         }
-        return dp[s.length()][p.length()];
+        return dp[m][n];
     }
 }
