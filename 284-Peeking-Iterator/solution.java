fe9dfcfabb2787f79733a170d91b19742e0b899c
jiangran910126 <jiangran0126@gmail.com>
2016-08-15 23:49:59 -0500

    [284. Peeking Iterator][Accepted]

--- 284-Peeking-Iterator/solution.java
+++ 284-Peeking-Iterator/solution.java
@@ -1,8 +1,8 @@
 // Java Iterator interface reference:
 // https://docs.oracle.com/javase/8/docs/api/java/util/Iterator.html
 class PeekingIterator implements Iterator<Integer> {
-    Integer next=null;
     Iterator<Integer> itr;
+    Integer next=null;
 	public PeekingIterator(Iterator<Integer> iterator) {
 	    // initialize any member here.
 	    itr=iterator;
@@ -18,8 +18,8 @@ class PeekingIterator implements Iterator<Integer> {
 	// Override them if needed.
 	@Override
 	public Integer next() {
-	    int tmp=next;
-	    next=itr.hasNext()?itr.next():null;
+	    Integer tmp=next;
+	    next=(itr.hasNext())?itr.next():null;
 	    return tmp;
 	}
 
