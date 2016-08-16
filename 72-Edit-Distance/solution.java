ebb3cfad0efb56cc16e95223c9631b9d89f52b9c
jiangran910126 <jiangran0126@gmail.com>
2016-08-16 15:31:35 -0500

    [72. Edit Distance][Accepted]

--- 72-Edit-Distance/solution.java
+++ 72-Edit-Distance/solution.java
@@ -7,8 +7,8 @@ public class Solution {
             dp[i][0]=i;
         for(int i=0;i<=n;i++)
             dp[0][i]=i;
-        for(int i=1;i<m+1;i++){
-            for(int j=1;j<n+1;j++){
+        for(int i=1;i<=m;i++){
+            for(int j=1;j<=n;j++){
                 if(word1.charAt(i-1)==word2.charAt(j-1))
                     dp[i][j]=dp[i-1][j-1];
                 else
