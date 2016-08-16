6e45c1cd59fc02a672694bcd6fabdc44b3ae144c
jiangran910126 <jiangran0126@gmail.com>
2016-08-16 16:37:57 -0500

    [99. Recover Binary Search Tree][Accepted]

--- 99-Recover-Binary-Search-Tree/solution.java
+++ 99-Recover-Binary-Search-Tree/solution.java
@@ -8,8 +8,8 @@
  * }
  */
 public class Solution {
-    int cur=Integer.MIN_VALUE;
-    TreeNode node1=null,node2=null,curnode=null;
+    TreeNode node1=null,node2=null,prevnode=null;
+    int prev=Integer.MIN_VALUE;
     public void recoverTree(TreeNode root) {
         helper(root);
         swap(node1,node2);
@@ -17,15 +17,15 @@ public class Solution {
     public void helper(TreeNode root){
         if(root==null) return;
         helper(root.left);
-        if(node1==null&&cur>=root.val) node1=curnode;
-        if(node1!=null&&cur>=root.val) node2=root;
-        cur=root.val;
-        curnode=root;
+        if(node1==null&&prev>=root.val) node1=prevnode;
+        if(node1!=null&&prev>=root.val) node2=root;
+        prevnode=root;
+        prev=root.val;
         helper(root.right);
     }
-    public void swap(TreeNode node1,TreeNode node2){
-        int tmp=node1.val;
-        node1.val=node2.val;
-        node2.val=tmp;
+    public void swap(TreeNode i,TreeNode j){
+        int tmp=i.val;
+        i.val=j.val;
+        j.val=tmp;
     }
 }
