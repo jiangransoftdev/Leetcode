28a119417af54945cf6a9fd06401f398ca545feb
jiangran910126 <jiangran0126@gmail.com>
2016-08-16 23:38:42 -0500

    [315. Count of Smaller Numbers After Self][Accepted]

--- 315-Count-of-Smaller-Numbers-After-Self/solution.java
+++ 315-Count-of-Smaller-Numbers-After-Self/solution.java
@@ -1,34 +1,33 @@
 public class Solution {
     public List<Integer> countSmaller(int[] nums) {
-        LinkedList<Integer> res=new LinkedList<>();
+        List<Integer> res=new ArrayList<>();
         if(nums.length==0) return res;
-        int max=Integer.MIN_VALUE;
-        int min=Integer.MAX_VALUE;
-        for(int i:nums)
-            min=Math.min(min,i);
+        int min=Integer.MAX_VALUE,max=Integer.MIN_VALUE;
+        for(int num:nums)
+            min=Math.min(min,num);
         int[] nums2=new int[nums.length];
         for(int i=0;i<nums.length;i++){
             nums2[i]=nums[i]-min+1;
-            max=Math.max(nums2[i],max);
+            max=Math.max(max,nums2[i]);
         }
-        int[] tree=new int[max+1];
+        int[] count=new int[max+1];
         for(int i=nums2.length-1;i>=0;i--){
-            res.add(0,get(nums2[i]-1,tree));
-            update(nums2[i],tree);
+            res.add(0,get(count,nums2[i]-1));
+            update(count,nums2[i]);
         }
         return res;
     }
-    public int get(int i,int[] tree){
+    public int get(int[] count,int i){
         int sum=0;
         while(i>0){
-            sum+=tree[i];
+            sum+=count[i];
             i-=i&-i;
         }
         return sum;
     }
-    public void update(int i,int[] tree){
-        while(i<tree.length){
-            tree[i]++;
+    public void update(int[] count,int i){
+        while(i<count.length){
+            count[i]+=1;
             i+=i&-i;
         }
     }
