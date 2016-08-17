eb81f20334fd20ffcf0cfef26d703a346e34c0e5
jiangran910126 <jiangran0126@gmail.com>
2016-08-17 01:59:12 -0500

    [222. Count Complete Tree Nodes][Accepted]

---
+++ 222-Count-Complete-Tree-Nodes/solution.java
@@ -0,0 +1,18 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+public class Solution {
+    public int height(TreeNode root){
+        return root==null?-1:1+height(root.left);
+    }
+    public int countNodes(TreeNode root) {
+        int h=height(root);
+        return h<0?0:height(root.right)==(h-1)?(1<<h)+countNodes(root.right):(1<<(h-1))+countNodes(root.left);
+    }
+}
