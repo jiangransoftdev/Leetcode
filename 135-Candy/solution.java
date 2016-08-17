1d2d9e6005998b6a5bdefe981cbb4e7b2bf3d5c6
jiangran910126 <jiangran0126@gmail.com>
2016-08-16 22:02:47 -0500

    [135. Candy][Accepted]

--- 135-Candy/solution.java
+++ 135-Candy/solution.java
@@ -1,41 +1,33 @@
 public class Solution {
     public int candy(int[] ratings) {
+        if(ratings.length==0) return 0;
         int n=ratings.length;
-    int index=0;
-    int total=n;
-    int increase=1;
-    int decrease=1;
-    while(index<n){
-    	int i=index;
-    	if(i+1==n) break;
-    	int count=1;
-    	if(i+1<n&&ratings[i+1]<ratings[i]){
-    		while(i+1<n&&ratings[i+1]<ratings[i]){
-    		    i++;
-    		    count++;
-    		}
-    		decrease=1;
-    		if(count>increase){
-    		    total+=count-increase;
-    		} 
-    		increase=decrease;
-    		for(int j=i-1;j>index;j--){
-    		    total+=decrease;
-    		    decrease++;
-    		}
-    		index=i;
-    	}
-    	else if(i+1<n&&ratings[i+1]>ratings[i]){
-    		total+=increase;
-    		increase++;
-    		index++;
-    	}
-    	else if(i+1<n&&ratings[i+1]==ratings[i]){
-    	    index++;
-    	    increase=1;
-    	}
-    		
-    }
-    return total;
+        int total=n,increase=1,index=0;
+        while(index<n){
+            int i=index;
+            if(i+1==n) break;
+            int count=1;
+            if(i+1<n&&ratings[i+1]<ratings[i]){
+                while(i+1<n&&ratings[i+1]<ratings[i]){
+                    i++;
+                    count++;
+                }
+                if(increase<count)
+                    total+=count-increase;
+                increase=1;
+                total+=(count-2)*(count-1)/2;
+                index=i;
+            }
+            else if(i+1<n&&ratings[i+1]>ratings[i]){
+                total+=increase;
+                increase++;
+                index++;
+            }
+            else if(i+1<n&&ratings[i+1]==ratings[i]){
+                increase=1;
+                index++;
+            }
+        }
+        return total;
     }
 }
