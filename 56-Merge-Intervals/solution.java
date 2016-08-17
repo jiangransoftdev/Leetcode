21de3447ce84ca476f63530cdd84f2948c8947b1
jiangran910126 <jiangran0126@gmail.com>
2016-08-17 02:15:57 -0500

    [56. Merge Intervals][Accepted]

--- 56-Merge-Intervals/solution.java
+++ 56-Merge-Intervals/solution.java
@@ -9,27 +9,26 @@
  */
 public class Solution {
     public List<Interval> merge(List<Interval> intervals) {
+        Collections.sort(intervals, new Comparator<Interval>(){
+        	public int compare(Interval a,Interval b){
+        		return a.start-b.start;
+        	}
+        });
         List<Interval> res=new ArrayList<>();
         if(intervals.size()==0) return res;
-        Collections.sort(intervals,new Comparator<Interval>(){
-            public int compare(Interval a,Interval b){
-                if(a.start!=b.start) return a.start-b.start;
-                else return a.end-b.end;
-            }
-            });
-        int i=1,start=intervals.get(0).start,end=intervals.get(0).end;
-        while(i<intervals.size()){
-            int tstart=intervals.get(i).start,tend=intervals.get(i).end;
+        int start=intervals.get(0).start;
+        int end=intervals.get(0).end;
+        for(int i=1;i<intervals.size();i++){
+            int tstart=intervals.get(i).start;
+            int tend=intervals.get(i).end;
             if(tstart>end){
                 res.add(new Interval(start,end));
                 start=tstart;
                 end=tend;
             }
-            else{
-                if(tend>end)
-                    end=tend;
+            else if(tstart<=end&&tend>end){
+                end=tend;
             }
-            i++;
         }
         res.add(new Interval(start,end));
         return res;
