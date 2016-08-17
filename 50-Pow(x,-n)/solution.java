23e23b61b1f66dac1d867c60cb4a7d1582881310
jiangran910126 <jiangran0126@gmail.com>
2016-08-17 01:13:34 -0500

    [50. Pow(x, n)][Accepted]

---
+++ 50-Pow(x,-n)/solution.java
@@ -0,0 +1,8 @@
+public class Solution {
+    public double myPow(double x, int n) {
+         if(n==0) return 1;
+        double t = myPow(x,n/2);
+        if(n%2!=0) return n<0 ? 1/x*t*t : x*t*t;
+        else return t*t;
+    }
+}
