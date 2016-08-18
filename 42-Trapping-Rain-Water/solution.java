9166849172c6ea85ec98d635f2b2ffb90b9c797c
jiangran910126 <jiangran0126@gmail.com>
2016-08-18 00:17:33 -0500

    [42. Trapping Rain Water][Accepted]

--- 42-Trapping-Rain-Water/solution.java
+++ 42-Trapping-Rain-Water/solution.java
@@ -1,22 +1,24 @@
 public class Solution {
     public int trap(int[] height) {
-        int[] maxl=new int[height.length],maxr=new int[height.length];
-        int max=0;
-        for(int i=1;i<height.length;i++){
-            if(max<height[i-1])
-                max=height[i-1];
+        if(height.length==0) return 0;
+        int n=height.length;
+        int[] maxl=new int[n];
+        int[] maxr=new int[n];
+        int max=0,total=0;
+        for(int i=0;i<n;i++){
             maxl[i]=max;
+            max=Math.max(height[i],max);
         }
         max=0;
-        for(int i=height.length-2;i>=0;i--){
-            if(max<height[i+1])
-                max=height[i+1];
+        for(int i=n-1;i>=0;i--){
             maxr[i]=max;
+            max=Math.max(height[i],max);
         }
-        int res=0;
-        for(int i=0;i<height.length;i++){
-            res+=(Math.min(maxl[i],maxr[i])-height[i])>0?(Math.min(maxl[i],maxr[i])-height[i]):0;
+        for(int i=0;i<n;i++){
+            int tap=Math.min(maxl[i],maxr[i])-height[i];
+            if(tap>0)
+                total+=tap;
         }
-        return res;
+        return total;
     }
 }
