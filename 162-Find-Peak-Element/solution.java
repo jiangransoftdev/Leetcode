31aa518320e221f6b7fb35ccf95985c1f0e2897c
jiangran910126 <jiangran0126@gmail.com>
2016-08-18 01:26:42 -0500

    [162. Find Peak Element][Accepted]

--- 162-Find-Peak-Element/solution.java
+++ 162-Find-Peak-Element/solution.java
@@ -3,19 +3,17 @@ public class Solution {
         return helper(nums,0,nums.length-1);
     }
     public int helper(int[] nums,int start,int end){
-	if(start==end) return start;
-	else if(start+1==end){
-	    if(nums[start]>nums[end]) return start;
-	    return end;
-	}
-	else{
-	    int mid=(start+end)/2;
-	    if(nums[mid]>nums[mid+1]&&nums[mid]>nums[mid-1])
-	        return mid;
-	    else if(nums[mid]<nums[mid+1]&&nums[mid]>nums[mid-1])
-	        return helper(nums,mid+1,end);
-	    else
-	        return helper(nums,start,mid-1);
-	}
-}
+        if(start==end)
+            return start;
+        else if(start+1==end){
+            if(nums[start]>nums[end]) return start;
+            return end;
+        }
+        else{
+            int mid=start+(end-start)/2;
+            if(nums[mid]>nums[mid-1]&&nums[mid]>nums[mid+1]) return mid;
+            else if(nums[mid-1]<nums[mid]&&nums[mid]<nums[mid+1]) return helper(nums,mid+1,end);
+            else return helper(nums,start,mid-1);
+        }
+    }
 }
