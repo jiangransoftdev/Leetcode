052d75592baf1a86f735eaedcfc38a3dc9b99292
jiangran910126 <jiangran0126@gmail.com>
2016-08-18 20:14:22 -0500

    [224. Basic Calculator][Accepted]

--- 224-Basic-Calculator/solution.java
+++ 224-Basic-Calculator/solution.java
@@ -1,30 +1,29 @@
 public class Solution {
     public int calculate(String s) {
-        if(s.length()==0) return 0;
-        int res=0,co=1;
+        int res=0,num=0,co=1;
         Stack<Integer> sta=new Stack<>();
         for(int i=0;i<s.length();i++){
-            if(s.charAt(i)>=48&&s.charAt(i)<=57){
-                int num=s.charAt(i)-'0';
-                while(i+1<s.length()&&s.charAt(i+1)>=48&&s.charAt(i+1)<=57){
-                    num=num*10+s.charAt(i+1)-'0';
+            char c=s.charAt(i);
+            if(c>='0'&&c<='9'){
+                num=c-'0';
+                while(i+1<s.length()&&s.charAt(i+1)>='0'&&s.charAt(i+1)<='9'){
+                    num=num*10+(s.charAt(i+1)-'0');
                     i++;
                 }
                 res+=num*co;
             }
-            else if(s.charAt(i)=='+')
+            else if(c=='+')
                 co=1;
-            else if(s.charAt(i)=='-')
+            else if(c=='-')
                 co=-1;
-            else if(s.charAt(i)=='('){
+            else if(c=='('){
                 sta.push(res);
                 sta.push(co);
                 res=0;
                 co=1;
             }
-            else if(s.charAt(i)==')'){
-                res=res*sta.pop();
-                res+=sta.pop();
+            else if(c==')'){
+                res=res*sta.pop()+sta.pop();
             }
         }
         return res;
