60709e20f8f97fcf315c4b8c38b19291a0269fff
jiangran910126 <jiangran0126@gmail.com>
2016-08-19 02:05:59 -0500

    [297. Serialize and Deserialize Binary Tree][Accepted]

--- 297-Serialize-and-Deserialize-Binary-Tree/solution.java
+++ 297-Serialize-and-Deserialize-Binary-Tree/solution.java
@@ -8,38 +8,37 @@
  * }
  */
 public class Codec {
+
     // Encodes a tree to a single string.
     public String serialize(TreeNode root) {
-       StringBuilder sb=new StringBuilder();
-       shelper(root,sb);
-       return sb.toString();
+        StringBuilder sb=new StringBuilder();
+	       shelper(root,sb);
+	       return sb.toString();
     }
     public void shelper(TreeNode root,StringBuilder sb){
-        if(root==null){
-            sb.append("null ");
-            return;
-        }
-        sb.append(String.valueOf(root.val));
-        sb.append(" ");
-        shelper(root.left,sb);
-        shelper(root.right,sb);
-    }
+	        if(root==null){
+	            sb.append("null ");
+	            return;
+	        }
+	        sb.append(String.valueOf(root.val));
+	        sb.append(" ");
+	        shelper(root.left,sb);
+	        shelper(root.right,sb);
+	    }
     // Decodes your encoded data to tree.
     public TreeNode deserialize(String data) {
         String[] spdata=data.split(" ");
-        int[] index=new int[1];
-        return dhelper(spdata,index);
+	        int[] index=new int[1];
+	        return dhelper(spdata,index);
     }
-    public TreeNode dhelper(String[] data,int[] i){
-        String s=data[i[0]++];
-        if(s.equals("null")) return null;
-        TreeNode node=new TreeNode(Integer.valueOf(s));
-        node.left=dhelper(data,i);
-        node.right=dhelper(data,i);
-        return node;
-    }
-    
-    
+     public TreeNode dhelper(String[] data,int[] i){
+	        String s=data[i[0]++];
+	        if(s.equals("null")) return null;
+	        TreeNode node=new TreeNode(Integer.valueOf(s));
+	        node.left=dhelper(data,i);
+	        node.right=dhelper(data,i);
+	        return node;
+	    }
 }
 
 // Your Codec object will be instantiated and called as such:
