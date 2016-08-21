b4377d1525632dfcff23e68ef852b8cd9e50369b
jiangran910126 <jiangran0126@gmail.com>
2016-08-20 20:43:46 -0500

    [372. Super Pow][Accepted]

--- 372-Super-Pow/solution.java
+++ 372-Super-Pow/solution.java
@@ -1,22 +1,13 @@
 public class Solution {
-    private static int M=1337;
     public int superPow(int a, int[] b) {
-        a%=M;
         int res=1;
-        for(int i=b.length-1;i>=0;i--){
-            res=res*pow(a,b[i])%M;
-            a=pow(a,10);
-        }
+        for(int i=0;i<b.length;i++)
+            res=pow(res,10)*pow(a,b[i])%1337;
         return res;
     }
     public int pow(int a,int b){
-        int res=1;
-        while(b!=0){
-            if(b%2!=0)
-                res=res*a%M;
-            a=a*a%M;
-            b/=2;
-        }
-        return res;
+        if(b==0) return 1;
+        if(b==1) return a%1337;
+        return pow(a%1337,b/2)*pow(a%1337,b-b/2)%1337;
     }
 }
