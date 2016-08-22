2f44208ef6f841ca0421023a2a5891deef20cd79
jiangran910126 <jiangran0126@gmail.com>
2016-08-22 16:06:59 -0500

    [336. Palindrome Pairs][Accepted]

--- 336-Palindrome-Pairs/solution.java
+++ 336-Palindrome-Pairs/solution.java
@@ -1,9 +1,9 @@
 class TrieNode{
-    TrieNode[] next;
+    TrieNode[] child;
     int index;
     List<Integer> list;
     public TrieNode(){
-        next=new TrieNode[26];
+        child=new TrieNode[26];
         index=-1;
         list=new ArrayList<>();
     }
@@ -11,6 +11,7 @@ class TrieNode{
 public class Solution {
     public List<List<Integer>> palindromePairs(String[] words) {
         List<List<Integer>> res=new ArrayList<>();
+        if(words.length==0) return res;
         TrieNode root=new TrieNode();
         for(int i=0;i<words.length;i++)
             addNode(words,i,root);
@@ -18,35 +19,35 @@ public class Solution {
             searchNode(words,i,root,res);
         return res;
     }
-    public void addNode(String[] words,int i,TrieNode root){
-        for(int j=words[i].length()-1;j>=0;j--){
-            int index=words[i].charAt(j)-'a';
-            if(root.next[index]==null)
-                root.next[index]=new TrieNode();
-            if(isPanlindrome(words[i],0,j))
-                root.list.add(i);
-            root=root.next[index];
+    public void addNode(String[] words,int index,TrieNode root){
+        for(int i=words[index].length()-1;i>=0;i--){
+            int idx=words[index].charAt(i)-'a';
+            if(root.child[idx]==null)
+                root.child[idx]=new TrieNode();
+            if(isPanlindrome(words[index],0,i))
+                root.list.add(index);
+            root=root.child[idx];
         }
-        root.index=i;
-        root.list.add(i);
-    }
-    public boolean isPanlindrome(String word,int i,int j){
-        while(i<=j){
-            if(word.charAt(i++)!=word.charAt(j--))
-                return false;
-        }
-        return true;
+        root.index=index;
+        root.list.add(index);
     }
     public void searchNode(String[] words,int index,TrieNode root,List<List<Integer>> res){
         for(int i=0;i<words[index].length();i++){
             if(root.index>=0&&root.index!=index&&isPanlindrome(words[index],i,words[index].length()-1))
                 res.add(Arrays.asList(index,root.index));
-            root=root.next[words[index].charAt(i)-'a'];
+            root=root.child[words[index].charAt(i)-'a'];
             if(root==null) return;
         }
         for(int i:root.list){
             if(i==index) continue;
             res.add(Arrays.asList(index,i));
         }
+    }
+    public boolean isPanlindrome(String s,int i,int j){
+        while(i<=j){
+            if(s.charAt(i++)!=s.charAt(j--))
+                return false;
+        }
+        return true;
     }
 }
