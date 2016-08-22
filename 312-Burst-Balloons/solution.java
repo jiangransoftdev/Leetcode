29fe3077f2c96ae1c0aad505e2e4c50ed506f2ca
jiangran910126 <jiangran0126@gmail.com>
2016-08-22 15:03:13 -0500

    [312. Burst Balloons][Accepted]

--- 312-Burst-Balloons/solution.java
+++ 312-Burst-Balloons/solution.java
@@ -2,14 +2,14 @@ public class Solution {
     public int maxCoins(int[] nums) {
         if(nums.length==0) return 0;
         int[][] dp=new int[nums.length][nums.length];
-        for(int len=1;len<=nums.length;len++){
-            for(int start=0;start+len<=nums.length;start++){
-                int end=start+len-1;
-                for(int i=start;i<=end;i++){
-                    int count=nums[i]*(start==0?1:nums[start-1])*(end==nums.length-1?1:nums[end+1]);
-                    count+=(i==start)?0:dp[start][i-1];
-                    count+=(i==end)?0:dp[i+1][end];
-                    dp[start][end]=Math.max(count,dp[start][end]);
+        for(int k=1;k<=nums.length;k++){
+            for(int i=0;i+k<=nums.length;i++){
+                int start=i,end=i+k-1;
+                for(int j=start;j<=end;j++){
+                    int count=nums[j]*(start-1==-1?1:nums[start-1])*(end+1==nums.length?1:nums[end+1]);
+                    count+=(start==j)?0:dp[start][j-1];
+                    count+=(end==j)?0:dp[j+1][end];
+                    dp[start][end]=Math.max(dp[start][end],count);
                 }
             }
         }
