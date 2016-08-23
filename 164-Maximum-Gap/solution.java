7ee700c15dc862b1c2ee4b04cb78ed5712ee1242
jiangran910126 <jiangran0126@gmail.com>
2016-08-23 15:25:14 -0500

    [164. Maximum Gap][Accepted]

--- 164-Maximum-Gap/solution.java
+++ 164-Maximum-Gap/solution.java
@@ -1,16 +1,16 @@
 public class Solution {
     public int maximumGap(int[] nums) {
         if(nums.length<2) return 0;
-        int m=Integer.MIN_VALUE,max=0;
+        int max=Integer.MIN_VALUE,res=Integer.MIN_VALUE;
         for(int i:nums)
-            m=Math.max(i,m);
+            max=Math.max(max,i);
         int[] another=new int[nums.length];
         int exp=1;
-        while(m/exp>0){
+        while(max/exp>0){
             int[] count=new int[10];
-            for(int i:nums)
-                count[(i/exp)%10]++;
-            for(int i=1;i<count.length;i++)
+            for(int i=0;i<nums.length;i++)
+                count[(nums[i]/exp)%10]++;
+            for(int i=1;i<10;i++)
                 count[i]+=count[i-1];
             for(int i=nums.length-1;i>=0;i--)
                 another[--count[(nums[i]/exp)%10]]=nums[i];
@@ -19,7 +19,7 @@ public class Solution {
             exp*=10;
         }
         for(int i=1;i<nums.length;i++)
-            max=Math.max(nums[i]-nums[i-1],max);
-        return max;
-    }   
+            res=Math.max(res,nums[i]-nums[i-1]);
+        return res;
+    }
 }
