56b82c8c61514e3d244273def17b97778d800b6a
jiangran910126 <jiangran0126@gmail.com>
2016-08-23 16:47:57 -0500

    [239. Sliding Window Maximum][Accepted]

--- 239-Sliding-Window-Maximum/solution.java
+++ 239-Sliding-Window-Maximum/solution.java
@@ -5,7 +5,7 @@ public class Solution {
         int[] res=new int[n-k+1];
         Deque<Integer> q=new LinkedList<>();
         for(int i=0;i<n;i++){
-            while(!q.isEmpty()&&q.peek()<i-k+1) q.poll();
+            while(!q.isEmpty()&&q.peek()==i-k) q.poll();
             while(!q.isEmpty()&&nums[q.peekLast()]<nums[i]) q.pollLast();
             q.offer(i);
             if(i>=k-1)
