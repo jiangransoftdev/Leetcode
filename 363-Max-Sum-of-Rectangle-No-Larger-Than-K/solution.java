d721fb5d820489347a0e7af722741b8fd350bb9a
jiangran910126 <jiangran0126@gmail.com>
2016-08-23 14:48:56 -0500

    [363. Max Sum of Rectangle No Larger Than K][Accepted]

--- 363-Max-Sum-of-Rectangle-No-Larger-Than-K/solution.java
+++ 363-Max-Sum-of-Rectangle-No-Larger-Than-K/solution.java
@@ -2,25 +2,25 @@ public class Solution {
     public int maxSumSubmatrix(int[][] matrix, int k) {
         if(matrix.length==0) return 0;
         int m=matrix.length,n=matrix[0].length;
+        int res=Integer.MIN_VALUE;
         int M=Math.max(m,n),N=Math.min(m,n);
-        int ans=Integer.MIN_VALUE;
         for(int i=0;i<N;i++){
             int[] sum=new int[M];
             for(int j=i;j<N;j++){
                 for(int z=0;z<M;z++)
                     sum[z]+=(m>n)?matrix[z][j]:matrix[j][z];
-                TreeSet<Integer> set=new TreeSet<>();
-                set.add(0);
+                TreeSet<Integer> tree=new TreeSet<>();
+                tree.add(0);
                 int num=0;
                 for(int z=0;z<M;z++){
                     num+=sum[z];
-                    Integer x=set.ceiling(num-k);
-                    if(x!=null)
-                        ans=Math.max(ans,num-x);
-                    set.add(num);
+                    Integer x=tree.ceiling(num-k);
+                    if(x!=null) 
+                        res=Math.max(res,num-x);
+                    tree.add(num);
                 }
             }
         }
-        return ans;
+        return res;
     }
 }
