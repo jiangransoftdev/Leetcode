fe71740be35ff7d255a5e78c82dc2b2bf35ebd21
jiangran910126 <jiangran0126@gmail.com>
2016-08-22 23:27:19 -0500

    [188. Best Time to Buy and Sell Stock IV][Accepted]

--- 188-Best-Time-to-Buy-and-Sell-Stock-IV/solution.java
+++ 188-Best-Time-to-Buy-and-Sell-Stock-IV/solution.java
@@ -11,8 +11,8 @@ public class Solution {
         int[][] buy=new int[prices.length+1][k+1];
         int[][] sell=new int[prices.length+1][k+1];
         for(int i=1;i<=k;i++) buy[0][i]=Integer.MIN_VALUE;
-        for(int i=1;i<=prices.length;i++){
-            for(int j=1;j<=k;j++){
+        for(int j=1;j<=k;j++){
+            for(int i=1;i<=prices.length;i++){
                 buy[i][j]=Math.max(buy[i-1][j],sell[i-1][j-1]-prices[i-1]);
                 sell[i][j]=Math.max(sell[i-1][j],buy[i-1][j]+prices[i-1]);
             }
