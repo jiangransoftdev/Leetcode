d34ea9e8771eb96997961661cacbf322a6a02209
jiangran910126 <jiangran0126@gmail.com>
2016-08-23 22:12:55 -0500

    [56. Merge Intervals][Accepted]

--- 56-Merge-Intervals/solution.java
+++ 56-Merge-Intervals/solution.java
@@ -9,25 +9,25 @@
  */
 public class Solution {
     public List<Interval> merge(List<Interval> intervals) {
-        Collections.sort(intervals, new Comparator<Interval>(){
-        	public int compare(Interval a,Interval b){
-        		return a.start-b.start;
-        	}
-        });
         List<Interval> res=new ArrayList<>();
         if(intervals.size()==0) return res;
-        int start=intervals.get(0).start;
-        int end=intervals.get(0).end;
-        for(int i=1;i<intervals.size();i++){
-            int tstart=intervals.get(i).start;
-            int tend=intervals.get(i).end;
-            if(tstart>end){
-                res.add(new Interval(start,end));
-                start=tstart;
-                end=tend;
+        Collections.sort(intervals,new Comparator<Interval>(){
+            public int compare(Interval a,Interval b){
+                if(a.start!=b.start) return a.start-b.start;
+                return a.end-b.end;
             }
-            else if(tstart<=end&&tend>end){
-                end=tend;
+        });
+        int start=intervals.get(0).start,end=intervals.get(0).end;
+        for(int i=1;i<intervals.size();i++){
+            int tmps=intervals.get(i).start;
+            int tmpe=intervals.get(i).end;
+            if(tmps<=end&&tmpe>end){
+                end=tmpe;
+            }
+            else if(tmps>end){
+                res.add(new Interval(start,end));
+                start=tmps;
+                end=tmpe;
             }
         }
         res.add(new Interval(start,end));
