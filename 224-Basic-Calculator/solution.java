0ab86a2abed9c0eafef34ef4adedb581b6b2f138
jiangran910126 <jiangran0126@gmail.com>
2016-08-24 16:08:53 -0500

    [224. Basic Calculator][Accepted]

--- 224-Basic-Calculator/solution.java
+++ 224-Basic-Calculator/solution.java
@@ -1,28 +1,28 @@
 public class Solution {
     public int calculate(String s) {
-        int res=0,num=0,co=1;
+        if(s.length()==0) return 0;
         Stack<Integer> sta=new Stack<>();
+        int co=1,res=0,num=0;
         for(int i=0;i<s.length();i++){
-            char c=s.charAt(i);
-            if(c>='0'&&c<='9'){
-                num=c-'0';
+            if(s.charAt(i)>='0'&&s.charAt(i)<='9'){
+                num=s.charAt(i)-'0';
                 while(i+1<s.length()&&s.charAt(i+1)>='0'&&s.charAt(i+1)<='9'){
                     num=num*10+(s.charAt(i+1)-'0');
                     i++;
                 }
                 res+=num*co;
             }
-            else if(c=='+')
+            else if(s.charAt(i)=='+')
                 co=1;
-            else if(c=='-')
+            else if(s.charAt(i)=='-')
                 co=-1;
-            else if(c=='('){
+            else if(s.charAt(i)=='('){
                 sta.push(res);
                 sta.push(co);
                 res=0;
                 co=1;
             }
-            else if(c==')'){
+            else if(s.charAt(i)==')'){
                 res=res*sta.pop()+sta.pop();
             }
         }
