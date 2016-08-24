0286adc8d95434ecb3ddac511c36d05979f4f811
jiangran910126 <jiangran0126@gmail.com>
2016-08-23 23:44:29 -0500

    [174. Dungeon Game][Accepted]

--- 174-Dungeon-Game/solution.java
+++ 174-Dungeon-Game/solution.java
@@ -2,8 +2,7 @@ public class Solution {
     public int calculateMinimumHP(int[][] dungeon) {
         int m=dungeon.length,n=dungeon[0].length;
         int[][] dp=new int[m][n];
-        if(dungeon[m-1][n-1]<0)
-            dp[m-1][n-1]=1-dungeon[m-1][n-1];
+        if(dungeon[m-1][n-1]<0) dp[m-1][n-1]=1-dungeon[m-1][n-1];
         else dp[m-1][n-1]=1;
         for(int i=m-2;i>=0;i--)
             dp[i][n-1]=Math.max(dp[i+1][n-1]-dungeon[i][n-1],1);
@@ -11,7 +10,7 @@ public class Solution {
             dp[m-1][i]=Math.max(dp[m-1][i+1]-dungeon[m-1][i],1);
         for(int i=m-2;i>=0;i--){
             for(int j=n-2;j>=0;j--){
-                dp[i][j]=Math.max(Math.min(dp[i+1][j]-dungeon[i][j],dp[i][j+1]-dungeon[i][j]),1);
+                dp[i][j]=Math.max(Math.min(dp[i+1][j],dp[i][j+1])-dungeon[i][j],1);
             }
         }
         return dp[0][0];
