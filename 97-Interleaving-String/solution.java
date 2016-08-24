98be8915f9c015e53d6e38cd37c5ab767470ab9d
jiangran910126 <jiangran0126@gmail.com>
2016-08-24 17:34:14 -0500

    [97. Interleaving String][Accepted]

--- 97-Interleaving-String/solution.java
+++ 97-Interleaving-String/solution.java
@@ -4,20 +4,15 @@ public class Solution {
         if(m+n!=l) return false;
         boolean[][] dp=new boolean[m+1][n+1];
         dp[0][0]=true;
-        for(int i=1;i<=m;i++){
+        for(int i=1;i<=m;i++)
             if(s1.charAt(i-1)==s3.charAt(i-1))
                 dp[i][0]=dp[i-1][0];
-        }
-        for(int i=1;i<=n;i++){
+        for(int i=1;i<=n;i++)
             if(s2.charAt(i-1)==s3.charAt(i-1))
                 dp[0][i]=dp[0][i-1];
-        }
-        for(int i=1;i<=m;i++){
-            for(int j=1;j<=n;j++){
-                if(s1.charAt(i-1)==s3.charAt(i+j-1))
-                dp[i][j]|=dp[i-1][j];
-                 if(s2.charAt(j-1)==s3.charAt(i+j-1))
-                dp[i][j]|=dp[i][j-1];
+        for(int i=1;i<=s1.length();i++){
+            for(int j=1;j<=s2.length();j++){
+                dp[i][j]=((s1.charAt(i-1)==s3.charAt(i+j-1))&&dp[i-1][j])||((s2.charAt(j-1)==s3.charAt(i+j-1))&&dp[i][j-1]);
             }
         }
         return dp[m][n];
