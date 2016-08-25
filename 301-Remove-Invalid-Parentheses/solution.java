a96b126c435066f7dd2b65dfb6f8d86697ba8bc8
jiangran910126 <jiangran0126@gmail.com>
2016-08-24 23:28:11 -0500

    [301. Remove Invalid Parentheses][Accepted]

--- 301-Remove-Invalid-Parentheses/solution.java
+++ 301-Remove-Invalid-Parentheses/solution.java
@@ -1,24 +1,23 @@
 public class Solution {
     public List<String> removeInvalidParentheses(String s) {
         List<String> res=new ArrayList<>();
-        helper(s,res,0,0,new char[]{'(',')'});
+        helper(s,0,0,new char[]{'(',')'},res);
         return res;
     }
-    public void helper(String s,List<String> res,int lasti,int lastj,char[] pair){
-        for(int stack=0,i=lasti;i<s.length();i++){
+    public void helper(String s,int lasti,int lastj,char[] pair,List<String> res){
+        for(int i=lasti,stack=0;i<s.length();i++){
             if(s.charAt(i)==pair[0]) stack++;
             if(s.charAt(i)==pair[1]) stack--;
             if(stack>=0) continue;
-            for(int j=lastj;j<s.length();j++){
+            for(int j=lastj;j<=i;j++){
                 if(s.charAt(j)==pair[1]&&(j==lastj||s.charAt(j-1)!=pair[1]))
-                    helper(s.substring(0,j)+s.substring(j+1),res,i,j,pair);
+                    helper(s.substring(0,j)+s.substring(j+1),i,j,pair,res);
             }
             return;
         }
-        String reversed=new StringBuilder(s).reverse().toString();
+        String reverse=new StringBuilder(s).reverse().toString();
         if(pair[0]=='(')
-            helper(reversed,res,0,0,new char[]{')','('});
-        else
-            res.add(reversed);
+            helper(reverse,0,0,new char[]{')','('},res);
+        else res.add(reverse);
     }
 }
