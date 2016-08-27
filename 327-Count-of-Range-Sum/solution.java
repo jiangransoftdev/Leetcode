919e59e889c4d7fa9a733bb3b5f197e64784042d
jiangran910126 <jiangran0126@gmail.com>
2016-08-27 03:05:54 -0500

    [327. Count of Range Sum][Accepted]

--- 327-Count-of-Range-Sum/solution.java
+++ 327-Count-of-Range-Sum/solution.java
@@ -3,19 +3,19 @@ public class Solution {
         int n=nums.length;
         long[] sum=new long[n+1];
         for(int i=0;i<n;i++)
-            sum[i+1]=nums[i]+sum[i];
-        return countWhileMergeSort(sum,0,n+1,lower,upper);
+            sum[i+1]=sum[i]+nums[i];
+        return helper(sum,0,n+1,lower,upper);
     }
-    public int countWhileMergeSort(long[] sum,int start,int end,int lower,int upper){
+    public int helper(long[] sum,int start,int end,int lower,int upper){
         if(end-start<=1) return 0;
         int mid=start+(end-start)/2;
-        int count=countWhileMergeSort(sum,start,mid,lower,upper)+countWhileMergeSort(sum,mid,end,lower,upper);
+        int count=helper(sum,start,mid,lower,upper)+helper(sum,mid,end,lower,upper);
         int j=mid,k=mid,t=mid;
         long[] cache=new long[end-start];
         for(int i=start,r=0;i<mid;i++,r++){
-            while(j<end&&sum[j]-sum[i]<lower)j++;
-            while(k<end&&sum[k]-sum[i]<=upper)k++;
-            while(t<end&&sum[t]<sum[i])cache[r++]=sum[t++];
+            while(j<end&&sum[j]-sum[i]<lower) j++;
+            while(k<end&&sum[k]-sum[i]<=upper) k++;
+            while(t<end&&sum[t]<sum[i]) cache[r++]=sum[t++];
             cache[r]=sum[i];
             count+=k-j;
         }
