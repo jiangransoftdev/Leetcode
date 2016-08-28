6e275311203087331a0c52ddd4bbbac998f8084b
jiangran910126 <jiangran0126@gmail.com>
2016-08-27 23:05:54 -0500

    [135. Candy][Accepted]

--- 135-Candy/solution.java
+++ 135-Candy/solution.java
@@ -2,31 +2,25 @@ public class Solution {
     public int candy(int[] ratings) {
         if(ratings.length==0) return 0;
         int n=ratings.length;
-        int total=n,increase=1,index=0;
-        while(index<n){
-            int i=index;
-            if(i+1==n) break;
-            int count=1;
-            if(i+1<n&&ratings[i+1]<ratings[i]){
-                while(i+1<n&&ratings[i+1]<ratings[i]){
+        int total=n,i=0;
+        while(i<n){
+            if(i+1<n&&ratings[i]!=ratings[i+1]){
+                int increase=1;
+                while(i+1<n&&ratings[i]<ratings[i+1]){
+                    total+=increase;
+                    increase++;
                     i++;
-                    count++;
                 }
-                if(increase<count)
-                    total+=count-increase;
-                increase=1;
-                total+=(count-2)*(count-1)/2;
-                index=i;
+                int decrease=1;
+                while(i+1<n&&ratings[i]>ratings[i+1]){
+                    decrease++;
+                    i++;
+                }
+                if(decrease>increase) total+=decrease-increase;
+                total+=(decrease-2)*(decrease-1)/2;
             }
-            else if(i+1<n&&ratings[i+1]>ratings[i]){
-                total+=increase;
-                increase++;
-                index++;
-            }
-            else if(i+1<n&&ratings[i+1]==ratings[i]){
-                increase=1;
-                index++;
-            }
+            else
+                i++;
         }
         return total;
     }
