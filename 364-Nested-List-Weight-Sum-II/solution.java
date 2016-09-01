09156b0d5402b038b44a05397efaf64f270cecb9
jiangran910126 <jiangran0126@gmail.com>
2016-09-01 01:07:52 -0500

    [364. Nested List Weight Sum II][Accepted]

---
+++ 364-Nested-List-Weight-Sum-II/solution.java
@@ -0,0 +1,45 @@
+/**
+ * // This is the interface that allows for creating nested lists.
+ * // You should not implement it, or speculate about its implementation
+ * public interface NestedInteger {
+ *     // Constructor initializes an empty nested list.
+ *     public NestedInteger();
+ *
+ *     // Constructor initializes a single integer.
+ *     public NestedInteger(int value);
+ *
+ *     // @return true if this NestedInteger holds a single integer, rather than a nested list.
+ *     public boolean isInteger();
+ *
+ *     // @return the single integer that this NestedInteger holds, if it holds a single integer
+ *     // Return null if this NestedInteger holds a nested list
+ *     public Integer getInteger();
+ *
+ *     // Set this NestedInteger to hold a single integer.
+ *     public void setInteger(int value);
+ *
+ *     // Set this NestedInteger to hold a nested list and adds a nested integer to it.
+ *     public void add(NestedInteger ni);
+ *
+ *     // @return the nested list that this NestedInteger holds, if it holds a nested list
+ *     // Return null if this NestedInteger holds a single integer
+ *     public List<NestedInteger> getList();
+ * }
+ */
+public class Solution {
+    public int depthSumInverse(List<NestedInteger> nestedList) {
+        return helper(nestedList,0);
+    }
+    public int helper(List<NestedInteger> nestedList,int sum){
+        List<NestedInteger> list=new ArrayList<>();
+        int res=0;
+        for(NestedInteger i:nestedList){
+            if(i.isInteger())
+                sum+=i.getInteger();
+            else
+                list.addAll(i.getList());
+        }
+        res=list.isEmpty()?0:helper(list,sum);
+        return res+sum;
+    }
+}
