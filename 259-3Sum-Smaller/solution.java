0d094095111305de7563b396d1d3cf5dec5034c2
jiangran910126 <jiangran0126@gmail.com>
2016-08-31 22:18:29 -0500

    [259. 3Sum Smaller][Accepted]

---
+++ 259-3Sum-Smaller/solution.java
@@ -0,0 +1,21 @@
+public class Solution {
+    public int threeSumSmaller(int[] nums, int target) {
+        int count = 0;
+        Arrays.sort(nums);
+        int len = nums.length;
+    
+        for(int i=0; i<len-2; i++) {
+            int left = i+1, right = len-1;
+            while(left < right) {
+                if(nums[i] + nums[left] + nums[right] < target) {
+                    count += right-left;
+                    left++;
+                } else {
+                    right--;
+                }
+            }
+        }
+        
+        return count;
+    }
+}
