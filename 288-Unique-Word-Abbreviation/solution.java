cffa7956a37a3fa67d3f35d8ffe3f583b7ddc9da
jiangran910126 <jiangran0126@gmail.com>
2016-09-01 21:23:34 -0500

    [288. Unique Word Abbreviation][Accepted]

---
+++ 288-Unique-Word-Abbreviation/solution.java
@@ -0,0 +1,33 @@
+public class ValidWordAbbr {
+    HashMap<String, String> map;
+    public ValidWordAbbr(String[] dictionary) {
+        map = new HashMap<String, String>();
+        for(String str:dictionary){
+            String key = getKey(str);
+            // If there is more than one string belong to the same key
+            // then the key will be invalid, we set the value to ""
+            if(map.containsKey(key)){
+                if(!map.get(key).equals(str)){
+                    map.put(key, "");
+                }
+            }
+            else{
+                map.put(key, str);
+            }
+        }
+    }
+
+    public boolean isUnique(String word) {
+        return !map.containsKey(getKey(word))||map.get(getKey(word)).equals(word);
+    }
+    public String getKey(String str){
+        if(str.length()<=2) return str;
+        return str.charAt(0)+Integer.toString(str.length()-2)+str.charAt(str.length()-1);
+    }
+}
+
+
+// Your ValidWordAbbr object will be instantiated and called as such:
+// ValidWordAbbr vwa = new ValidWordAbbr(dictionary);
+// vwa.isUnique("Word");
+// vwa.isUnique("anotherWord");
