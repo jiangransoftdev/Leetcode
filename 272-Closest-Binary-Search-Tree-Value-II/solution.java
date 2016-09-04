78fe63bf07bc4e24e21fe3dee55d344d6652295d
jiangran910126 <jiangran0126@gmail.com>
2016-09-04 15:31:59 -0500

    [272. Closest Binary Search Tree Value II][Accepted]

---
+++ 272-Closest-Binary-Search-Tree-Value-II/solution.java
@@ -0,0 +1,35 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+public class Solution {
+    public List<Integer> closestKValues(TreeNode root, double target, int k) {
+        List<Integer> res=new ArrayList<>();
+        Stack<Integer> s1=new Stack<>();
+        Stack<Integer> s2=new Stack<>();
+        inorder(root,target,false,s1);
+        inorder(root,target,true,s2);
+        while(k-->0){
+            if(s1.isEmpty())
+                res.add(s2.pop());
+            else if(s2.isEmpty())
+                res.add(s1.pop());
+            else if(Math.abs(target-s1.peek())<Math.abs(target-s2.peek()))
+                res.add(s1.pop());
+            else res.add(s2.pop());
+        }
+        return res;
+    }
+    public void inorder(TreeNode root,double target,boolean reverse,Stack<Integer> s){
+        if(root==null) return;
+        inorder(reverse?root.right:root.left,target,reverse,s);
+        if((reverse&&root.val<=target)||(!reverse&&root.val>target)) return;
+        s.push(root.val);
+        inorder(reverse?root.left:root.right,target,reverse,s);
+    }
+}
