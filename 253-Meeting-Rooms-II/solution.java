73a883a54fc9e5b7865885fbc9e9d47d8db689d5
jiangran910126 <jiangran0126@gmail.com>
2016-09-06 15:17:21 -0500

    [253. Meeting Rooms II][Accepted]

--- 253-Meeting-Rooms-II/solution.java
+++ 253-Meeting-Rooms-II/solution.java
@@ -10,25 +10,20 @@
 public class Solution {
     public int minMeetingRooms(Interval[] intervals) {
         if(intervals.length==0) return 0;
-        Arrays.sort(intervals, new Comparator<Interval>(){
-        	public int compare(Interval a,Interval b){
-        		return a.start-b.start;
-        	}
-        });
-        PriorityQueue<Interval> pq=new PriorityQueue<>(new Comparator<Interval>(){
-            public int compare(Interval a,Interval b){
-                return a.end-b.end;
-            }
-        });
-        pq.offer(intervals[0]);
-        for(int i=1;i<intervals.length;i++){
-            Interval interval=pq.poll();
-            if(intervals[i].start>=interval.end)
-                interval.end=intervals[i].end;
-            else
-                pq.offer(intervals[i]);
-            pq.offer(interval);
+        int[] start=new int[intervals.length];
+        int[] end=new int[intervals.length];
+        for(int i=0;i<intervals.length;i++){
+            start[i]=intervals[i].start;
+            end[i]=intervals[i].end;
         }
-        return pq.size();
+        Arrays.sort(start);
+        Arrays.sort(end);
+        int endidx=0,room=0;
+        for(int i=0;i<intervals.length;i++){
+            if(start[i]<end[endidx])
+                room++;
+            else endidx++;
+        }
+        return room;
     }
 }
