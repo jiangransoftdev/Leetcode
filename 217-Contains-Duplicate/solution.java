a3ef68f4bba446839189d0458c01f2edbbc58638
jiangran910126 <jiangran0126@gmail.com>
2016-09-10 16:23:06 -0500

    [217. Contains Duplicate][Accepted]

--- 217-Contains-Duplicate/solution.java
+++ 217-Contains-Duplicate/solution.java
@@ -1,11 +1,9 @@
 public class Solution {
     public boolean containsDuplicate(int[] nums) {
-        if(nums.length==0) return false;
         Set<Integer> set=new HashSet<>();
         for(int num:nums){
-            if(!set.contains(num))
-                set.add(num);
-            else return true;
+            if(set.contains(num)) return true;
+            set.add(num);
         }
         return false;
     }
