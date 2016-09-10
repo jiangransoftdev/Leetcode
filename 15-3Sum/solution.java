34dfeb743c844e6b5a315f4090ffc6f23e2ba553
jiangran910126 <jiangran0126@gmail.com>
2016-09-10 14:36:25 -0500

    [15. 3Sum][Accepted]

--- 15-3Sum/solution.java
+++ 15-3Sum/solution.java
@@ -1,22 +1,25 @@
 public class Solution {
     public List<List<Integer>> threeSum(int[] nums) {
         List<List<Integer>> res=new ArrayList<>();
+        if(nums.length==0) return res;
         Arrays.sort(nums);
         for(int i=0;i<nums.length-2;i++){
             if(i>0&&nums[i]==nums[i-1]) continue;
-            int left=i+1,right=nums.length-1;
-            while(left<right){
-                int sum=nums[i]+nums[left]+nums[right];
-                if(sum==0){
-                    res.add(Arrays.asList(nums[i],nums[left],nums[right]));
-                    while(left<right&&nums[left]==nums[left+1]) left++;
-                    while(left<right&&nums[right]==nums[right-1]) right--;
-                    left++;
-                    right--;
+            int start=i+1;
+            int end=nums.length-1;
+            while(start<end){
+                if(nums[i]+nums[start]+nums[end]==0){
+                    res.add(Arrays.asList(nums[i],nums[start],nums[end]));
+                    while(start<end&&nums[start]==nums[start+1])
+                        start++;
+                    while(end>start&&nums[end]==nums[end-1])
+                        end--;
+                    start++;
+                    end--;
                 }
-                else if(sum>0)
-                    right--;
-                else left++;
+                else if(nums[i]+nums[start]+nums[end]>0)
+                    end--;
+                else start++;
             }
         }
         return res;
