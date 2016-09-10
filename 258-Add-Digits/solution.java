7d7a6ecfeb8fe40abfe014c2b2d922c19ca60dbb
jiangran910126 <jiangran0126@gmail.com>
2016-09-10 15:36:17 -0500

    [258. Add Digits][Accepted]

--- 258-Add-Digits/solution.java
+++ 258-Add-Digits/solution.java
@@ -1,6 +1,7 @@
 public class Solution {
     public int addDigits(int num) {
-        if(num==0) return 0;
-        return num%9==0?9:num%9;
+        if(num<=9) return num;
+        if(num%9==0) return 9;
+        return num%9;
     }
 }
