59bc2e530a43a3005ac1debd17c0629527f85677
jiangran910126 <jiangran0126@gmail.com>
2016-09-11 16:28:17 -0500

    [36. Valid Sudoku][Accepted]

--- 36-Valid-Sudoku/solution.java
+++ 36-Valid-Sudoku/solution.java
@@ -1,26 +1,24 @@
 public class Solution {
     public boolean isValidSudoku(char[][] board) {
-        if(board.length==0) return false;
         for(int i=0;i<9;i++){
             for(int j=0;j<9;j++){
                 if(board[i][j]=='.') continue;
-                if(!isValid(board,i,j))
-                    return false;
+                if(!isValid(board,i,j)) return false;
             }
         }
         return true;
     }
     public boolean isValid(char[][] board,int i,int j){
-        char c=board[i][j];
-        board[i][j]='#';
-        for(int a=0;a<9;a++)
-            if(board[a][j]==c) return false;
-        for(int a=0;a<9;a++)
-            if(board[i][a]==c) return false;
-        for(int a=(i/3)*3;a<(i/3+1)*3;a++)
-            for(int b=(j/3)*3;b<(j/3+1)*3;b++)
-                if(board[a][b]==c) return false;
-        board[i][j]=c;
+        for(int row=0;row<9;row++){
+            if(row!=i&&board[row][j]==board[i][j]) return false;
+        }
+        for(int col=0;col<9;col++){
+            if(col!=j&&board[i][col]==board[i][j]) return false;
+        }
+        for(int row=(i/3)*3;row<(i/3+1)*3;row++){
+            for(int col=(j/3)*3;col<(j/3+1)*3;col++)
+                if(row!=i&&col!=j&&board[i][j]==board[row][col]) return false;
+        }
         return true;
     }
 }
