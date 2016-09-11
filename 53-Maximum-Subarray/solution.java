3f274e5cf7db9f26ee79cf4bea13d8f6923730ee
jiangran910126 <jiangran0126@gmail.com>
2016-09-11 01:06:01 -0500

    [53. Maximum Subarray][Accepted]

--- 53-Maximum-Subarray/solution.java
+++ 53-Maximum-Subarray/solution.java
@@ -1,10 +1,11 @@
 public class Solution {
     public int maxSubArray(int[] nums) {
-        int res=Integer.MIN_VALUE,cur=0;
-        for(int i=0;i<nums.length;i++){
-            cur=Math.max(cur+nums[i],nums[i]);
-            res=Math.max(res,cur);
+        if(nums.length==0) return 0;
+        int max=Integer.MIN_VALUE,tmp=0;
+        for(int num:nums){
+            tmp=Math.max(tmp+num,num);
+            max=Math.max(max,tmp);
         }
-        return res;
+        return max;
     }
 }
