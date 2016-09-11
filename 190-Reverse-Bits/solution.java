5d9ae4c335dfff574589aa8ad9ed275277be4aa0
jiangran910126 <jiangran0126@gmail.com>
2016-09-11 18:48:39 -0500

    [190. Reverse Bits][Accepted]

--- 190-Reverse-Bits/solution.java
+++ 190-Reverse-Bits/solution.java
@@ -1,14 +1,15 @@
 public class Solution {
     // you need treat n as an unsigned value
     public int reverseBits(int n) {
-       for(int offset=0;offset<16;offset++){
-           int left=(n>>offset)&1;
-           int right=(n>>(31-offset))&1;
-           if(left!=right){
-               n^=(1<<offset);
-               n^=(1<<(31-offset));
-           }
-       }
-       return n;
+        int num=0;
+        for(int offset=0;offset<16;offset++){
+    	    int right=(n>>offset)&1;
+    	    int left=(n>>(31-offset))&1;
+    	    if(left!=right){
+    		    n^=(1<<offset);
+    		    n^=(1<<(31-offset));
+    	    }
+        }
+        return n;
     }
 }
