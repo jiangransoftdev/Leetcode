8c839ed7d6b315481bccfa5ea1d6cd35d5c5ac7a
jiangran910126 <jiangran0126@gmail.com>
2016-09-11 17:46:26 -0500

    [101. Symmetric Tree][Accepted]

--- 101-Symmetric-Tree/solution.java
+++ 101-Symmetric-Tree/solution.java
@@ -14,8 +14,7 @@ public class Solution {
     }
     public boolean helper(TreeNode p,TreeNode q){
         if(p==null&&q==null) return true;
-        else if(p!=null&&q==null) return false;
-        else if(p==null&&q!=null) return false;
+        else if(p==null||q==null) return false;
         else if(p.val!=q.val) return false;
         return helper(p.left,q.right)&&helper(p.right,q.left);
     }
