6119dd5b76c7bcfe3544729b8c701e3a0d239763
jiangran910126 <jiangran0126@gmail.com>
2016-09-11 17:41:59 -0500

    [238. Product of Array Except Self][Accepted]

--- 238-Product-of-Array-Except-Self/solution.java
+++ 238-Product-of-Array-Except-Self/solution.java
@@ -3,14 +3,13 @@ public class Solution {
         if(nums.length==0) return new int[0];
         int[] res=new int[nums.length];
         int factor=1;
-        Arrays.fill(res,1);
         for(int i=0;i<nums.length;i++){
-            res[i]=res[i]*factor;
+            res[i]=factor;
             factor*=nums[i];
         }
         factor=1;
         for(int i=nums.length-1;i>=0;i--){
-            res[i]=res[i]*factor;
+            res[i]*=factor;
             factor*=nums[i];
         }
         return res;
