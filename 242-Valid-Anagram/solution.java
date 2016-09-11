31e93cda723150ee108432c076e8655f3a15941d
jiangran910126 <jiangran0126@gmail.com>
2016-09-11 02:34:58 -0500

    [242. Valid Anagram][Accepted]

--- 242-Valid-Anagram/solution.java
+++ 242-Valid-Anagram/solution.java
@@ -1,14 +1,12 @@
 public class Solution {
     public boolean isAnagram(String s, String t) {
         if(s.length()!=t.length()) return false;
-        int[] count=new int[26];
-        for(char c:s.toCharArray()){
-            count[c-'a']++;
-        }
-        for(char c:t.toCharArray()){
-            count[c-'a']--;
-            if(count[c-'a']<0)
-                return false;
+        int[] count=new int[256];
+        for(int i=0;i<s.length();i++)
+            count[s.charAt(i)]++;
+        for(int i=0;i<t.length();i++){
+            count[t.charAt(i)]--;
+            if(count[t.charAt(i)]<0) return false;
         }
         return true;
     }
