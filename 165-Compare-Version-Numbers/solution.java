0096814f3308ee797c393aa3f4cfc5c862e4aae3
jiangran910126 <jiangran0126@gmail.com>
2016-09-11 18:33:49 -0500

    [165. Compare Version Numbers][Accepted]

--- 165-Compare-Version-Numbers/solution.java
+++ 165-Compare-Version-Numbers/solution.java
@@ -1,13 +1,16 @@
 public class Solution {
     public int compareVersion(String version1, String version2) {
-        String[] s1=version1.split("\\.");
-        String[] s2=version2.split("\\.");
-        int i=0,j=0,a=0,b=0;
-        while(i<s1.length||j<s2.length){
-            a=i<s1.length?Integer.parseInt(s1[i++]):0;
-            b=j<s2.length?Integer.parseInt(s2[j++]):0;
-            if(a>b) return 1;
-            if(b>a) return -1;
+        String[] strv1=version1.split("\\.");
+        String[] strv2=version2.split("\\.");
+        int i=0;
+        int j=0;
+        int a,b;
+        while(i<strv1.length||j<strv2.length){
+            a=(i<strv1.length)?Integer.parseInt(strv1[i++]):0;
+                b=(j<strv2.length)?Integer.parseInt(strv2[j++]):0;
+            if(a==b) continue;
+            else if(a>b) return 1;
+            else return -1;
         }
         return 0;
     }
