a09b4a1299c8e842aa47dc3a28e7c7014b6db7ed
jiangran910126 <jiangran0126@gmail.com>
2016-09-11 18:08:57 -0500

    [116. Populating Next Right Pointers in Each Node][Accepted]

--- 116-Populating-Next-Right-Pointers-in-Each-Node/solution.java
+++ 116-Populating-Next-Right-Pointers-in-Each-Node/solution.java
@@ -10,9 +10,10 @@ public class Solution {
     public void connect(TreeLinkNode root) {
         if(root==null) return;
         if(root.left==null&&root.right==null) return;
-        root.left.next=root.right;
+        if(root.left!=null) root.left.next=root.right;
         if(root.next!=null) root.right.next=root.next.left;
         connect(root.left);
         connect(root.right);
     }
+    
 }
