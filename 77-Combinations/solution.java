8ec057c2be8f0b5f7c50a0b94a021e43b922bd2e
jiangran910126 <jiangran0126@gmail.com>
2016-09-11 18:55:36 -0500

    [77. Combinations][Accepted]

--- 77-Combinations/solution.java
+++ 77-Combinations/solution.java
@@ -1,18 +1,17 @@
 public class Solution {
     public List<List<Integer>> combine(int n, int k) {
         List<List<Integer>> res=new ArrayList<>();
-        if(k==0) return res;
-        helper(n,1,k,0,new ArrayList<>(),res);
+        helper(n,1,0,k,new ArrayList<>(),res);
         return res;
     }
-    public void helper(int n,int num,int k,int index,List<Integer> solu,List<List<Integer>> res){
-        if(index==k){
-            res.add(new ArrayList<>(solu));
+    public void helper(int n,int index,int level,int k,List<Integer> solu,List<List<Integer>> res){
+        if(level==k){
+            res.add(new ArrayList(solu));
             return;
         }
-        for(int i=num;i<=n;i++){
+        for(int i=index;i<=n;i++){
             solu.add(i);
-            helper(n,i+1,k,index+1,solu,res);
+            helper(n,i+1,level+1,k,solu,res);
             solu.remove(solu.size()-1);
         }
     }
