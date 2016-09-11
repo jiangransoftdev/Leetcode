3c0299c217a9f6492a120c68efea33dd8ccba654
jiangran910126 <jiangran0126@gmail.com>
2016-09-11 18:41:58 -0500

    [179. Largest Number][Accepted]

--- 179-Largest-Number/solution.java
+++ 179-Largest-Number/solution.java
@@ -1,20 +1,20 @@
 public class Solution {
     public String largestNumber(int[] nums) {
         if(nums.length==0) return "";
-        String[] strs=new String[nums.length];
+        String[] str=new String[nums.length];
         for(int i=0;i<nums.length;i++)
-            strs[i]=nums[i]+"";
-        Arrays.sort(strs,new Comparator<String>(){
-            public int compare(String s1,String s2){
-                String t1=s1+s2;
-                String t2=s2+s1;
+            str[i]=nums[i]+"";
+        Arrays.sort(str,new Comparator<String>(){
+            public int compare(String a,String b){
+                String t1=a+b;
+                String t2=b+a;
                 return t1.compareTo(t2);
             }
         });
-        if(strs[strs.length-1].charAt(0)=='0') return "0";
         StringBuilder sb=new StringBuilder();
-        for(String str:strs)
-            sb.insert(0,str);
+        if(str[str.length-1].charAt(0)=='0') return "0";
+        for(String s:str)
+            sb.insert(0,s);
         return sb.toString();
     }
 }
