1635941eddd66f0df31baf8653591b6f8e08ce7e
jiangran910126 <jiangran0126@gmail.com>
2016-09-11 19:06:23 -0500

    [39. Combination Sum][Accepted]

--- 39-Combination-Sum/solution.java
+++ 39-Combination-Sum/solution.java
@@ -6,16 +6,17 @@ public class Solution {
         return res;
     }
     public void helper(int[] candidates,int target,int index,List<Integer> solu,List<List<Integer>> res){
-        if(target<0) return;
         if(target==0){
-            res.add(new ArrayList<>(solu));
+            res.add(new ArrayList(solu));
             return;
         }
-        for(int i=index;i<candidates.length;i++){
-            if(target<candidates[i]) continue;
-            solu.add(candidates[i]);
-            helper(candidates,target-candidates[i],i,solu,res);
-            solu.remove(solu.size()-1);
+        else if(target>0){
+            for(int i=index;i<candidates.length;i++){
+                if(candidates[i]>target) continue;
+                solu.add(candidates[i]);
+                helper(candidates,target-candidates[i],i,solu,res);
+                solu.remove(solu.size()-1);
+            }
         }
     }
 }
