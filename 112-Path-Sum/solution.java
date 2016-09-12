fb265cf5677774b73f2e0bc91c1961ab7aa4ae11
jiangran910126 <jiangran0126@gmail.com>
2016-09-12 03:25:49 -0500

    [112. Path Sum][Accepted]

--- 112-Path-Sum/solution.java
+++ 112-Path-Sum/solution.java
@@ -8,12 +8,18 @@
  * }
  */
 public class Solution {
+    boolean res=false;
     public boolean hasPathSum(TreeNode root, int sum) {
-        if(root==null) return false;
+        helper(root,sum);
+        return res;
+    }
+    public void helper(TreeNode root,int sum){
+        if(root==null) return;
         if(root.left==null&&root.right==null){
-            if(sum==root.val) return true;
-            else return false;
+            if(root.val==sum) res=true;
+            return;
         }
-        return hasPathSum(root.left,sum-root.val)||hasPathSum(root.right,sum-root.val);
+        helper(root.left,sum-root.val);
+        helper(root.right,sum-root.val);
     }
 }
