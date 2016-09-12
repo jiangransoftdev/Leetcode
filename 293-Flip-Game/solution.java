d473a05f49c238cbba7ed13e0da80050dba18e5e
jiangran910126 <jiangran0126@gmail.com>
2016-09-12 15:39:53 -0500

    [293. Flip Game][Accepted]

--- 293-Flip-Game/solution.java
+++ 293-Flip-Game/solution.java
@@ -1,13 +1,11 @@
 public class Solution {
     public List<String> generatePossibleNextMoves(String s) {
         List<String> res=new ArrayList<>();
-        if(s.length()<2) return res;
-        for(int i=0;i+2<=s.length();i++){
-            String sub=s.substring(i,i+2);
-            if(sub.equals("++")){
-                StringBuilder sb=new StringBuilder();
-                sb.append(s.substring(0,i)).append("--").append(s.substring(i+2));
-                res.add(sb.toString());
+        if(s.length()==0) return res;
+        for(int i=0;i<s.length()-1;i++){
+            if(s.startsWith("++",i)){
+                String another=s.substring(0,i)+"--"+s.substring(i+2);
+                res.add(another);
             }
         }
         return res;
