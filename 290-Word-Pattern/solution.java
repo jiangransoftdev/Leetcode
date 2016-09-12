c73d333db3baa348bbce786e6f0d08f9782a23c0
jiangran910126 <jiangran0126@gmail.com>
2016-09-12 18:35:59 -0500

    [290. Word Pattern][Accepted]

--- 290-Word-Pattern/solution.java
+++ 290-Word-Pattern/solution.java
@@ -1,19 +1,17 @@
 public class Solution {
     public boolean wordPattern(String pattern, String str) {
-        String[] arr=str.split(" ");
-        if(pattern.length()!=arr.length) return false;
         Map<Character,String> map=new HashMap<>();
+        String[] strs=str.split(" ");
+        if(pattern.length()!=strs.length) return false;
         for(int i=0;i<pattern.length();i++){
-            char c=pattern.charAt(i);
-            if(map.containsKey(c)){
-                if(!map.get(c).equals(arr[i]))
-                    return false;
+            if(map.containsKey(pattern.charAt(i))){
+                if(!strs[i].equals(map.get(pattern.charAt(i)))) return false;
             }
             else{
-                if(map.containsValue(arr[i]))
-                    return false;
-                map.put(c,arr[i]);
+                if(map.containsValue(strs[i])) return false;
+                map.put(pattern.charAt(i),strs[i]);
             }
+            
         }
         return true;
     }
