331c3d69902679a88e77c15b112ad2949450197d
jiangran910126 <jiangran0126@gmail.com>
2016-09-12 15:53:01 -0500

    [243. Shortest Word Distance][Accepted]

--- 243-Shortest-Word-Distance/solution.java
+++ 243-Shortest-Word-Distance/solution.java
@@ -1,6 +1,6 @@
 public class Solution {
     public int shortestDistance(String[] words, String word1, String word2) {
-        int pos1=-1,pos2=-1,res=Integer.MAX_VALUE;
+         int pos1=-1,pos2=-1,res=Integer.MAX_VALUE;
         for(int i=0;i<words.length;i++){
             if(words[i].equals(word1)) pos1=i;
             if(words[i].equals(word2)) pos2=i;
