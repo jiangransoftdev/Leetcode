35ea38cd5c8208b0f559d6e3b537dbe87a00b4fb
jiangran910126 <jiangran0126@gmail.com>
2016-09-12 15:35:24 -0500

    [81. Search in Rotated Sorted Array II][Accepted]

--- 81-Search-in-Rotated-Sorted-Array-II/solution.java
+++ 81-Search-in-Rotated-Sorted-Array-II/solution.java
@@ -1,18 +1,21 @@
 public class Solution {
     public boolean search(int[] nums, int target) {
-        int s=0,e=nums.length-1;
-        while(s<=e){
-            int m=s+(e-s)/2;
-            if(target==nums[m]) return true;
-            if(nums[s]==nums[m]) s++;
-            else if(nums[s]<nums[m]){
-                if(target>=nums[s]&&target<nums[m]) e=m-1;
-                else s=m+1;
+        if(nums.length==0) return false;
+        int l=0,h=nums.length-1;
+        while(l<=h){
+            int m=l+(h-l)/2;
+            if(nums[m]==target) return true;
+            if(nums[l]<nums[m]){
+                if(target>=nums[l]&&target<nums[m])
+                    h=m-1;
+                else l=m+1;
             }
-            else{
-                if(target>nums[m]&&target<=nums[e]) s=m+1;
-                else e=m-1;
+            else if(nums[l]>nums[m]){
+                if(target>nums[m]&&target<=nums[h])
+                    l=m+1;
+                else h=m-1;
             }
+            else l++;
         }
         return false;
     }
