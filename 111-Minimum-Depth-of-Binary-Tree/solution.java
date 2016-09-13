483432ec553929721ecc94612e56fc565491fead
jiangran910126 <jiangran0126@gmail.com>
2016-09-13 14:44:13 -0500

    [111. Minimum Depth of Binary Tree][Accepted]

--- 111-Minimum-Depth-of-Binary-Tree/solution.java
+++ 111-Minimum-Depth-of-Binary-Tree/solution.java
@@ -10,11 +10,9 @@
 public class Solution {
     public int minDepth(TreeNode root) {
         if(root==null) return 0;
-        return helper(root);
-    }
-    public int helper(TreeNode root){
-        if(root==null) return Integer.MAX_VALUE;
         if(root.left==null&&root.right==null) return 1;
-        return 1+Math.min(helper(root.left),helper(root.right));
+        int left=minDepth(root.left);
+        int right=minDepth(root.right);
+        return 1+Math.min(left==0?Integer.MAX_VALUE:left,right==0?Integer.MAX_VALUE:right);
     }
 }
