84c0765e63edcd5670c8b603a4fa0a2a3d56f57b
jiangran910126 <jiangran0126@gmail.com>
2016-09-12 21:12:30 -0500

    [150. Evaluate Reverse Polish Notation][Accepted]

--- 150-Evaluate-Reverse-Polish-Notation/solution.java
+++ 150-Evaluate-Reverse-Polish-Notation/solution.java
@@ -1,29 +1,34 @@
 public class Solution {
     public int evalRPN(String[] tokens) {
-        if(tokens.length==0) return 0;
-        Stack<String> s=new Stack<>();
+        Stack<Integer> s=new Stack<>();
         int a=0,b=0;
-        for(String token:tokens){
-            switch(token){
-                case "+":a=Integer.parseInt(s.pop());
-                         b=Integer.parseInt(s.pop());
-                         s.push(a+b+"");
-                         break;
-                case "-":a=Integer.parseInt(s.pop());
-                         b=Integer.parseInt(s.pop());
-                         s.push(b-a+"");
-                         break;
-                case "*":a=Integer.parseInt(s.pop());
-                         b=Integer.parseInt(s.pop());
-                         s.push(a*b+"");
-                         break;
-                case "/":a=Integer.parseInt(s.pop());
-                         b=Integer.parseInt(s.pop());
-                         s.push(b/a+"");
-                         break;
-                default:s.push(token);
+        for(int i=0;i<tokens.length;i++){
+            String op=tokens[i];
+            switch(op){
+                case "+":
+                    a=s.pop();
+                    b=s.pop();
+                    s.push(a+b);
+                    break;
+                case "-":
+                    a=s.pop();
+                    b=s.pop();
+                    s.push(b-a);
+                    break;
+                case "*":
+                    a=s.pop();
+                    b=s.pop();
+                    s.push(a*b);
+                    break;
+                case "/":
+                    a=s.pop();
+                    b=s.pop();
+                    s.push(b/a);
+                    break;
+                default:
+                    s.push(Integer.parseInt(op));
             }
         }
-        return Integer.parseInt(s.peek());
+        return s.isEmpty()?0:s.peek();
     }
 }
