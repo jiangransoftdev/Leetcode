2592a4e393d552a965572534f498586fb82893f8
jiangran910126 <jiangran0126@gmail.com>
2016-09-13 17:26:59 -0500

    [35. Search Insert Position][Accepted]

--- 35-Search-Insert-Position/solution.java
+++ 35-Search-Insert-Position/solution.java
@@ -1,14 +1,12 @@
 public class Solution {
     public int searchInsert(int[] nums, int target) {
-        if(nums.length==0) return 0;
-        int low=0,high=nums.length-1;
-        while(low<high){
-            int mid=low+(high-low)/2;
-            if(nums[mid]==target) return mid;
-            else if(target<nums[mid])
-                high=mid;
-            else low=mid+1;
+        int l=0,h=nums.length-1;
+        while(l<=h){
+            int m=l+(h-l)/2;
+            if(nums[m]==target) return m;
+            else if(nums[m]<target) l=m+1;
+            else h=m-1;
         }
-        return nums[low]>=target?low:low+1;
+        return l;
     }
 }
