e9fec2f96f626b8680b40eb28d19f66c3d355c3f
jiangran910126 <jiangran0126@gmail.com>
2016-09-12 21:56:27 -0500

    [148. Sort List][Accepted]

--- 148-Sort-List/solution.java
+++ 148-Sort-List/solution.java
@@ -9,45 +9,45 @@
 public class Solution {
     ListNode node;
     public ListNode sortList(ListNode head) {
-        if(head==null||head.next==null) return head;
         node=head;
-        int count=0;
-        ListNode index=head;
-        while(index!=null){
-            count++;
-            index=index.next;
+        if(head==null||head.next==null) return head;
+        int size=0;
+        ListNode cur=head;
+        while(cur!=null){
+            size++;
+            cur=cur.next;
         }
-        return helper(0,count-1);
+        return helper(0,size-1);
     }
-    public ListNode helper(int s,int e){
-        if(s>e) return null;
-        if(s==e){
-            ListNode tmp=node;
+    public ListNode helper(int left,int right){
+        if(left>right) return null;
+        if(left==right){
+            ListNode cur=node;
             node=node.next;
-            tmp.next=null;
-            return tmp;
+            cur.next=null;
+            return cur;
         }
-        int m=s+(e-s)/2;
-        ListNode left=helper(s,m);
-        ListNode right=helper(m+1,e);
-        return combine(left,right);
+        int mid=left+(right-left)/2;
+        ListNode l=helper(left,mid);
+        ListNode r=helper(mid+1,right);
+        return combine(l,r);
     }
     public ListNode combine(ListNode left,ListNode right){
-        ListNode l=left,r=right;
-        ListNode dummy=new ListNode(0),index=dummy;
-        while(l!=null&&r!=null){
-            if(l.val<=r.val){
-                index.next=l;
-                l=l.next;
+        ListNode dummy=new ListNode(0);
+        ListNode index=dummy,index1=left,index2=right;
+        while(index1!=null&&index2!=null){
+            if(index1.val<=index2.val){
+                index.next=index1;
+                index1=index1.next;
             }
             else{
-                index.next=r;
-                r=r.next;
+                index.next=index2;
+                index2=index2.next;
             }
             index=index.next;
         }
-        if(l!=null) index.next=l;
-        if(r!=null) index.next=r;
+        if(index1!=null) index.next=index1;
+        if(index2!=null) index.next=index2;
         return dummy.next;
     }
 }
