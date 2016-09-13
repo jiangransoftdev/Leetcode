91483588277352cd246861b4ef3de23d6bc31328
jiangran910126 <jiangran0126@gmail.com>
2016-09-13 03:52:40 -0500

    [28. Implement strStr()][Accepted]

--- 28-Implement-strStr()/solution.java
+++ 28-Implement-strStr()/solution.java
@@ -1,9 +1,9 @@
 public class Solution {
     public int strStr(String haystack, String needle) {
-        int l=needle.length();
-        if(l==0) return 0;
-        for(int i=0;i+l<=haystack.length();i++){
-            String sub=haystack.substring(i,i+l);
+        int len=needle.length();
+        if(len==0) return 0;
+        for(int i=0;i+len<=haystack.length();i++){
+            String sub=haystack.substring(i,i+len);
             if(sub.equals(needle)) return i;
         }
         return -1;
