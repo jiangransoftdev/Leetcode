aeb28f02829c55332863312b5fbb17f480adb0a9
jiangran910126 <jiangran0126@gmail.com>
2016-09-13 14:56:42 -0500

    [360. Sort Transformed Array][Accepted]

--- 360-Sort-Transformed-Array/solution.java
+++ 360-Sort-Transformed-Array/solution.java
@@ -2,35 +2,24 @@ public class Solution {
     public int[] sortTransformedArray(int[] nums, int a, int b, int c) {
         int[] res=new int[nums.length];
         if(a>0){
-            int i=0,j=nums.length-1,index=res.length-1;
+            int i=0,j=nums.length-1,k=nums.length-1;
             while(i<=j){
-                int left=a*nums[i]*nums[i]+b*nums[i]+c;
-                int right=a*nums[j]*nums[j]+b*nums[j]+c;
-                if(left>=right){
-                    res[index--]=left;
-                    i++;
-                }
-                else{
-                    res[index--]=right;
-                    j--;
-                }
+                if(cal(nums[i],a,b,c)>=cal(nums[j],a,b,c))
+                    res[k--]=cal(nums[i++],a,b,c);
+                else res[k--]=cal(nums[j--],a,b,c);
             }
         }
         else{
-            int i=0,j=nums.length-1,index=0;
+            int i=0,j=nums.length-1,k=0;
             while(i<=j){
-                int left=a*nums[i]*nums[i]+b*nums[i]+c;
-                int right=a*nums[j]*nums[j]+b*nums[j]+c;
-                if(left<=right){
-                    res[index++]=left;
-                    i++;
-                }
-                else{
-                    res[index++]=right;
-                    j--;
-                }
+                if(cal(nums[i],a,b,c)<=cal(nums[j],a,b,c))
+                    res[k++]=cal(nums[i++],a,b,c);
+                else res[k++]=cal(nums[j--],a,b,c);
             }
         }
         return res;
+    }
+    public int cal(int x,int a,int b,int c){
+        return a*x*x+b*x+c;
     }
 }
