73b772dfd0cee580a12af5f97de9033f9df845e2
jiangran910126 <jiangran0126@gmail.com>
2016-09-13 15:02:56 -0500

    [268. Missing Number][Accepted]

--- 268-Missing-Number/solution.java
+++ 268-Missing-Number/solution.java
@@ -1,8 +1,9 @@
 public class Solution {
     public int missingNumber(int[] nums) {
-        int res=(0+nums.length)*(nums.length+1)/2;
-        for(int i:nums)
-            res-=i;
+        int n=nums.length;
+        int res=(n+1)*n/2;
+        for(int num:nums)
+            res-=num;
         return res;
     }
 }
