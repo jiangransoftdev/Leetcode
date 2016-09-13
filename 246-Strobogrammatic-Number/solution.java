6df45613ad34a048f72235290033005c0a1d99b2
jiangran910126 <jiangran0126@gmail.com>
2016-09-13 14:35:46 -0500

    [246. Strobogrammatic Number][Accepted]

--- 246-Strobogrammatic-Number/solution.java
+++ 246-Strobogrammatic-Number/solution.java
@@ -1,21 +1,16 @@
 public class Solution {
     public boolean isStrobogrammatic(String num) {
-        Map<Character, Character> map = new HashMap<Character, Character>();
-    map.put('6', '9');
-    map.put('9', '6');
-    map.put('0', '0');
-    map.put('1', '1');
-    map.put('8', '8');
-   
-    int l = 0, r = num.length() - 1;
-    while (l <= r) {
-        if (!map.containsKey(num.charAt(l))) return false;
-        if (map.get(num.charAt(l)) != num.charAt(r))
-            return false;
-        l++;
-        r--;
-    }
-    
-    return true;
+        Map<Character,Character> map=new HashMap<>();
+        map.put('6','9');
+        map.put('9','6');
+        map.put('1','1');
+        map.put('8','8');
+        map.put('0','0');
+        for(int i=0;i<(num.length()+1)/2;i++){
+            char c=num.charAt(i);
+            char cc=num.charAt(num.length()-1-i);
+            if(!map.containsKey(c)||map.get(c)!=cc) return false;
+        }
+        return true;
     }
 }
