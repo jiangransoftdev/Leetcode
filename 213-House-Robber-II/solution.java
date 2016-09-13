b38bad19f028914f0708d859d8341e5cf76f3358
jiangran910126 <jiangran0126@gmail.com>
2016-09-12 19:33:54 -0500

    [213. House Robber II][Accepted]

--- 213-House-Robber-II/solution.java
+++ 213-House-Robber-II/solution.java
@@ -4,12 +4,13 @@ public class Solution {
         if(nums.length==1) return nums[0];
         return Math.max(helper(nums,0,nums.length-2),helper(nums,1,nums.length-1));
     }
-    public int helper(int[] nums,int s,int e){
+    public int helper(int[] nums,int start,int end){
         int take=0,nontake=0,profit=0;
-        for(int i=s;i<=e;i++){
+        for(int i=start;i<=end;i++){
+            int tmp=take;
             take=nontake+nums[i];
-            nontake=profit;
-            profit=Math.max(nontake,take);
+            nontake=Math.max(nontake,tmp);
+            profit=Math.max(take,nontake);
         }
         return profit;
     }
