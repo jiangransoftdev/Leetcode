74f4db2a3e1e28c68fd389b3bc9c596106593363
jiangran910126 <jiangran0126@gmail.com>
2016-09-13 17:35:40 -0500

    [69. Sqrt(x)][Accepted]

--- 69-Sqrt(x)/solution.java
+++ 69-Sqrt(x)/solution.java
@@ -1,13 +1,13 @@
 public class Solution {
     public int mySqrt(int x) {
         if(x==0) return 0;
-        int low=1,high=x;
-        while(low<high){
-            int mid=low+(high-low)/2+1;
-            if(x/mid==mid) return mid;
-            else if(x/mid>mid) low=mid;
-            else high=mid-1;
+        int l=1,h=x;
+        while(l<=h){
+            int m=l+(h-l)/2;
+            if(m==x/m) return m;
+            else if(m>x/m) h=m-1;
+            else l=m+1;
         }
-        return high;
+        return h;
     }
 }
