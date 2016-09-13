7fb45c8b68e29dd435533de0bb756554d05b3638
jiangran910126 <jiangran0126@gmail.com>
2016-09-12 23:24:57 -0500

    [223. Rectangle Area][Accepted]

--- 223-Rectangle-Area/solution.java
+++ 223-Rectangle-Area/solution.java
@@ -1,14 +1,12 @@
 public class Solution {
     public int computeArea(int A, int B, int C, int D, int E, int F, int G, int H) {
-        int areaA=(C-A)*(D-B);
-        int areaB=(G-E)*(H-F);
         int left=Math.max(A,E);
         int right=Math.min(C,G);
         int top=Math.min(D,H);
-        int bottom=Math.max(F,B);
-        int overlap=0;
-        if(right>left&&top>bottom)
-            overlap=(right-left)*(top-bottom);
-        return areaA+areaB-overlap;
+        int bottom=Math.max(B,F);
+        int overlap=(right-left)*(top-bottom);
+        int area=(C-A)*(D-B)+(G-E)*(H-F);
+        if(right>left&&top>bottom) return area-overlap;
+        return area;
     }
 }
