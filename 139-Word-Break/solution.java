1e27a216914d9109a4316cc6424991569a199971
jiangran910126 <jiangran0126@gmail.com>
2016-09-12 21:03:53 -0500

    [139. Word Break][Accepted]

--- 139-Word-Break/solution.java
+++ 139-Word-Break/solution.java
@@ -2,14 +2,15 @@ public class Solution {
     public boolean wordBreak(String s, Set<String> wordDict) {
         boolean[] dp=new boolean[s.length()+1];
         for(int i=1;i<=s.length();i++){
-            if(wordDict.contains(s.substring(0,i)))
-                dp[i]=true;
-            for(int j=1;j<i;j++){
-                if(dp[j]&&wordDict.contains(s.substring(j,i))){
-                     dp[i]=true;
-                     break;
+            String sub=s.substring(0,i);
+            if(wordDict.contains(sub)) dp[i]=true;
+            else{
+                for(int j=1;j<i;j++){
+                    if(dp[j]&&wordDict.contains(s.substring(j,i))){
+                        dp[i]=true;
+                        break;
+                    } 
                 }
-                   
             }
         }
         return dp[s.length()];
