6360b444c2022ab4469b22bafe925aacbb738219
jiangran910126 <jiangran0126@gmail.com>
2016-09-14 02:25:54 -0500

    [110. Balanced Binary Tree][Accepted]

--- 110-Balanced-Binary-Tree/solution.java
+++ 110-Balanced-Binary-Tree/solution.java
@@ -9,12 +9,11 @@
  */
 public class Solution {
     public boolean isBalanced(TreeNode root) {
-        int res=helper(root);
-        if(res==-1) return false;
-        return true;
+        return helper(root)!=-1;
     }
     public int helper(TreeNode root){
-        if(root==null) return 0;
+        if(root==null) return Integer.MIN_VALUE;
+        if(root.left==null&&root.right==null) return 0;
         int left=helper(root.left);
         int right=helper(root.right);
         if(left==-1||right==-1||Math.abs(left-right)>1) return -1;
