716df41fe7e442b252bb11b8b6416c8d6d609b69
jiangran910126 <jiangran0126@gmail.com>
2016-09-14 02:30:07 -0500

    [343. Integer Break][Accepted]

--- 343-Integer-Break/solution.java
+++ 343-Integer-Break/solution.java
@@ -1,16 +1,18 @@
 public class Solution {
     public int integerBreak(int n) {
-        if(n==1) return 1;
+        if(n==0) return 0;
+        if(n==1) return 0;
         if(n==2) return 1;
         if(n==3) return 2;
         if(n==4) return 4;
         int[] dp=new int[n+1];
-        dp[1]=1;
-        dp[2]=2;
-        dp[3]=3;
         dp[4]=4;
-        for(int i=5;i<=n;i++)
+        dp[3]=3;
+        dp[2]=2;
+        dp[1]=1;
+        for(int i=5;i<=n;i++){
             dp[i]=dp[i-3]*3;
+        }
         return dp[n];
     }
 }
