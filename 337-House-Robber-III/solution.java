e9de2d3a4c7a4219925b13782aef144c9ead2301
jiangran910126 <jiangran0126@gmail.com>
2016-09-13 22:49:50 -0500

    [337. House Robber III][Accepted]

--- 337-House-Robber-III/solution.java
+++ 337-House-Robber-III/solution.java
@@ -13,12 +13,12 @@ public class Solution {
         return Math.max(res[0],res[1]);
     }
     public int[] helper(TreeNode root){
-        if(root==null) return new int[2];
+        if(root==null) return new int[]{0,0};
         int[] left=helper(root.left);
         int[] right=helper(root.right);
-        int[] val=new int[2];
-        val[0]=Math.max(left[0],left[1])+Math.max(right[0],right[1]);
-        val[1]=root.val+left[0]+right[0];
-        return val;
+        int[] res=new int[2];
+        res[0]=Math.max(left[0],left[1])+Math.max(right[0],right[1]);
+        res[1]=root.val+left[0]+right[0];
+        return res;
     }
 }
