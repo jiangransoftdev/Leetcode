d6039e1e10d87de08e15e0718e8081b2318b254e
jiangran910126 <jiangran0126@gmail.com>
2016-09-14 18:38:12 -0500

    [161. One Edit Distance][Accepted]

--- 161-One-Edit-Distance/solution.java
+++ 161-One-Edit-Distance/solution.java
@@ -1,14 +1,11 @@
 public class Solution {
     public boolean isOneEditDistance(String s, String t) {
-        for(int i=0;i<Math.min(s.length(),t.length());i++){
-            if(s.charAt(i)!=t.charAt(i)){
-                if(s.length()==t.length())
-                    return s.substring(i+1).equals(t.substring(i+1));
-                else if(s.length()<t.length())
-                    return s.substring(i).equals(t.substring(i+1));
-                else return s.substring(i+1).equals(t.substring(i));
-            }
+        for(int i=0,j=0;i<s.length()&&j<t.length();i++,j++){
+            if(s.charAt(i)==t.charAt(j)) continue;
+            else if(s.length()==t.length()) return s.substring(i+1).equals(t.substring(j+1));
+            else if(s.length()>t.length()) return s.substring(i+1).equals(t.substring(j));
+            else return s.substring(i).equals(t.substring(j+1));
         }
-        return Math.abs(s.length()-t.length())==1;
+        return s.length()+1==t.length()||s.length()==t.length()+1;
     }
 }
