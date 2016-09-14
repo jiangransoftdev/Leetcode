359d5af8aec3042d6969224c7f22644696d768ab
jiangran910126 <jiangran0126@gmail.com>
2016-09-13 21:42:18 -0500

    [143. Reorder List][Accepted]

--- 143-Reorder-List/solution.java
+++ 143-Reorder-List/solution.java
@@ -9,30 +9,30 @@
 public class Solution {
     public void reorderList(ListNode head) {
         if(head==null||head.next==null) return;
-        ListNode slow=head,fast=head.next;
+        ListNode slow=head;
+        ListNode fast=head.next;
         while(fast!=null&&fast.next!=null){
-            fast=fast.next.next;
-            slow=slow.next;
+        	slow=slow.next;
+        	fast=fast.next.next;
         }
         ListNode p1=slow.next;
         slow.next=null;
         ListNode p2=p1.next;
         p1.next=null;
         while(p1!=null&&p2!=null){
-            ListNode t=p2.next;
-            p2.next=p1;
-            p1=p2;
-            p2=t;
+        	ListNode t=p2.next;
+        	p2.next=p1;
+        	p1=p2;
+        	p2=t;
         }
         slow=head;
-        fast=p1;
-        while(slow!=null&&fast!=null){
-            ListNode tmp=fast.next;
-            fast.next=slow.next;
-            slow.next=fast;
-            fast=tmp;
-            slow=slow.next.next;
-        }
+        while(p1!=null&&slow!=null){
+        	ListNode ap1=p1.next;
+        	ListNode aslow=slow.next;
+        	slow.next=p1;
+        	p1.next=aslow;
+        	p1=ap1;
+        	slow=aslow;
     }
-    
+    }
 }
