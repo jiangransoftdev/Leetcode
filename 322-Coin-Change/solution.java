69ec65737db24c4c2f06dd4b52af105b0aa979ef
jiangran910126 <jiangran0126@gmail.com>
2016-09-14 18:18:05 -0500

    [322. Coin Change][Accepted]

--- 322-Coin-Change/solution.java
+++ 322-Coin-Change/solution.java
@@ -1,13 +1,11 @@
 public class Solution {
     public int coinChange(int[] coins, int amount) {
-        if(amount==0) return 0;
         int[] dp=new int[amount+1];
         Arrays.fill(dp,Integer.MAX_VALUE);
         dp[0]=0;
         for(int i=1;i<=amount;i++){
-            for(int j=0;j<coins.length;j++){
-                if(i-coins[j]>=0&&dp[i-coins[j]]!=Integer.MAX_VALUE)
-                    dp[i]=Math.min(dp[i],dp[i-coins[j]]+1);
+            for(int coin:coins){
+                if(i-coin>=0&&dp[i-coin]!=Integer.MAX_VALUE)   dp[i]=Math.min(dp[i],dp[i-coin]+1);
             }
         }
         return dp[amount]==Integer.MAX_VALUE?-1:dp[amount];
