2e0ae3be7e1934c6509eae3de8dcd8fe71da8fbd
jiangran910126 <jiangran0126@gmail.com>
2016-09-14 01:50:30 -0500

    [256. Paint House][Accepted]

--- 256-Paint-House/solution.java
+++ 256-Paint-House/solution.java
@@ -1,12 +1,11 @@
 public class Solution {
     public int minCost(int[][] costs) {
-        if(costs.length==0) return 0;
         int r=0,g=0,b=0;
-        for(int i=0;i<costs.length;i++){
+        for(int[] cost:costs){
             int tmpr=r,tmpg=g,tmpb=b;
-            r=Math.min(tmpg,tmpb)+costs[i][0];
-            b=Math.min(tmpr,tmpg)+costs[i][1];
-            g=Math.min(tmpr,tmpb)+costs[i][2];
+            r=Math.min(tmpg,tmpb)+cost[0];
+            g=Math.min(tmpr,tmpb)+cost[2];
+            b=Math.min(tmpr,tmpg)+cost[1];
         }
         return Math.min(r,Math.min(g,b));
     }
