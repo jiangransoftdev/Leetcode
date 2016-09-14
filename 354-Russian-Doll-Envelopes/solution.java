a2d03681f65e88f1ff95fe199f08efa34d05fd4f
jiangran910126 <jiangran0126@gmail.com>
2016-09-14 01:32:40 -0500

    [354. Russian Doll Envelopes][Accepted]

--- 354-Russian-Doll-Envelopes/solution.java
+++ 354-Russian-Doll-Envelopes/solution.java
@@ -3,19 +3,17 @@ public class Solution {
         if(envelopes.length==0) return 0;
         Arrays.sort(envelopes,new Comparator<int[]>(){
             public int compare(int[] a,int[] b){
-                if(a[0]!=b[0]) return a[0]-b[0];
-                else return b[1]-a[1];
+                if(a[0]==b[0]) return b[1]-a[1];
+                return a[0]-b[0];
             }
         });
         int len=0;
         int[] dp=new int[envelopes.length];
-        for(int i=0;i<envelopes.length;i++){
-            int index=Arrays.binarySearch(dp,0,len,envelopes[i][1]);
-            if(index<0)
-                index=-(index+1);
-            dp[index]=envelopes[i][1];
-            if(index==len)
-                len++;
+        for(int[] envelope:envelopes){
+            int i=Arrays.binarySearch(dp,0,len,envelope[1]);
+            if(i<0) i=-(i+1);
+            dp[i]=envelope[1];
+            if(i==len) len++;
         }
         return len;
     }
