a48c24f0305a3a0cba5ba1fcd8ae654ad6ab3871
jiangran910126 <jiangran0126@gmail.com>
2016-09-14 18:26:40 -0500

    [338. Counting Bits][Accepted]

--- 338-Counting-Bits/solution.java
+++ 338-Counting-Bits/solution.java
@@ -1,11 +1,11 @@
 public class Solution {
     public int[] countBits(int num) {
-        if(num==0) return new int[]{0};
         int[] dp=new int[num+1];
+        if(num==0) return dp;
+        int factor=1;
         dp[1]=1;
-        int factor=2;
         for(int i=2;i<=num;i++){
-            if(i==factor*2) factor*=2;
+            if(i>=factor*2) factor*=2;
             dp[i]=dp[i-factor]+1;
         }
         return dp;
