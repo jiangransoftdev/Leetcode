1e74a7252753aca8611ba4931065b24134146712
jiangran910126 <jiangran0126@gmail.com>
2016-09-14 21:45:54 -0500

    [276. Paint Fence][Accepted]

--- 276-Paint-Fence/solution.java
+++ 276-Paint-Fence/solution.java
@@ -1,14 +1,13 @@
 public class Solution {
     public int numWays(int n, int k) {
-        int first=k,second=k*k;
-        if(n==1) return first;
-        if(n==2) return second;
-        int res=0;
-        for(int i=3;i<=n;i++){
-            res=(first+second)*(k-1);
-            first=second;
-            second=res;
-        }
-        return res;
+        int[] dp=new int[n+1];
+        if(n==0) return 0;
+        if(n==1) return k;
+        dp[1]=k;
+        
+        dp[2]=k*k;
+        for(int i=3;i<=n;i++)
+            dp[i]=dp[i-2]*(k-1)+dp[i-1]*(k-1);
+        return dp[n];
     }
 }
