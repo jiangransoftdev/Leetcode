9f86270e136635b66935ba6d6811405abbeb34f6
jiangran910126 <jiangran0126@gmail.com>
2016-09-15 02:22:58 -0500

    [118. Pascal's Triangle][Accepted]

--- 118-Pascal's-Triangle/solution.java
+++ 118-Pascal's-Triangle/solution.java
@@ -5,11 +5,8 @@ public class Solution {
         for(int i=0;i<numRows;i++){
             List<Integer> solu=new ArrayList<>();
             for(int j=0;j<=i;j++){
-                if(j==0||j==i)
-                    solu.add(1);
-                else{
-                    solu.add(res.get(i-1).get(j-1)+res.get(i-1).get(j));
-                }
+                if(j==0||j==i) solu.add(1);
+                else solu.add(res.get(i-1).get(j-1)+res.get(i-1).get(j));
             }
             res.add(solu);
         }
