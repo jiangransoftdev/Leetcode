57697b9e0fc631f4615add3184d1f16670940435
jiangran910126 <jiangran0126@gmail.com>
2016-09-15 00:06:07 -0500

    [64. Minimum Path Sum][Accepted]

--- 64-Minimum-Path-Sum/solution.java
+++ 64-Minimum-Path-Sum/solution.java
@@ -2,16 +2,14 @@ public class Solution {
     public int minPathSum(int[][] grid) {
         if(grid.length==0) return 0;
         int m=grid.length,n=grid[0].length;
-        int[][] dp=new int[m][n];
-        dp[0][0]=grid[0][0];
-        for(int i=1;i<m;i++)
-            dp[i][0]=dp[i-1][0]+grid[i][0];
-        for(int i=1;i<n;i++)
-            dp[0][i]=dp[0][i-1]+grid[0][i];
-        for(int i=1;i<m;i++){
-            for(int j=1;j<n;j++)
-                dp[i][j]=Math.min(dp[i-1][j],dp[i][j-1])+grid[i][j];
+        int[][] dp=new int[m+1][n+1];
+        dp[1][1]=grid[0][0];
+        for(int i=1;i<=m;i++){
+            for(int j=1;j<=n;j++){
+                if(i==1&&j==1) continue;
+                dp[i][j]=Math.min(i-1==0?Integer.MAX_VALUE:dp[i-1][j],j-1==0?Integer.MAX_VALUE:dp[i][j-1])+grid[i-1][j-1];
+            }
         }
-        return dp[m-1][n-1];
+        return dp[m][n];
     }
 }
