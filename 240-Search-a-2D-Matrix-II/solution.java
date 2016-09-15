7924a93d2e0c524461e1f03b1c8b9b8eb435af3f
jiangran910126 <jiangran0126@gmail.com>
2016-09-15 16:46:02 -0500

    [240. Search a 2D Matrix II][Accepted]

--- 240-Search-a-2D-Matrix-II/solution.java
+++ 240-Search-a-2D-Matrix-II/solution.java
@@ -1,11 +1,12 @@
 public class Solution {
     public boolean searchMatrix(int[][] matrix, int target) {
         if(matrix.length==0) return false;
-        int i=matrix.length-1,j=0;
-        while(i>=0&&j<matrix[0].length){
-            if(matrix[i][j]==target) return true;
-            else if(matrix[i][j]>target) i--;
-            else j++;
+        int m=matrix.length,n=matrix[0].length;
+        int mrow=0,mcol=n-1;
+        while(mrow<m&&mcol>=0){
+            if(matrix[mrow][mcol]==target) return true;
+            else if(matrix[mrow][mcol]<target) mrow++;
+            else mcol--;
         }
         return false;
     }
