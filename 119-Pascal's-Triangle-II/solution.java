9ac445905a6a3e3ca73bbb0dcd2d04e291647267
jiangran910126 <jiangran0126@gmail.com>
2016-09-15 02:27:51 -0500

    [119. Pascal's Triangle II][Accepted]

--- 119-Pascal's-Triangle-II/solution.java
+++ 119-Pascal's-Triangle-II/solution.java
@@ -1,17 +1,15 @@
 public class Solution {
     public List<Integer> getRow(int rowIndex) {
-        List<Integer> res=new ArrayList<>();
-        int[] nums=new int[rowIndex+1];
+        List<Integer> ans=new ArrayList<>();
+        int[] res=new int[rowIndex+1];
         for(int i=0;i<=rowIndex;i++){
             for(int j=i;j>=0;j--){
-                if(j==i||j==0) nums[j]=1;
-                else{
-                    nums[j]+=nums[j-1];
-                }
+                if(j==0||j==i) res[j]=1;
+                else res[j]=res[j]+res[j-1];
             }
         }
-        for(int num:nums)
-            res.add(num);
-        return res;
+        for(int i=0;i<res.length;i++)
+            ans.add(res[i]);
+        return ans;
     }
 }
