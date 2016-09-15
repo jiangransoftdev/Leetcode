8eb2451100cf9e26c17afe8d02f9742a03741bd1
jiangran910126 <jiangran0126@gmail.com>
2016-09-15 17:54:00 -0500

    [280. Wiggle Sort][Accepted]

--- 280-Wiggle-Sort/solution.java
+++ 280-Wiggle-Sort/solution.java
@@ -2,15 +2,15 @@ public class Solution {
     public void wiggleSort(int[] nums) {
         for(int i=0;i<nums.length;i++){
             if(i%2==1){
-                if(nums[i-1]>nums[i]) swap(nums,i);
+                if(nums[i-1]>nums[i]) swap(nums,i-1,i);
             }
-            else
-                if(i!=0&&nums[i]>nums[i-1]) swap(nums,i);
+            else if(i!=0&&nums[i]>nums[i-1]) swap(nums,i,i-1);
         }
+        
     }
-    public void swap(int[] nums,int i){
+    public void swap(int[] nums,int i,int j){
         int tmp=nums[i];
-        nums[i]=nums[i-1];
-        nums[i-1]=tmp;
+        nums[i]=nums[j];
+        nums[j]=tmp;
     }
 }
