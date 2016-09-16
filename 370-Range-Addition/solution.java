5a193d207a46a984396c7a156b766699ebae80f2
jiangran910126 <jiangran0126@gmail.com>
2016-09-15 23:38:40 -0500

    [370. Range Addition][Accepted]

--- 370-Range-Addition/solution.java
+++ 370-Range-Addition/solution.java
@@ -2,17 +2,12 @@ public class Solution {
     public int[] getModifiedArray(int length, int[][] updates) {
         int[] res=new int[length];
         for(int[] update:updates){
-            int start=update[0];
-            int end=update[1];
-            int val=update[2];
-            res[start]+=val;
-            if(end+1<length) res[end+1]-=val;
+            int start=update[0],end=update[1],num=update[2];
+            res[start]+=num;
+            if(end+1<length) res[end+1]+=-num;
         }
-        int sum=0;
-        for(int i=0;i<length;i++){
-            sum+=res[i];
-            res[i]=sum;
-        }
+        for(int i=1;i<length;i++)
+            res[i]+=res[i-1];
         return res;
     }
 }
