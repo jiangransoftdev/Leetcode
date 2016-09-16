e08b4a70afed6b8db95a30135e3959d00ede33fc
jiangran910126 <jiangran0126@gmail.com>
2016-09-15 23:21:52 -0500

    [314. Binary Tree Vertical Order Traversal][Accepted]

--- 314-Binary-Tree-Vertical-Order-Traversal/solution.java
+++ 314-Binary-Tree-Vertical-Order-Traversal/solution.java
@@ -8,37 +8,37 @@
  * }
  */
 public class Solution {
+   
     public List<List<Integer>> verticalOrder(TreeNode root) {
         List<List<Integer>> res=new ArrayList<>();
         if(root==null) return res;
-        Map<Integer,List<Integer>> map=new HashMap<>();
-        Queue<Integer> col=new LinkedList<>();
-        Queue<TreeNode> q=new LinkedList<>();
         int min=0,max=0;
-        col.offer(0);
+        Map<Integer,List<Integer>> map=new HashMap<>();
+        Queue<Integer> cols=new LinkedList<>();
+        Queue<TreeNode> q=new LinkedList<>();
         q.offer(root);
+        cols.offer(0);
         while(!q.isEmpty()){
             int size=q.size();
             for(int i=0;i<size;i++){
                 TreeNode node=q.poll();
-                int cur=col.poll();
-                if(!map.containsKey(cur)) map.put(cur,new ArrayList<>());
-                map.get(cur).add(node.val);
+                int col=cols.poll();
+                if(!map.containsKey(col)) map.put(col,new ArrayList<>());
+                map.get(col).add(node.val);
                 if(node.left!=null){
                     q.offer(node.left);
-                    col.offer(cur-1);
-                    min=Math.min(min,cur-1);
+                    cols.offer(col-1);
+                    min=Math.min(min,col-1);
                 }
                 if(node.right!=null){
                     q.offer(node.right);
-                    col.offer(cur+1);
-                    max=Math.max(max,cur+1);
+                    cols.offer(col+1);
+                    max=Math.max(max,col+1);
                 }
             }
         }
-        for(int i=min;i<=max;i++){
+        for(int i=min;i<=max;i++)
             res.add(map.get(i));
-        }
         return res;
     }
 }
