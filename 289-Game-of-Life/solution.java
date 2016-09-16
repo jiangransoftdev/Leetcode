edb02e6535d00503e9e00c08a5d25e6a5d88cce6
jiangran910126 <jiangran0126@gmail.com>
2016-09-15 22:31:58 -0500

    [289. Game of Life][Accepted]

--- 289-Game-of-Life/solution.java
+++ 289-Game-of-Life/solution.java
@@ -4,13 +4,11 @@ public class Solution {
         int m=board.length,n=board[0].length;
         for(int i=0;i<m;i++){
             for(int j=0;j<n;j++){
-                int count=neighbors(board,i,j,m,n);
+                int count=countNeighbor(board,i,j);
                 if(board[i][j]==1){
-                    if(count==2||count==3) board[i][j]=3;
+                    if(count==2||count==3) board[i][j]+=2;
                 }
-                else if(board[i][j]==0){
-                    if(count==3) board[i][j]=2;
-                }
+                else if(count==3) board[i][j]+=2;
             }
         }
         for(int i=0;i<m;i++){
@@ -18,12 +16,12 @@ public class Solution {
                 board[i][j]=board[i][j]>>1;
         }
     }
-    public int neighbors(int[][] board,int i,int j,int m,int n){
+    public int countNeighbor(int[][] board,int i,int j){
         int count=0;
-        for(int a=Math.max(0,i-1);a<=Math.min(i+1,m-1);a++){
-            for(int b=Math.max(0,j-1);b<=Math.min(j+1,n-1);b++){
-                if(a==i&&b==j) continue;
-                if((board[a][b]&1)==1) count++;
+        for(int row=Math.max(0,i-1);row<=Math.min(i+1,board.length-1);row++){
+            for(int col=Math.max(0,j-1);col<=Math.min(j+1,board[0].length-1);col++){
+                if(row==i&&col==j) continue;
+                if((board[row][col]&1)==1) count++;
             }
         }
         return count;
