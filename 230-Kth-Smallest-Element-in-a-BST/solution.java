1ffd812586215a00bff8d30fc44f665b0fbd2b64
jiangran910126 <jiangran0126@gmail.com>
2016-09-15 23:07:08 -0500

    [230. Kth Smallest Element in a BST][Accepted]

--- 230-Kth-Smallest-Element-in-a-BST/solution.java
+++ 230-Kth-Smallest-Element-in-a-BST/solution.java
@@ -8,18 +8,20 @@
  * }
  */
 public class Solution {
-    int i=0;
     public int kthSmallest(TreeNode root, int k) {
-        int res=Integer.MAX_VALUE;
-        if(root==null) return res;
-        int left=kthSmallest(root.left,k);
-        i++;
-        if(i==k) res=root.val;
-        int right=kthSmallest(root.right,k);
-        if(left!=Integer.MAX_VALUE) return left;
-        if(right!=Integer.MAX_VALUE) return right;
-        if(res!=Integer.MAX_VALUE) return res;
-        return Integer.MAX_VALUE;
+        int count = countNodes(root.left);
+        if (k <= count) {
+            return kthSmallest(root.left, k);
+        } else if (k > count + 1) {
+            return kthSmallest(root.right, k-1-count); // 1 is counted as current node
+        }
+        
+        return root.val;
     }
     
+    public int countNodes(TreeNode n) {
+        if (n == null) return 0;
+        
+        return 1 + countNodes(n.left) + countNodes(n.right);
+    }
 }
