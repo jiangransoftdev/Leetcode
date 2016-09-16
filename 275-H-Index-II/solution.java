4d6ef1ff6fdc7e66553496edad27b4cfe2b04d22
jiangran910126 <jiangran0126@gmail.com>
2016-09-16 16:50:00 -0500

    [275. H-Index II][Accepted]

--- 275-H-Index-II/solution.java
+++ 275-H-Index-II/solution.java
@@ -1,16 +1,15 @@
 public class Solution {
     public int hIndex(int[] citations) {
         int n=citations.length;
-        if(n==0) return 0;
-        int start=0,end=n-1,res=0;
-        while(start<=end){
-            int mid=start+(end-start)/2;
+        int low=0,high=n-1,res=0;
+        while(low<=high){
+            int mid=low+(high-low)/2;
             if(citations[mid]>=n-mid){
                 res=Math.max(res,n-mid);
-                end=mid-1;
+                high=mid-1;
             }
-            else
-                start=mid+1;
+                
+            else low=mid+1;
         }
         return res;
     }
