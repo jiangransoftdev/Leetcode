6bfa08d41bae2e9ba23de23504b53065b3a38794
jiangran910126 <jiangran0126@gmail.com>
2016-09-16 16:41:16 -0500

    [274. H-Index][Accepted]

--- 274-H-Index/solution.java
+++ 274-H-Index/solution.java
@@ -2,17 +2,14 @@ public class Solution {
     public int hIndex(int[] citations) {
         int[] count=new int[citations.length+1];
         for(int i=0;i<citations.length;i++){
-            if(citations[i]>=citations.length)
-                count[citations.length]++;
-            else
-                count[citations[i]]++;
+            if(citations[i]>=count.length-1) count[count.length-1]++;
+            else count[citations[i]]++;
         }
-        int res=0;
+        int total=0;
         for(int i=count.length-1;i>=0;i--){
-            res+=count[i];
-            if(res>=i) return i;
+            total+=count[i];
+            if(i<=total) return i;
         }
         return 0;
-        
     }
 }
