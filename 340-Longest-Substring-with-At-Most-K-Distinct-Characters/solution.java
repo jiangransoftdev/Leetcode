c4cf9349d666436eda4082bce3d237724014ab7e
jiangran910126 <jiangran0126@gmail.com>
2016-09-16 15:38:05 -0500

    [340. Longest Substring with At Most K Distinct Characters][Accepted]

--- 340-Longest-Substring-with-At-Most-K-Distinct-Characters/solution.java
+++ 340-Longest-Substring-with-At-Most-K-Distinct-Characters/solution.java
@@ -1,28 +1,27 @@
 public class Solution {
     public int lengthOfLongestSubstringKDistinct(String s, int k) {
-        if(s.length()==0) return 0;
         Map<Character,Integer> map=new HashMap<>();
-        int slow=0,fast=0,count=0,max=0;
+        int slow=0,fast=0,res=0,count=0;
         while(fast<s.length()){
             char c=s.charAt(fast);
             if(!map.containsKey(c)){
-                count++;
                 map.put(c,1);
+                count++;
             }
-            else{
+            else {
                 if(map.get(c)==0) count++;
                 map.put(c,map.get(c)+1);
-            } 
-            while(count>k){
-                char cs=s.charAt(slow);
-                map.put(cs,map.get(cs)-1);
-                if(map.get(cs)==0) count--;
-                slow++;
             }
-            max=Math.max(max,fast-slow+1);
+            while(count>k){
+                char a=s.charAt(slow);
+                map.put(a,map.get(a)-1);
+                slow++;
+                if(map.get(a)==0) count--;
+            }
+            res=Math.max(res,fast-slow+1);
             fast++;
         }
-        max=Math.max(max,fast-slow);
-        return max;
+        res=Math.max(res,fast-slow);
+        return res;
     }
 }
