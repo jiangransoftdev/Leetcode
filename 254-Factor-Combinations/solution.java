d8811d060f549ef42258833bde0384ee02c0176a
jiangran910126 <jiangran0126@gmail.com>
2016-09-17 02:16:37 -0500

    [254. Factor Combinations][Accepted]

--- 254-Factor-Combinations/solution.java
+++ 254-Factor-Combinations/solution.java
@@ -1,24 +1,22 @@
 public class Solution {
     public List<List<Integer>> getFactors(int n) {
-    List<List<Integer>> result = new ArrayList<List<Integer>>();
-    helper(result, new ArrayList<Integer>(), n, 2);
-    return result;
-}
-
-public void helper(List<List<Integer>> result, List<Integer> item, int n, int start){
-    if (n <= 1) {
-        if (item.size() > 1) {
-            result.add(new ArrayList<Integer>(item));
-        }
-        return;
+        List<List<Integer>> res=new ArrayList<>();
+        if(n<=1) return res;
+        helper(n,n,2,new ArrayList<>(),res);
+        return res;
     }
-    
-    for (int i = start; i <= n; ++i) {
-        if (n % i == 0) {
-            item.add(i);
-            helper(result, item, n/i, i);
-            item.remove(item.size()-1);
+    public void helper(int n,int origin,int f,List<Integer> solu,List<List<Integer>> res){
+        if(n==1){
+            res.add(new ArrayList(solu));
+            return;
+        }
+        for(int i=f;i<=n;i++){
+            if(i==origin) continue;
+            if(n%i==0){
+                solu.add(i);
+                helper(n/i,origin,i,solu,res);
+                solu.remove(solu.size()-1);
+            }
         }
     }
-}
 }
