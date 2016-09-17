d6c62c6e62aa177dbc381cb2dd2e1647d434777c
jiangran910126 <jiangran0126@gmail.com>
2016-09-17 15:43:49 -0500

    [368. Largest Divisible Subset][Accepted]

--- 368-Largest-Divisible-Subset/solution.java
+++ 368-Largest-Divisible-Subset/solution.java
@@ -3,15 +3,12 @@ public class Solution {
         List<Integer> res=new ArrayList<>();
         if(nums.length==0) return res;
         int[] len=new int[nums.length];
-        // len[0]=1;
         int maxl=0;
         Arrays.sort(nums);
         for(int i=1;i<nums.length;i++){
-            // int l=1;
             for(int j=0;j<i;j++){
                 if(nums[i]%nums[j]==0) len[i]=Math.max(len[i],len[j]+1);
             }
-            // len[i]=l;
             maxl=Math.max(len[i],maxl);
         }
         if(maxl+1==nums.length){
