921cc82065fb4002575c76b7c5ef7b5c6975529d
jiangran910126 <jiangran0126@gmail.com>
2016-09-17 16:11:26 -0500

    [91. Decode Ways][Accepted]

--- 91-Decode-Ways/solution.java
+++ 91-Decode-Ways/solution.java
@@ -3,18 +3,18 @@ public class Solution {
         if(s.length()==0) return 0;
         int[] dp=new int[s.length()+1];
         dp[s.length()]=1;
-        dp[s.length()-1]=s.charAt(s.length()-1)=='0'?0:1;
+        if(s.charAt(s.length()-1)!='0') dp[s.length()-1]=1;
+        else dp[s.length()-1]=0;
+        if(s.length()==1) return dp[0];
         for(int i=s.length()-2;i>=0;i--){
-            if(s.charAt(i)=='0') dp[i]=0;
-            else{
-                int num=(s.charAt(i)-'0')*10+(s.charAt(i+1)-'0');
-                if(num>=10&&num<=26)
-                    dp[i]=dp[i+1]+dp[i+2];
-                else{
-                    if(s.charAt(i+1)=='0') break;
-                    dp[i]=dp[i+1];
-                }
+            if(s.charAt(i)-'0'==0){
+                dp[i]=0;
+                continue;
             }
+            int num=(s.charAt(i)-'0')*10+(s.charAt(i+1)-'0');
+            if(num>=10&&num<=26)
+                dp[i]=dp[i+1]+dp[i+2];
+            else dp[i]=dp[i+1];
         }
         return dp[0];
     }
