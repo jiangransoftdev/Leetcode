cf5b6355a73b3f21553212781d57bfd97a20d773
jiangran910126 <jiangran0126@gmail.com>
2016-09-17 03:51:52 -0500

    [38. Count and Say][Accepted]

--- 38-Count-and-Say/solution.java
+++ 38-Count-and-Say/solution.java
@@ -1,27 +1,30 @@
 public class Solution {
     public String countAndSay(int n) {
-        String s="1";
-        int index=1;
-        while(index<n){
-            char c=s.charAt(0);
-            int count=0;
-            StringBuilder sb=new StringBuilder();
-            for(int i=0;i<=s.length();i++){
-                if(i!=s.length()&&s.charAt(i)==c)
-                    count++;
-                else{
-                    sb.append(count);
-                    sb.append(c);
-                    if(i!=s.length()){
-                        c=s.charAt(i);
-                    count=1;
-                    }
-                    
-                }
-            }
-            s=sb.toString();
-            index++;
+        int i=1;
+        String seq="1";
+        while(i<n){
+        	StringBuilder sb=new StringBuilder();
+        	int count=0;
+        	char c=seq.charAt(0);
+        	for(int j=0;j<=seq.length();j++){
+        		if(j!=seq.length()&&seq.charAt(j)==c){
+        			count++;
+        			continue;
+        		}
+        		else{
+        	        sb.append(count);
+        	        sb.append(c);
+        	        if(j!=seq.length()){
+        	        	c=seq.charAt(j);
+            			count=1;
+        	        }
+        	        
+        		}
+        		
+        	}
+        	seq=sb.toString();
+    		i++;
         }
-        return s;
+        return seq;
     }
 }
