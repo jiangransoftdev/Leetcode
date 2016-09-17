b6506f6a68da111a6fbb2a78884fd5c78e982d17
jiangran910126 <jiangran0126@gmail.com>
2016-09-17 14:51:27 -0500

    [55. Jump Game][Accepted]

--- 55-Jump-Game/solution.java
+++ 55-Jump-Game/solution.java
@@ -1,10 +1,10 @@
 public class Solution {
     public boolean canJump(int[] nums) {
         if(nums.length==1) return true;
-        int step=nums[0];
-        for(int i=0;i<nums.length;i++){
-            if(step<i) return false;
-            step=Math.max(step,i+nums[i]);
+        int total=0;
+        for(int i=0;i<nums.length-1;i++){
+            total=Math.max(total,i+nums[i]);
+            if(total<=i) return false;
         }
         return true;
     }
