292e1f7060e0f16b0b2672d01244d4b3b2b66ade
jiangran910126 <jiangran0126@gmail.com>
2016-09-17 14:30:10 -0500

    [18. 4Sum][Accepted]

--- 18-4Sum/solution.java
+++ 18-4Sum/solution.java
@@ -1,26 +1,24 @@
 public class Solution {
     public List<List<Integer>> fourSum(int[] nums, int target) {
         List<List<Integer>> res=new ArrayList<>();
+        if(nums.length==0) return res;
         Arrays.sort(nums);
         for(int i=0;i<nums.length-3;i++){
             if(i>0&&nums[i]==nums[i-1]) continue;
             for(int j=i+1;j<nums.length-2;j++){
                 if(j>i+1&&nums[j]==nums[j-1]) continue;
-                int left=j+1;
-                int right=nums.length-1;
-                while(left<right){
-                    int sum=nums[i]+nums[j]+nums[left]+nums[right];
-                    if(sum<target)
-                        left++;
-                    else if(sum>target)
-                        right--;
-                    else{
-                        res.add(Arrays.asList(nums[i],nums[j],nums[left],nums[right]));
-                        while(left<right&&nums[left]==nums[left+1]) left++;
-                        while(left<right&&nums[right]==nums[right-1]) right--;
-                        left++;
-                        right--;
+                int m=j+1,n=nums.length-1;
+                while(m<n){
+                    int sum=nums[i]+nums[j]+nums[m]+nums[n];
+                    if(sum==target){
+                        res.add(Arrays.asList(nums[i],nums[j],nums[m],nums[n]));
+                        while(m<n&&nums[m]==nums[m+1]) m++;
+                        while(m<n&&nums[n]==nums[n-1]) n--;
+                        m++;
+                        n--;
                     }
+                    else if(sum>target) n--;
+                    else m++;
                 }
             }
         }
