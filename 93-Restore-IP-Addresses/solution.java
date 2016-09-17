c80056e1ea7c4a179f3e061107d17b175248c2ce
jiangran910126 <jiangran0126@gmail.com>
2016-09-17 14:45:46 -0500

    [93. Restore IP Addresses][Accepted]

--- 93-Restore-IP-Addresses/solution.java
+++ 93-Restore-IP-Addresses/solution.java
@@ -5,18 +5,18 @@ public class Solution {
         helper(s,0,"",res);
         return res;
     }
-    public void helper(String s,int index,String solu,List<String> res){
-        if(index==4){
-            if(s.length()==0)
-                res.add(solu);
+    public void helper(String s,int level,String solu,List<String> res){
+        if(level==4&&s.length()==0){
+            res.add(solu);
             return;
         }
-        for(int i=1;i<=3;i++){
-            if(i>s.length()) break;
+        for(int i=1;i<=s.length();i++){
             String sub=s.substring(0,i);
-            if((sub.startsWith("0")&&sub.length()>1)||Integer.parseInt(sub)>255) break;
-            String another=solu+(index==3?(sub+""):(sub+"."));
-            helper(s.substring(i),index+1,another,res);
+            if((sub.startsWith("0",0)&&sub.length()>1)||Integer.parseInt(sub)>255||level>4) break;
+            String another="";
+            if(level<3) another=solu+sub+".";
+            else if(level==3) another=solu+sub;
+            helper(s.substring(i),level+1,another,res);
         }
     }
 }
