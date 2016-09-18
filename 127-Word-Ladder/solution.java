b56c6ee043cf76af180794a4ef3265b0dd4da4da
jiangran910126 <jiangran0126@gmail.com>
2016-09-18 15:16:13 -0500

    [127. Word Ladder][Accepted]

--- 127-Word-Ladder/solution.java
+++ 127-Word-Ladder/solution.java
@@ -1,31 +1,29 @@
 public class Solution {
     public int ladderLength(String beginWord, String endWord, Set<String> wordList) {
+        int len=1;
         Queue<String> q=new LinkedList<>();
-        if(beginWord.equals(endWord)) return 0;
-        int length=1;
-        wordList.add(endWord);
-        wordList.remove(beginWord);
         q.offer(beginWord);
+        wordList.add(endWord);
         while(!q.isEmpty()){
             int size=q.size();
             for(int i=0;i<size;i++){
                 String s=q.poll();
-                if(s.equals(endWord)) return length;
-                char[] cs=s.toCharArray();
-                for(int l=0;l<s.length();l++){
-                    char c=cs[l];
-                    for(char cc='a';cc<='z';cc++){
-                        cs[l]=cc;
-                        String another=new String(cs);
-                        if(wordList.contains(another)){
-                            q.offer(another);
-                            wordList.remove(another);
-                        }
+                if(s.equals(endWord)) return len;
+                char[] schar=s.toCharArray();
+                for(int j=0;j<schar.length;j++){
+                    char old=schar[j];
+                    for(char c='a';c<='z';c++){
+                        schar[j]=c;
+                        String ss=new String(schar);
+                        if(wordList.contains(ss)){
+                            q.offer(ss);
+                            wordList.remove(ss);
+                        } 
                     }
-                    cs[l]=c;
+                    schar[j]=old;
                 }
             }
-            length++;
+            len++;
         }
         return 0;
     }
