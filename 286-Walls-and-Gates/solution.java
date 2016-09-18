3974961198e1678e2ae3185adccb4aadb1570eca
jiangran910126 <jiangran0126@gmail.com>
2016-09-17 20:56:37 -0500

    [286. Walls and Gates][Accepted]

--- 286-Walls-and-Gates/solution.java
+++ 286-Walls-and-Gates/solution.java
@@ -1,17 +1,21 @@
 public class Solution {
     int[] d={0,1,0,-1,0};
     public void wallsAndGates(int[][] rooms) {
-        for(int i=0;i<rooms.length;i++){
-            for(int j=0;j<rooms[0].length;j++)
-                if(rooms[i][j]==0) dfs(rooms,i,j);
+        int m=rooms.length;
+        if(m==0) return;
+        int n=rooms[0].length;
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+                if(rooms[i][j]==0)  explore(rooms,i,j);
+            }
         }
     }
-    public void dfs(int[][] rooms,int i,int j){
+    public void explore(int[][] rooms,int i,int j){
         for(int k=0;k<4;k++){
             int p=i+d[k],q=j+d[k+1];
             if(p>=0&&p<rooms.length&&q>=0&&q<rooms[0].length&&rooms[p][q]>rooms[i][j]+1){
                 rooms[p][q]=rooms[i][j]+1;
-                dfs(rooms,p,q);
+                explore(rooms,p,q);
             }
         }
     }
