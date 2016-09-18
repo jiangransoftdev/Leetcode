1e3537c8a6387265a85b7acfadf353f3e3593c75
jiangran910126 <jiangran0126@gmail.com>
2016-09-18 17:44:18 -0500

    [403. Frog Jump][Accepted]

--- 403-Frog-Jump/solution.java
+++ 403-Frog-Jump/solution.java
@@ -1,24 +1,31 @@
 public class Solution {
     public boolean canCross(int[] stones) {
-        Map<Integer,Integer> map=new HashMap<>();
-        if(stones.length<=1) return true;
-        int n=stones.length;
-        boolean[][] dp=new boolean[n][n];
-        boolean res=false;
-        if(stones[1]==1) dp[0][1]=true;
-        for(int i=0;i<stones.length;i++){
-            map.put(stones[i],i);
-        }
-        for(int i=1;i<stones.length;i++){
-            for(int j=i+1;j<stones.length;j++){
-                int spos=stones[i];
-                int epos=stones[j];
-                int speed1=epos-spos,speed2=speed1-1,speed3=speed1+1;
-                dp[i][j]=(map.containsKey(spos-speed1)&&dp[map.get(spos-speed1)][i])||(map.containsKey(spos-speed2)&&dp[map.get(spos-speed2)][i])||(map.containsKey(spos-speed3)&&dp[map.get(spos-speed3)][i]);
+       final int l = stones != null ? stones.length : 0;
+        if (l < 1 || stones[0] != 0) return false;
+        final Map<Integer, Set<Integer>> map = new HashMap<>();
+        for (int s : stones) map.put(s, new HashSet<Integer>());
+        for (int s : stones) {
+            Set<Integer> jSet = map.get(s);
+            // Initial condition
+            if (s == 0) {
+                jSet.add(0);
+                if (map.containsKey(1)) map.get(1).add(1);
+                continue;
+            }
+            // For other stones
+            for (int j : jSet) {
+                int jj = j - 1;
+                int ss = s + jj;
+                // Previous jump - 1
+                if (ss != s && map.containsKey(ss)) map.get(ss).add(jj);
+                // Previous jump
+                jj++; ss++;
+                if (ss != s && map.containsKey(ss)) map.get(ss).add(jj);
+                // Previous jump + 1
+                jj++; ss++;
+                if (ss != s && map.containsKey(ss)) map.get(ss).add(jj);
             }
         }
-        for(int i=0;i<n;i++)
-            res|=dp[i][n-1];
-        return res;
+        return !map.get(stones[l - 1]).isEmpty();
     }
 }
