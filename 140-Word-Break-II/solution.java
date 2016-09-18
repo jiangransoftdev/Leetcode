2e0d2e85f0a4e71327f0608fbd6e52b368c8306d
jiangran910126 <jiangran0126@gmail.com>
2016-09-17 20:02:56 -0500

    [140. Word Break II][Accepted]

--- 140-Word-Break-II/solution.java
+++ 140-Word-Break-II/solution.java
@@ -1,22 +1,19 @@
 public class Solution {
     Map<String,List<String>> map=new HashMap<>();
     public List<String> wordBreak(String s, Set<String> wordDict) {
+        List<String> res=new ArrayList();
+        if(s.length()==0) return res;
         if(map.containsKey(s)) return map.get(s);
-        if(s.length()==0) return new ArrayList<>();
-        List<String> res=new ArrayList<>();
         for(int i=1;i<=s.length();i++){
             String sub=s.substring(0,i);
             if(!wordDict.contains(sub)) continue;
             List<String> list=wordBreak(s.substring(i),wordDict);
             if(list.size()==0){
-                if(i==s.length()){
-                    res.add(sub);
-                }
+                if(i==s.length()) res.add(sub);
             }
             else{
-                for(String tmp:list){
-                    String t=sub+" "+tmp;
-                    res.add(t);
+                for(String str:list){
+                    res.add(sub+" "+str);
                 }
             }
         }
