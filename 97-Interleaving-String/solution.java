974467311a03561b73a0bebf282a722472dead87
jiangran910126 <jiangran0126@gmail.com>
2016-09-17 22:17:24 -0500

    [97. Interleaving String][Accepted]

--- 97-Interleaving-String/solution.java
+++ 97-Interleaving-String/solution.java
@@ -1,20 +1,22 @@
 public class Solution {
     public boolean isInterleave(String s1, String s2, String s3) {
-        int m=s1.length(),n=s2.length(),l=s3.length();
-        if(m+n!=l) return false;
-        boolean[][] dp=new boolean[m+1][n+1];
+        int p=s1.length(),q=s2.length(),r=s3.length();
+        if(p+q!=r) return false;
+        boolean[][] dp=new boolean[p+1][q+1];
         dp[0][0]=true;
-        for(int i=1;i<=m;i++)
+        for(int i=1;i<=p;i++){
             if(s1.charAt(i-1)==s3.charAt(i-1))
                 dp[i][0]=dp[i-1][0];
-        for(int i=1;i<=n;i++)
+        }
+        for(int i=1;i<=q;i++){
             if(s2.charAt(i-1)==s3.charAt(i-1))
                 dp[0][i]=dp[0][i-1];
-        for(int i=1;i<=s1.length();i++){
-            for(int j=1;j<=s2.length();j++){
+        }
+        for(int i=1;i<=p;i++){
+            for(int j=1;j<=q;j++){
                 dp[i][j]=((s1.charAt(i-1)==s3.charAt(i+j-1))&&dp[i-1][j])||((s2.charAt(j-1)==s3.charAt(i+j-1))&&dp[i][j-1]);
             }
         }
-        return dp[m][n];
+        return dp[p][q];
     }
 }
