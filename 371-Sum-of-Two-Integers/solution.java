a06ae7250060836efde16fcafff4675dbb35d992
jiangran910126 <jiangran0126@gmail.com>
2016-09-17 23:20:14 -0500

    [371. Sum of Two Integers][Accepted]

--- 371-Sum-of-Two-Integers/solution.java
+++ 371-Sum-of-Two-Integers/solution.java
@@ -1,38 +1,28 @@
 public class Solution {
     public int getSum(int a, int b) {
-        if(a==0) return b;
-        if(b==0) return a;
-        int indexa=0,indexb=0,res=0,index=0,add=0;
-        while(index<32){
-            int digita=a&1;
-            int digitb=b&1;
+        int i=0,res=0,add=0,index=0;
+        while(i<32){
             int digit=0;
-            if((digita&digitb)==1){
-                digit=add;
+            int digita=(a>>i)&1;
+            int digitb=(b>>i)&1;
+            if(add==1&&(digita==1||digitb==1)){
                 add=1;
+                if(digita==1&&digitb==1) digit=1;
+                else digit=0;
             }
             else{
-                if(add==1){
-                    if((digita|digitb)==1){
-                        digit=0;
-                        add=1;
-                    }
-                    else{
-                        digit=1;
-                        add=0;
-                    }
+                if(add==0){
+                    add=digita&digitb;
+                    digit=digita^digitb;
                 }
                 else{
                     add=0;
-                    if((digita|digitb)==1)
-                        digit=1;
-                    else digit=0;
+                    digit=1;
                 }
             }
-            res+=digit<<index;
+            res|=digit<<index;
             index++;
-            a=a>>1;
-            b=b>>1;
+            i++;
         }
         return res;
     }
