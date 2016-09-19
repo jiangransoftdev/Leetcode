80e44e4f5fd264e351b2a98d149bd496f52fda46
jiangran910126 <jiangran0126@gmail.com>
2016-09-19 18:01:05 -0500

    [37. Sudoku Solver][Accepted]

--- 37-Sudoku-Solver/solution.java
+++ 37-Sudoku-Solver/solution.java
@@ -6,10 +6,10 @@ public class Solution {
         for(int i=0;i<9;i++){
             for(int j=0;j<9;j++){
                 if(board[i][j]=='.'){
-                    for(char c='1';c<='9';c++){
-                        if(isValid(board,i,j,c)){
-                            board[i][j]=c;
-                            if(helper(board)) return true;;
+                    for(char k='1';k<='9';k++){
+                        if(isValid(board,i,j,k)){
+                            board[i][j]=k;
+                            if(helper(board)) return true;
                         }
                     }
                     board[i][j]='.';
@@ -19,15 +19,12 @@ public class Solution {
         }
         return true;
     }
-    public boolean isValid(char[][] board,int row,int col,char c){
-        for(int i=0;i<9;i++)
-            if(board[row][i]==c) return false;
-        for(int i=0;i<9;i++)
-            if(board[i][col]==c) return false;
-        for(int i=(row/3)*3;i<(row/3+1)*3;i++){
-            for(int j=(col/3)*3;j<(col/3+1)*3;j++){
-                if(board[i][j]==c) return false;
-            }
+    public boolean isValid(char[][] board,int i,int j,int k){
+        for(int row=0;row<9;row++) if(board[row][j]==k) return false;
+        for(int col=0;col<9;col++) if(board[i][col]==k) return false;
+        for(int row=(i/3)*3;row<(i/3+1)*3;row++){
+            for(int col=(j/3)*3;col<(j/3+1)*3;col++)
+                if(board[row][col]==k) return false;
         }
         return true;
     }
