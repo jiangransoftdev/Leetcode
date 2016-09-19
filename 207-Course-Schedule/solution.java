5bc945ba71454e1edc85c7e6f1f7908c53a75802
jiangran910126 <jiangran0126@gmail.com>
2016-09-19 17:21:14 -0500

    [207. Course Schedule][Accepted]

--- 207-Course-Schedule/solution.java
+++ 207-Course-Schedule/solution.java
@@ -1,23 +1,22 @@
 public class Solution {
     public boolean canFinish(int numCourses, int[][] prerequisites) {
+        if(numCourses<=1) return true;
         List[] graph=new ArrayList[numCourses];
-        boolean[] visit=new boolean[numCourses];
         for(int i=0;i<numCourses;i++)
         	graph[i]=new ArrayList();
         for(int i=0;i<prerequisites.length;i++)
         	graph[prerequisites[i][1]].add(prerequisites[i][0]);
+        boolean[] visit=new boolean[numCourses];
         for(int i=0;i<numCourses;i++){
-            if(!dfs(i,visit,graph))
-                return false;
+            if(!helper(i,visit,graph)) return false; 
         }
         return true;
     }
-    public boolean dfs(int course,boolean[] visit,List[] graph){
+    public boolean helper(int course,boolean[] visit,List[] graph){
         if(visit[course]) return false;
         visit[course]=true;
-
         for(int i=0;i<graph[course].size();i++){
-            if(!dfs((int)graph[course].get(i),visit,graph)) return false;
+            if(!helper((int)graph[course].get(i),visit,graph)) return false;
         }
         visit[course]=false;
         return true;
