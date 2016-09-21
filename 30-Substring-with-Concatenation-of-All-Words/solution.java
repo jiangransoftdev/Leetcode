5ba9e161f206d7e5b5359b34f80423dbd631ead8
jiangran910126 <jiangran0126@gmail.com>
2016-09-21 17:52:12 -0500

    [30. Substring with Concatenation of All Words][Accepted]

--- 30-Substring-with-Concatenation-of-All-Words/solution.java
+++ 30-Substring-with-Concatenation-of-All-Words/solution.java
@@ -1,46 +1,47 @@
 public class Solution {
     public List<Integer> findSubstring(String s, String[] words) {
-        List<Integer> res=new ArrayList<>();
         Map<String,Integer> need=new HashMap<>();
+        List<Integer> res=new ArrayList<>();
         for(String word:words){
-            if(need.containsKey(word)){
-                need.put(word,need.get(word)+1);
-            }
-            else need.put(word,1);
+            if(!need.containsKey(word))
+                need.put(word,1);
+            else need.put(word,need.get(word)+1);
         }
-        for(int i=0;i<words[0].length();i++){
-            int left=i,count=0;
-            Map<String,Integer> curmap=new HashMap<>();
-            for(int j=i;j+words[0].length()<=s.length();j+=words[0].length()){
-                String str=s.substring(j,j+words[0].length());
-                if(need.containsKey(str)){
-                    if(curmap.containsKey(str))
-                        curmap.put(str,curmap.get(str)+1);
-                    else curmap.put(str,1);
-                    if(curmap.get(str)<=need.get(str)) count++;
+        int len=words[0].length();
+        for(int i=0;i<len;i++){
+            int left=i;
+            int count=0;
+            Map<String,Integer> found=new HashMap<>();
+            for(int j=i;j+len<=s.length();j+=len){
+                String sub=s.substring(j,j+len);
+                if(need.containsKey(sub)){
+                    if(!found.containsKey(sub))
+                        found.put(sub,1);
+                    else found.put(sub,found.get(sub)+1);
+                    if(found.get(sub)<=need.get(sub))
+                        count++;
                     else{
-                        while(curmap.get(str)>need.get(str)){
-                            String tmp=s.substring(left,left+words[0].length());
-                            curmap.put(tmp,curmap.get(tmp)-1);
-                            if(curmap.get(tmp)<need.get(tmp)) count--;
-                            left+=words[0].length();
+                        while(found.get(sub)>need.get(sub)){
+                            String start=s.substring(left,left+len);
+                            found.put(start,found.get(start)-1);
+                            if(found.get(start)<need.get(sub)) count--;
+                            left+=len;
                         }
                     }
                     if(count==words.length){
-                            res.add(left);
-                            String tmp=s.substring(left,left+words[0].length());
-                            curmap.put(tmp,curmap.get(tmp)-1);
-                            count--;
-                            left+=words[0].length();
-                        }
+                        res.add(left);
+                        String temp=s.substring(left,left+len);
+						found.put(temp, found.get(temp)-1);
+						count--;
+						left+=len;
+                    }
                 }
                 else{
-                    curmap.clear();
-                    left=j+words[0].length();
+                    found.clear();
                     count=0;
+                    left=j+len;
                 }
             }
-            
         }
         return res;
     }
