7d31e7b5399c9d98cb8faee7ad40189d596fb82e
jiangran910126 <jiangran0126@gmail.com>
2016-09-20 21:54:46 -0500

    [239. Sliding Window Maximum][Accepted]

--- 239-Sliding-Window-Maximum/solution.java
+++ 239-Sliding-Window-Maximum/solution.java
@@ -1,15 +1,16 @@
 public class Solution {
     public int[] maxSlidingWindow(int[] nums, int k) {
-        int n=nums.length,index=0;
-        if(k==0) return new int[0];
-        int[] res=new int[n-k+1];
+        if(nums.length==0) return new int[0];
         Deque<Integer> q=new LinkedList<>();
-        for(int i=0;i<n;i++){
-            while(!q.isEmpty()&&q.peek()==i-k) q.poll();
+        int[] res=new int[nums.length-k+1];
+        int index=0;
+        for(int i=0;i<nums.length;i++){
+            while(!q.isEmpty()&&q.peek()+k<=i)  q.poll();
             while(!q.isEmpty()&&nums[q.peekLast()]<nums[i]) q.pollLast();
             q.offer(i);
-            if(i>=k-1)
+            if(i>=k-1){
                 res[index++]=nums[q.peek()];
+            }
         }
         return res;
     }
