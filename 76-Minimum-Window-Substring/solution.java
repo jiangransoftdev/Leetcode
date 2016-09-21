dccaa98f971f5922ff3c8955ec3bb5ed44f7261a
jiangran910126 <jiangran0126@gmail.com>
2016-09-21 16:42:29 -0500

    [76. Minimum Window Substring][Accepted]

--- 76-Minimum-Window-Substring/solution.java
+++ 76-Minimum-Window-Substring/solution.java
@@ -1,29 +1,28 @@
 public class Solution {
     public String minWindow(String s, String t) {
-        int minstart=0,minend=0,minsize=Integer.MAX_VALUE,start=0,end=0,count=t.length();
-        int[] found=new int[256],need=new int[256];
+        int minstart=0,minend=0,minsize=Integer.MAX_VALUE,count=0;
+        int[] need=new int[256],found=new int[256];
         for(int i=0;i<t.length();i++)
             need[t.charAt(i)]++;
-        while(true){
-            if(count==0){
-                while(found[s.charAt(start)]>need[s.charAt(start)]){
-                    found[s.charAt(start)]--;
-                    start++;
-                }
-                if(end-start+1<minsize){
-                    minend=end;
-                    minstart=start;
-                    minsize=end-start+1;
-                }
-            }
-            if(end<s.length()){
+        int start=0,end=0;
+        while(end<s.length()){
+            if(count<t.length()){
                 found[s.charAt(end)]++;
-                if(found[s.charAt(end)]<=need[s.charAt(end)]){
-                    count--;
-                }
+                if(need[s.charAt(end)]>=found[s.charAt(end)])
+                    count++;
                 end++;
             }
-            else break;
+            while(count==t.length()){
+                if(end-start+1<minsize){
+                    minsize=end-start+1;
+                    minstart=start;
+                    minend=end;
+                }
+                found[s.charAt(start)]--;
+                if(need[s.charAt(start)]>found[s.charAt(start)])
+                    count--;
+                start++;
+            }
         }
         if(minsize==Integer.MAX_VALUE) return "";
         return s.substring(minstart,minend);
