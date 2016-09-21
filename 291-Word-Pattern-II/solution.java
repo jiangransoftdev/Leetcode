576879c04330353c5b3ef24d4ca1b4ad8cf67c05
jiangran910126 <jiangran0126@gmail.com>
2016-09-20 21:16:57 -0500

    [291. Word Pattern II][Accepted]

--- 291-Word-Pattern-II/solution.java
+++ 291-Word-Pattern-II/solution.java
@@ -2,27 +2,28 @@ public class Solution {
     public boolean wordPatternMatch(String pattern, String str) {
         Map<Character,String> map=new HashMap<>();
         Set<String> set=new HashSet<>();
-        return helper(str,0,pattern,0,map,set);
+        return helper(pattern,0,str,map,set);
     }
-    public boolean helper(String str,int i,String pattern,int j,Map<Character,String> map,Set<String> set){
-        if(i==str.length()&&j==pattern.length()) return true;
-        if(i==str.length()||j==pattern.length()) return false;
-        char c=pattern.charAt(j);
+    public boolean helper(String pattern,int p,String str,Map<Character,String> map,Set<String> set){
+        if(p==pattern.length()&&str.length()==0) return true;
+        if(p==pattern.length()||str.length()==0) return false;
+        char c=pattern.charAt(p);
         if(map.containsKey(c)){
-            if(!str.startsWith(map.get(c),i)) return false;
-            return helper(str,i+map.get(c).length(),pattern,j+1,map,set);
+            if(str.startsWith(map.get(c),0)) return helper(pattern,p+1,str.substring(map.get(c).length()),map,set);
+            else return false;
         }
         else{
-            for(int k=i;k<str.length();k++){
-                String sub=str.substring(i,k+1);
-                if(set.contains(sub)) continue;
-                map.put(c,sub);
-                set.add(sub);
-                if(helper(str,k+1,pattern,j+1,map,set)) return true;
-                map.remove(c);
-                set.remove(sub);
-            }
+            for(int i=1;i<=str.length();i++){
+            String sub=str.substring(0,i);
+            if(set.contains(sub)) continue;
+            map.put(c,sub);
+            set.add(sub);
+            if(helper(pattern,p+1,str.substring(i),map,set)) return true;
+            map.remove(c);
+            set.remove(sub);
         }
+        }
+        
         return false;
     }
 }
