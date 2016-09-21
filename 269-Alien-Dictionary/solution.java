18f6fe58f858d6f80bedb62bd9c436676e7b3ece
jiangran910126 <jiangran0126@gmail.com>
2016-09-20 20:46:03 -0500

    [269. Alien Dictionary][Accepted]

--- 269-Alien-Dictionary/solution.java
+++ 269-Alien-Dictionary/solution.java
@@ -1,33 +1,32 @@
 public class Solution {
     public String alienOrder(String[] words) {
-        int[] visited=new int[26];
+        int[] visit=new int[26];
         boolean[][] adj=new boolean[26][26];
-        buildGraph(words,adj,visited);
         StringBuilder sb=new StringBuilder();
+        build(words,visit,adj);
         for(int i=0;i<26;i++){
-            if(visited[i]==0)
-                if(!dfs(adj,visited,sb,i)) return "";
+            if(visit[i]==0&&!helper(adj,visit,sb,i)) return "";
         }
         return sb.toString();
     }
-    public boolean dfs(boolean[][] adj,int[] visited,StringBuilder sb,int i){
-        visited[i]=1;
-
+    public boolean helper(boolean[][] adj,int[] visit,StringBuilder sb,int i){
+        if(visit[i]==1) return false;
+        visit[i]=1;
         for(int j=0;j<26;j++){
             if(adj[i][j]){
-                if(visited[j]==1) return false;
-                if(visited[j]==0)
-                    if(!dfs(adj,visited,sb,j)) return false;
+                if(visit[j]==1) return false;
+                if(visit[j]==0&&!helper(adj,visit,sb,j)) return false;
             }
         }
-        sb.insert(0,(char)(i+'a'));
-        visited[i]=2;
+        visit[i]=2;
+        sb.insert(0,(char)('a'+i));
         return true;
     }
-    public void buildGraph(String[] words,boolean[][] adj,int[] visited){
-        Arrays.fill(visited,-1);
+    
+    public void build(String[] words,int[] visit,boolean[][] adj){
+        Arrays.fill(visit,-1);
         for(int i=0;i<words.length;i++){
-            for(char c:words[i].toCharArray()) visited[c-'a']=0;
+            for(int j=0;j<words[i].length();j++) visit[words[i].charAt(j)-'a']=0;
             if(i>0){
                 String w1=words[i-1],w2=words[i];
                 int len=Math.min(w1.length(),w2.length());
