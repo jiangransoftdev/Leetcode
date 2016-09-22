12eec27dbf3f98bf88e7cd71a8efd424b98b65f4
jiangran910126 <jiangran0126@gmail.com>
2016-09-22 16:53:38 -0500

    [333. Largest BST Subtree][Accepted]

--- 333-Largest-BST-Subtree/solution.java
+++ 333-Largest-BST-Subtree/solution.java
@@ -30,7 +30,7 @@ public class Solution {
         if(left.size==-1||right.size==-1||root.val<=left.upper||root.val>=right.lower)
             return new Result(-1,0,0);
         int size=left.size+right.size+1;
-        max=Math.max(max,size);
+        max=Math.max(size,max);
         return new Result(size,Math.min(left.lower,root.val),Math.max(right.upper,root.val));
     }
 }
