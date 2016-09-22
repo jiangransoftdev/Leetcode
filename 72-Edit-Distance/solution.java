5e0ee23471afa484ae625ba3ae1cb1a3bebb925b
jiangran910126 <jiangran0126@gmail.com>
2016-09-22 17:16:15 -0500

    [72. Edit Distance][Accepted]

--- 72-Edit-Distance/solution.java
+++ 72-Edit-Distance/solution.java
@@ -1,19 +1,17 @@
 public class Solution {
     public int minDistance(String word1, String word2) {
-        int[][] dp=new int[word1.length()+1][word2.length()+1];
-        for(int i=0;i<=word1.length();i++)
-            dp[i][0]=i;
-        for(int i=0;i<=word2.length();i++)
+        int m=word1.length(),n=word2.length();
+        int[][] dp=new int[m+1][n+1];
+        for(int i=0;i<=n;i++)
             dp[0][i]=i;
-        for(int i=1;i<=word1.length();i++){
-            for(int j=1;j<=word2.length();j++){
-                if(word1.charAt(i-1)==word2.charAt(j-1))
-                    dp[i][j]=dp[i-1][j-1];
-                else{
-                    dp[i][j]=Math.min(dp[i-1][j-1],Math.min(dp[i-1][j],dp[i][j-1]))+1;
-                }
+        for(int i=0;i<=m;i++)
+            dp[i][0]=i;
+        for(int i=1;i<=m;i++){
+            for(int j=1;j<=n;j++){
+                if(word1.charAt(i-1)==word2.charAt(j-1)) dp[i][j]=dp[i-1][j-1];
+                else dp[i][j]=Math.min(dp[i-1][j-1],Math.min(dp[i-1][j],dp[i][j-1]))+1;
             }
         }
-        return dp[word1.length()][word2.length()];
+        return dp[m][n];
     }
 }
