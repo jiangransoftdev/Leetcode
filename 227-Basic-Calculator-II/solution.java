dd27c15b5a97b65a710c3858594cc9f6a8c8155e
jiangran910126 <jiangran0126@gmail.com>
2016-09-22 00:56:37 -0500

    [227. Basic Calculator II][Accepted]

--- 227-Basic-Calculator-II/solution.java
+++ 227-Basic-Calculator-II/solution.java
@@ -1,8 +1,7 @@
 public class Solution {
     public int calculate(String s) {
-        if(s.length()==0) return 0;
-        Stack<Integer> sta=new Stack<>();
         char sign='+';
+        Stack<Integer> sta=new Stack<>();
         int i=0;
         while(i<s.length()){
             int num=0;
@@ -14,17 +13,15 @@ public class Solution {
             switch(sign){
                 case '+':sta.push(num);break;
                 case '-':sta.push(-num);break;
-                case '*':sta.push(sta.pop()*num);break;
+                case '*':sta.push(num*sta.pop());break;
                 case '/':sta.push(sta.pop()/num);break;
                 default:
             }
-            if(i<s.length())
-                sign=s.charAt(i);
+            if(i<s.length()) sign=s.charAt(i);
             i++;
         }
         int res=0;
-        for(int num:sta)
-            res+=num;
+        for(int a:sta) res+=a;
         return res;
     }
 }
