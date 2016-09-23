edc80dcc1cd770644b2d573380ebed323800a57a
jiangran910126 <jiangran0126@gmail.com>
2016-09-22 21:47:05 -0500

    [373. Find K Pairs with Smallest Sums][Accepted]

--- 373-Find-K-Pairs-with-Smallest-Sums/solution.java
+++ 373-Find-K-Pairs-with-Smallest-Sums/solution.java
@@ -1,36 +1,30 @@
-class Sum{
-    int row,col,val;
-    public Sum(int val,int row,int col){
-        this.val=val;
+class Item{
+    int row,col,sum;
+    public Item(int row,int col,int sum){
         this.row=row;
         this.col=col;
+        this.sum=sum;
     }
 }
 public class Solution {
     public List<int[]> kSmallestPairs(int[] nums1, int[] nums2, int k) {
         List<int[]> res=new ArrayList<>();
-        if(nums1.length==0||nums2.length==0||k<=0) return res;
-        PriorityQueue<Sum> pq=new PriorityQueue<>(new Comparator<Sum>(){
-            public int compare(Sum a,Sum b){
-                return a.val-b.val;
+        if(nums1.length==0||nums2.length==0) return res;
+        PriorityQueue<Item> pq=new PriorityQueue<>(new Comparator<Item>(){
+            public int compare(Item a,Item b){
+                return a.sum-b.sum;
             }
         });
-        for(int i=0;i<Math.min(nums1.length,k);i++){
-            int row=i;
-            int col=0;
-            int val=nums1[row]+nums2[col];
-            pq.offer(new Sum(val,row,col));
-        }
-        for(int i=0;i<Math.min(nums1.length*nums2.length,k);i++){
-            Sum sum=pq.poll();
-            int row=sum.row;
-            int col=sum.col;
-            int[] pair={nums1[row],nums2[col]};
-            res.add(pair);
-            if(col<nums2.length-1){
-                int val=nums1[row]+nums2[col+1];
-                pq.offer(new Sum(val,row,col+1));
-            }
+        for(int i=0;i<nums1.length;i++)
+            pq.offer(new Item(i,0,nums2[0]+nums1[i]));
+        while(res.size()<k&&!pq.isEmpty()){
+            Item cur=pq.poll();
+            int row=cur.row,col=cur.col;
+            int[] sub=new int[2];
+            sub[0]=nums1[row];
+            sub[1]=nums2[col];
+            res.add(sub);
+            if(col+1<nums2.length) pq.offer(new Item(row,col+1,nums1[row]+nums2[col+1]));
         }
         return res;
     }
