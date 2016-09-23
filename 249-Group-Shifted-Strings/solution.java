37e50c19f430b16fc874a4c79d5a95619bd8ed5d
jiangran910126 <jiangran0126@gmail.com>
2016-09-22 22:53:09 -0500

    [249. Group Shifted Strings][Accepted]

--- 249-Group-Shifted-Strings/solution.java
+++ 249-Group-Shifted-Strings/solution.java
@@ -2,12 +2,12 @@ public class Solution {
     public List<List<String>> groupStrings(String[] strings) {
         Map<String,List<String>> map=new HashMap<>();
         Arrays.sort(strings);
-        for(String s:strings){
+        for(String string:strings){
             String key="";
-            for(int i=1;i<s.length();i++)
-                key+=String.format("%2d", (s.charAt(i) - s.charAt(i-1) + 26) % 26);
+            for(int i=1;i<string.length();i++)
+                key+=String.format("%2d",(string.charAt(i)-string.charAt(i-1)+26)%26);
             if(!map.containsKey(key)) map.put(key,new ArrayList<>());
-            map.get(key).add(s);
+            map.get(key).add(string);
         }
         return new ArrayList<List<String>>(map.values());
     }
