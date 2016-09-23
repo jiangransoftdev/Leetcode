b58b33b218c6f1cfd17147e8983b0c8f8a873347
jiangran910126 <jiangran0126@gmail.com>
2016-09-23 18:35:11 -0500

    [394. Decode String][Accepted]

--- 394-Decode-String/solution.java
+++ 394-Decode-String/solution.java
@@ -1,30 +1,33 @@
 public class Solution {
     public String decodeString(String s) {
+        if(s.length()==0) return "";
         Stack<Integer> count=new Stack<>();
         Stack<String> res=new Stack<>();
-        int i=0;
         res.push("");
+        int i=0;
         while(i<s.length()){
-            char c=s.charAt(i);
-            if(c>='0'&&c<='9'){
-                int start=i;
-                while(i+1<s.length()&&s.charAt(i+1)>='0'&&s.charAt(i+1)<='9') i++;
-                count.push(Integer.parseInt(s.substring(start,i+1)));
+            if(s.charAt(i)>='0'&&s.charAt(i)<='9'){
+                int num=s.charAt(i)-'0';
+                while(i+1<s.length()&&s.charAt(i+1)>='0'&&s.charAt(i+1)<='9'){
+                    num=num*10+s.charAt(i+1)-'0';
+                    i++;
+                }
+                count.push(num);
             }
-            else if(c=='[')
+            else if(s.charAt(i)=='['){
                 res.push("");
-            else if(c==']'){
+            }
+            else if(s.charAt(i)==']'){
                 StringBuilder sb=new StringBuilder();
-                int times=count.pop();
+                int time=count.pop();
                 String tmp=res.pop();
-                for(int j=0;j<times;j++)
+                for(int j=0;j<time;j++)
                     sb.append(tmp);
                 res.push(res.pop()+sb.toString());
             }
-            else
-                res.push(res.pop()+c);
+            else res.push(res.pop()+s.charAt(i));
             i++;
         }
-        return res.pop();
+        return res.peek();
     }
 }
