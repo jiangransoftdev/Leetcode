0c5d0fbf51ca580f9f7c4ff1e44954cd13a6a4d2
jiangran910126 <jiangran0126@gmail.com>
2016-09-22 22:59:01 -0500

    [335. Self Crossing][Accepted]

--- 335-Self-Crossing/solution.java
+++ 335-Self-Crossing/solution.java
@@ -1,9 +1,10 @@
 public class Solution {
     public boolean isSelfCrossing(int[] x) {
+        if(x.length==0) return false;
         for(int i=3;i<x.length;i++){
-            if(x[i-3]>=x[i-1]&&x[i]>=x[i-2]) return true;
-            if(i>3&&x[i-3]==x[i-1]&&x[i-4]+x[i]>=x[i-2]&&x[i-2]>=x[i-4]) return true;
-            if(i>4&&x[i-5]+x[i-1]>=x[i-3]&&x[i-3]>=x[i-1]&&x[i]+x[i-4]>=x[i-2]&&x[i-2]>=x[i-4]) return true;
+            if(x[i]>=x[i-2]&&x[i-3]>=x[i-1]) return true;
+            if(i>3&&(x[i]+x[i-4]>=x[i-2])&&x[i-1]==x[i-3]&&x[i-2]>=x[i-4]) return true;
+            if(i>4&&(x[i-4]+x[i]>=x[i-2])&&x[i-2]>=x[i-4]&&(x[i-5]+x[i-1]>=x[i-3])&&x[i-3]>=x[i-1]) return true;
         }
         return false;
     }
