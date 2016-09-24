564504f2d462c9b14b9d71609bee8bd6073167b3
jiangran910126 <jiangran0126@gmail.com>
2016-09-24 01:34:32 -0500

    [375. Guess Number Higher or Lower II][Accepted]

--- 375-Guess-Number-Higher-or-Lower-II/solution.java
+++ 375-Guess-Number-Higher-or-Lower-II/solution.java
@@ -1,11 +1,11 @@
 public class Solution {
     public int getMoneyAmount(int n) {
         int[][] dp=new int[n+1][n+1];
-        for(int L=n-1;L>0;L--){
+        for(int L=n-1;L>=1;L--){
             for(int R=L+1;R<=n;R++){
                 dp[L][R]=Integer.MAX_VALUE;
                 for(int i=L;i<R;i++)
-                    dp[L][R]=Math.min(dp[L][R],i+Math.max(dp[L][i-1],dp[i+1][R]));
+                    dp[L][R]=Math.min(dp[L][R],Math.max(dp[L][i-1],dp[i+1][R])+i);
             }
         }
         return dp[1][n];
