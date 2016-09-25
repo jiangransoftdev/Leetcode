09e91ab17f237183423be655df313cbde72309ef
jiangran910126 <jiangran0126@gmail.com>
2016-09-24 23:48:37 -0500

    [247. Strobogrammatic Number II][Accepted]

--- 247-Strobogrammatic-Number-II/solution.java
+++ 247-Strobogrammatic-Number-II/solution.java
@@ -1,31 +1,30 @@
 public class Solution {
     public List<String> findStrobogrammatic(int n) {
-        List<String> res=new ArrayList<>();
         return helper(n,n);
     }
     public List<String> helper(int n,int origin){
-        if(n<=1){
-            List<String> res=new ArrayList<>();
-            if(n==0){
-                res.add("");
-                return res;
-            }
-            else{
-                res.add("0");
-                res.add("1");
-                res.add("8");
-                return res;
-            }
+        List<String> res=new ArrayList<>();
+        if(n==0){
+            String s="";
+            res.add(s);
+            return res;
         }
-        List<String> newres=new ArrayList<>();
+        if(n==1){
+            res.add("0");
+            res.add("1");
+            res.add("8");
+            return res;
+        }
         List<String> list=helper(n-2,origin);
         for(String s:list){
-            if(n!=origin) newres.add("0"+s+"0");
-            newres.add("1"+s+"1");
-            newres.add("6"+s+"9");
-            newres.add("8"+s+"8");
-            newres.add("9"+s+"6");
+            if(n!=origin){
+                res.add("0"+s+"0");
+            }
+            res.add("1"+s+"1");
+            res.add("6"+s+"9");
+            res.add("9"+s+"6");
+            res.add("8"+s+"8");
         }
-        return newres;
+        return res;
     }
 }
