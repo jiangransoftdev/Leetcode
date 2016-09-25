81b2e65c500a8b56327c00f84b1ff46ba14ea45b
jiangran910126 <jiangran0126@gmail.com>
2016-09-24 23:41:06 -0500

    [73. Set Matrix Zeroes][Accepted]

--- 73-Set-Matrix-Zeroes/solution.java
+++ 73-Set-Matrix-Zeroes/solution.java
@@ -1,25 +1,25 @@
 public class Solution {
     public void setZeroes(int[][] matrix) {
-        if(matrix.length==0) return;
-        int m=matrix.length,n=matrix[0].length;
-        int index=0;
-        for(;index<n;index++)
-            if(matrix[0][index]==0) break;
+        int m=matrix.length;
+        if(m==0) return;
+        int n=matrix[0].length;
+        int count=0;
+        for(int i=0;i<n;i++){
+            if(matrix[0][i]==0) count++;
+        }
         for(int i=1;i<m;i++){
             for(int j=0;j<n;j++){
                 if(matrix[i][j]==0){
-                    matrix[i][0]=0;
                     matrix[0][j]=0;
+                    matrix[i][0]=0;
                 }
             }
         }
         for(int i=1;i<m;i++){
             for(int j=n-1;j>=0;j--){
-                if(matrix[i][0]==0||matrix[0][j]==0)
-                    matrix[i][j]=0;
+                if(matrix[i][0]==0||matrix[0][j]==0) matrix[i][j]=0;
             }
         }
-        
-        if(index<n) Arrays.fill(matrix[0],0);
+        if(count>0) Arrays.fill(matrix[0],0);
     }
 }
