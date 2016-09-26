446296e15c1ebb8966d25e791274b032c3d0795c
jiangran910126 <jiangran0126@gmail.com>
2016-09-26 01:21:26 -0500

    [106. Construct Binary Tree from Inorder and Postorder Traversal][Accepted]

--- 106-Construct-Binary-Tree-from-Inorder-and-Postorder-Traversal/solution.java
+++ 106-Construct-Binary-Tree-from-Inorder-and-Postorder-Traversal/solution.java
@@ -14,12 +14,14 @@ public class Solution {
             map.put(inorder[i],i);
         return helper(inorder,0,inorder.length-1,postorder,0,postorder.length-1,map);
     }
-    public TreeNode helper(int[] inorder,int inl,int inr,int[] postorder,int pol,int por,Map<Integer,Integer> map){
+    public TreeNode helper(int[] in,int inl,int inr,int[] post,int pol,int por,Map<Integer,Integer> map){
         if(inl>inr) return null;
-        TreeNode root=new TreeNode(postorder[por]);
-        int rsize=inr-map.get(root.val);
-        root.right=helper(inorder,inr-rsize+1,inr,postorder,por-rsize,por-1,map);
-        root.left=helper(inorder,inl,inr-rsize-1,postorder,pol,por-rsize-1,map);
+        TreeNode root=new TreeNode(post[por]);
+        int rsize=inr-map.get(post[por]);
+        TreeNode right=helper(in,inr-rsize+1,inr,post,por-rsize,por-1,map);
+        TreeNode left=helper(in,inl,inr-rsize-1,post,pol,por-rsize-1,map);
+        root.left=left;
+        root.right=right;
         return root;
     }
 }
