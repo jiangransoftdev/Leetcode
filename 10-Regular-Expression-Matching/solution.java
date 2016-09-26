705b282f4c2ec3502686190cba87a7e0ba00de08
jiangran910126 <jiangran0126@gmail.com>
2016-09-26 14:44:29 -0500

    [10. Regular Expression Matching][Accepted]

--- 10-Regular-Expression-Matching/solution.java
+++ 10-Regular-Expression-Matching/solution.java
@@ -10,11 +10,11 @@ public class Solution {
             for(int j=1;j<=p.length();j++){
                 if(s.charAt(i-1)==p.charAt(j-1)||p.charAt(j-1)=='.')
                     dp[i][j]=dp[i-1][j-1];
-                else if(p.charAt(j-1)=='*'){
-                    if(s.charAt(i-1)!=p.charAt(j-2)&&p.charAt(j-2)!='.')
-                        dp[i][j]=dp[i][j-2];
-                    else
-                        dp[i][j]=dp[i][j-2]||dp[i-1][j]||dp[i][j-1];
+                else{
+                    if(p.charAt(j-1)=='*'){
+                        if(p.charAt(j-2)=='.'||p.charAt(j-2)==s.charAt(i-1)) dp[i][j]=dp[i][j-1]||dp[i-1][j]||dp[i][j-2];
+                        else dp[i][j]=dp[i][j-2];
+                    }
                 }
             }
         }
