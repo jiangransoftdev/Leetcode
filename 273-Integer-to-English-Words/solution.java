fd9013f59f1255a7c7ccb5c44c3620dd3f3a4a30
jiangran910126 <jiangran0126@gmail.com>
2016-09-26 15:49:44 -0500

    [273. Integer to English Words][Accepted]

--- 273-Integer-to-English-Words/solution.java
+++ 273-Integer-to-English-Words/solution.java
@@ -1,14 +1,15 @@
 public class Solution {
-    final String[] Less20={"", "One", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine", "Ten", "Eleven", "Twelve", "Thirteen", "Fourteen", "Fifteen", "Sixteen", "Seventeen", "Eighteen", "Nineteen"};
-	    final String[] Tens={"", "Ten", "Twenty", "Thirty", "Forty", "Fifty", "Sixty", "Seventy", "Eighty", "Ninety"};
-	    final String[] Thousand={"","Thousand","Million","Billion"};
+    String[] less20={"","One","Two","Three","Four","Five","Six","Seven","Eight","Nine","Ten","Eleven","Twelve","Thirteen","Fourteen","Fifteen","Sixteen","Seventeen","Eighteen","Nineteen"};
+    String[] tens={"","Ten","Twenty","Thirty","Forty","Fifty","Sixty","Seventy","Eighty","Ninety"};
+    String[] thousands={"","Thousand","Million","Billion"};
     public String numberToWords(int num) {
         if(num==0) return "Zero";
         String word="";
         int i=0;
         while(num>0){
-            if(num%1000!=0)
-                word=helper(num%1000)+Thousand[i]+" "+word;
+            if(num%1000!=0){
+                word=helper(num%1000)+thousands[i]+" "+word;
+            }
             num/=1000;
             i++;
         }
@@ -16,9 +17,8 @@ public class Solution {
     }
     public String helper(int num){
         if(num==0) return "";
-        if(num<20) return Less20[num]+" ";
-        if(num<100) return Tens[num/10]+" "+helper(num%10);
-        return Less20[num/100]+" Hundred "+helper(num%100);
-        
+        if(num<20) return less20[num%20]+" ";
+        else if(num<100) return tens[num/10]+" "+helper(num%10);
+        else return less20[num/100]+" Hundred "+helper(num%100);
     }
 }
