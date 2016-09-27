ea2fa596097de5b8e4564ebf69f6abb1be597133
jiangran910126 <jiangran0126@gmail.com>
2016-09-27 02:42:04 -0500

    [391. Perfect Rectangle][Accepted]

--- 391-Perfect-Rectangle/solution.java
+++ 391-Perfect-Rectangle/solution.java
@@ -1,31 +1,52 @@
+class Event{
+    int time;
+    int[] rect;
+    public Event(int time,int[] rect){
+        this.time=time;
+        this.rect=rect;
+    }
+}
 public class Solution {
     public boolean isRectangleCover(int[][] rectangles) {
-        Set<String> set=new HashSet<>();
-        int area=0,minx=Integer.MAX_VALUE,maxx=Integer.MIN_VALUE,miny=Integer.MAX_VALUE,maxy=Integer.MIN_VALUE;
+        PriorityQueue<Event> pq=new PriorityQueue<>(new Comparator<Event>(){
+            public int compare(Event a,Event b){
+                if(a.time!=b.time) return a.time-b.time;
+                return a.rect[0]-b.rect[0];
+            }
+        });
+        int[] border={Integer.MAX_VALUE,Integer.MIN_VALUE};
         for(int[] rect:rectangles){
-            minx=Math.min(minx,rect[0]);
-            maxx=Math.max(maxx,rect[2]);
-            miny=Math.min(miny,rect[1]);
-            maxy=Math.max(maxy,rect[3]);
-            area+=(rect[2]-rect[0])*(rect[3]-rect[1]);
-            String t1=rect[0]+","+rect[1];
-            String t2=rect[2]+","+rect[1];
-            String t3=rect[0]+","+rect[3];
-            String t4=rect[2]+","+rect[3];
-            if(set.contains(t1)) set.remove(t1);
-            else set.add(t1);
-            if(set.contains(t2)) set.remove(t2);
-            else set.add(t2);
-            if(set.contains(t3)) set.remove(t3);
-            else set.add(t3);
-            if(set.contains(t4)) set.remove(t4);
-            else set.add(t4);
+            Event a=new Event(rect[0],rect);
+            Event b=new Event(rect[2],rect);
+            pq.offer(a);
+            pq.offer(b);
+            if(rect[1]<border[0]) border[0]=rect[1];
+            if(rect[3]>border[1]) border[1]=rect[3];
         }
-        String t1=minx+","+miny;
-        String t2=minx+","+maxy;
-        String t3=maxx+","+miny;
-        String t4=maxx+","+maxy;
-        if(!set.contains(t1)||!set.contains(t2)||!set.contains(t3)||!set.contains(t4)||set.size()!=4) return false;
-        return (maxx-minx)*(maxy-miny)==area;
+        TreeSet<int[]> set=new TreeSet<>(new Comparator<int[]>(){
+            public int compare(int[] a,int[] b){
+                if(a[3]<=b[1]) return 1;
+                else if(b[3]<=a[1]) return -1;
+                else return 0;
+            }
+        }); 
+        int y=0;
+        while(!pq.isEmpty()){
+            int time=pq.peek().time;
+            while(!pq.isEmpty()&&pq.peek().time==time){
+                Event e=pq.poll();
+                int[] rect=e.rect;
+                if(time==rect[2]){
+                    set.remove(rect);
+                    y-=rect[3]-rect[1];
+                }
+                else{
+                    if(!set.add(rect)) return false;
+                    y+=rect[3]-rect[1];
+                }
+            }
+            if(!pq.isEmpty()&&y!=border[1]-border[0]) return false;
+        }
+        return true;
     }
 }
