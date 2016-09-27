20efd6e36331708251761e8880291b693935f62e
jiangran910126 <jiangran0126@gmail.com>
2016-09-27 14:48:27 -0500

    [41. First Missing Positive][Accepted]

--- 41-First-Missing-Positive/solution.java
+++ 41-First-Missing-Positive/solution.java
@@ -1,14 +1,16 @@
 public class Solution {
     public int firstMissingPositive(int[] nums) {
-        if(nums.length==0) return 1;
-        int i=0;
+        int i=0,prev=Integer.MIN_VALUE;
         while(i<nums.length){
-            if(nums[i]==i+1||(nums[i]<1||nums[i]>nums.length)) i++;
+            if(nums[i]==i+1||nums[i]>nums.length||nums[i]<=0)
+                i++;
             else if(nums[i]!=nums[nums[i]-1]) swap(nums,i,nums[i]-1);
             else i++;
+                
         }
-        for(i=0;i<nums.length;i++){
-            if(i+1!=nums[i]) break;
+        i=0;
+        for(;i<nums.length;i++){
+            if(nums[i]!=i+1) break;
         }
         return i+1;
     }
