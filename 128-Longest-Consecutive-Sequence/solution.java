f8745b7e3ecf064c5f346236c7f701323575b126
jiangran910126 <jiangran0126@gmail.com>
2016-09-28 15:12:35 -0500

    [128. Longest Consecutive Sequence][Accepted]

--- 128-Longest-Consecutive-Sequence/solution.java
+++ 128-Longest-Consecutive-Sequence/solution.java
@@ -5,49 +5,45 @@ class UF{
     public UF(int n){
         id=new int[n];
         sz=new int[n];
-        max=1;
         for(int i=0;i<n;i++){
             id[i]=i;
             sz[i]=1;
         }
+        max=1;
     }
     public int find(int p){
-        while(id[p]!=p){
+        while(p!=id[p]){
             id[p]=id[id[p]];
             p=id[p];
         }
         return p;
     }
-    public boolean isConnect(int p,int q){
-        if(find(p)==find(q)) return true;
-        return false;
-    }
-    public void connect(int p,int q){
-        int i=find(p);
-        int j=find(q);
-        if(i==j) return;
-        if(sz[i]>sz[j]){
-            id[j]=i;
-            sz[i]+=sz[j];
-            max=Math.max(max,sz[i]);
+    public void union(int p,int q){
+        int rootp=find(p),rootq=find(q);
+        if(rootp==rootq) return;
+        if(sz[rootp]>sz[rootq]){
+            id[rootq]=rootp;
+            sz[rootp]+=sz[rootq];
+            max=Math.max(max,sz[rootp]);
         }
         else{
-            id[i]=j;
-            sz[j]+=sz[i];
-            max=Math.max(max,sz[j]);
+            id[rootp]=rootq;
+            sz[rootq]+=sz[rootp];
+            max=Math.max(max,sz[rootq]);
         }
+       
     }
 }
 public class Solution {
+    Map<Integer,Integer> map=new HashMap<>();
     public int longestConsecutive(int[] nums) {
         if(nums.length==0) return 0;
         UF uf=new UF(nums.length);
-        Map<Integer,Integer> map=new HashMap<>();
         for(int i=0;i<nums.length;i++){
             if(map.containsKey(nums[i])) continue;
             map.put(nums[i],i);
-            if(map.containsKey(nums[i]+1)) uf.connect(i,map.get(nums[i]+1));
-            if(map.containsKey(nums[i]-1)) uf.connect(i,map.get(nums[i]-1));
+            if(map.containsKey(nums[i]-1)) uf.union(map.get(nums[i]),map.get(nums[i]-1));
+            if(map.containsKey(nums[i]+1)) uf.union(map.get(nums[i]),map.get(nums[i]+1));
         }
         return uf.max;
     }
