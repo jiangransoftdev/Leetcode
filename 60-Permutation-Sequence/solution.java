408cc8afbe2bb5c1a14898913bb28b92b109b0a0
jiangran910126 <jiangran0126@gmail.com>
2016-09-28 03:19:19 -0500

    [60. Permutation Sequence][Accepted]

--- 60-Permutation-Sequence/solution.java
+++ 60-Permutation-Sequence/solution.java
@@ -1,17 +1,17 @@
 public class Solution {
     public String getPermutation(int n, int k) {
-        int fact=1;
+        List<Integer> num=new ArrayList<>();
         StringBuilder sb=new StringBuilder();
-        List<Integer> res=new ArrayList<>();
+        int fact=1;
         for(int i=1;i<=n;i++){
             fact*=i;
-            res.add(i);
+            num.add(i);
         }
             
         for(int i=n,l=k-1;i>0;i--){
             fact/=i;
             int index=l/fact;
-            sb.append(res.remove(index));
+            sb.append(num.remove(index));
             l-=index*fact;
         }
         return sb.toString();
