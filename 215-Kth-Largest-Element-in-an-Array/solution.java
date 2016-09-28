313845e2077b453161b5be588016ccb6e36110d2
jiangran910126 <jiangran0126@gmail.com>
2016-09-28 16:47:48 -0500

    [215. Kth Largest Element in an Array][Accepted]

--- 215-Kth-Largest-Element-in-an-Array/solution.java
+++ 215-Kth-Largest-Element-in-an-Array/solution.java
@@ -1,10 +1,27 @@
 public class Solution {
     public int findKthLargest(int[] nums, int k) {
-        PriorityQueue<Integer> pq=new PriorityQueue<>();
-        for(int num:nums){
-            pq.offer(num);
-            if(pq.size()>k) pq.poll();
-        }
-        return pq.peek();
+        int n=nums.length;
+        int p=quickSelect(nums,0,n-1,n-k+1);
+        return nums[p];
+    }
+    public int quickSelect(int[] num,int low,int high,int k){
+        int l=low;
+		int h=high;
+		int pavot=num[low];
+		while(l<h){
+			while(pavot<=num[h]&&l<h){
+				h--;
+			}
+			num[l]=num[h];
+			while(pavot>=num[l]&&l<h){
+				l++;
+			}
+			num[h]=num[l];
+		}
+		num[l]=pavot;
+        int m=l-low+1;
+        if(m==k) return l;
+        else if(m>k) return quickSelect(num,low,l-1,k);
+        else return quickSelect(num,l+1,high,k-m);
     }
 }
