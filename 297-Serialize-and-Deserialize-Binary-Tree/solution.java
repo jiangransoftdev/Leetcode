5b223d715abe6777d5cb9a3a839c6983cdbe1719
jiangran910126 <jiangran0126@gmail.com>
2016-09-27 21:00:48 -0500

    [297. Serialize and Deserialize Binary Tree][Accepted]

--- 297-Serialize-and-Deserialize-Binary-Tree/solution.java
+++ 297-Serialize-and-Deserialize-Binary-Tree/solution.java
@@ -8,33 +8,38 @@
  * }
  */
 public class Codec {
+    
     // Encodes a tree to a single string.
     public String serialize(TreeNode root) {
         StringBuilder sb=new StringBuilder();
-        shelper(root,sb);
+        helper(root,sb);
         return sb.toString();
     }
-    public void shelper(TreeNode root,StringBuilder sb){
+    public void helper(TreeNode root,StringBuilder sb){
         if(root==null){
-            sb.append("null ");
+            sb.append("null,");
             return;
-        }
-        sb.append(root.val+" ");
-        shelper(root.left,sb);
-        shelper(root.right,sb);
+        } 
+        sb.append(root.val+",");
+        helper(root.left,sb);
+        helper(root.right,sb);
     }
     // Decodes your encoded data to tree.
     public TreeNode deserialize(String data) {
-        String[] sdata=data.split(" ");
-        int[] index=new int[1];
-        return dhelper(sdata,index);
+        String[] strs=data.split(",");
+        int[] i=new int[1];
+        return dhelper(strs,i);
     }
-    public TreeNode dhelper(String[] data,int[] index){
-        String s=data[index[0]++];
-        if(s.equals("null")) return null;
-        TreeNode root=new TreeNode(Integer.parseInt(s));
-        root.left=dhelper(data,index);
-        root.right=dhelper(data,index);
+    public TreeNode dhelper(String[] data,int[] i){
+        if(data[i[0]].equals("null")){
+            i[0]++;
+            return null;
+        }
+        TreeNode root=new TreeNode(Integer.parseInt(data[i[0]++]));
+        TreeNode left=dhelper(data,i);
+        TreeNode right=dhelper(data,i);
+        root.left=left;
+        root.right=right;
         return root;
     }
 }
