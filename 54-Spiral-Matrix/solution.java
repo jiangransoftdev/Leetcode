dd2de304709981726235921c3d8db0748786ff2d
jiangran910126 <jiangran0126@gmail.com>
2016-09-28 15:34:12 -0500

    [54. Spiral Matrix][Accepted]

--- 54-Spiral-Matrix/solution.java
+++ 54-Spiral-Matrix/solution.java
@@ -1,31 +1,26 @@
 public class Solution {
     public List<Integer> spiralOrder(int[][] matrix) {
         List<Integer> res=new ArrayList<>();
-        int m=matrix.length;
-        if(m==0) return res;
-        int n=matrix[0].length;
-        helper(matrix,0,m,n,res);
+        if(matrix.length==0) return res;
+        int m=matrix.length,n=matrix[0].length;
+        helper(matrix,0,0,m,n,res);
         return res;
     }
-    public void helper(int[][] matrix,int offside,int msize,int nsize,List<Integer> res){
-        if(msize<=1){
-            for(int i=0;i<nsize&&msize==1;i++)
-                res.add(matrix[offside][i+offside]);
+    public void helper(int[][] matrix,int startm,int startn,int m,int n,List<Integer> res){
+        if(m==0||n==0) return;
+        if(m==1||n==1){
+            if(m==1){
+                for(int i=0;i<n;i++) res.add(matrix[startm][startn+i]);
+            }
+            else if(n==1){
+                for(int i=0;i<m;i++) res.add(matrix[startm+i][startn]);
+            }
             return;
         }
-        if(nsize<=1){
-            for(int i=0;i<msize&&nsize==1;i++)
-                res.add(matrix[offside+i][offside]);
-            return;
-        }
-        for(int i=0;i<nsize;i++)
-            res.add(matrix[offside][i+offside]);
-        for(int i=1;i<msize-1;i++)
-            res.add(matrix[offside+i][offside+nsize-1]);
-        for(int i=0;i<nsize;i++)
-            res.add(matrix[offside+msize-1][offside+nsize-1-i]);
-        for(int i=1;i<msize-1;i++)
-            res.add(matrix[offside+msize-1-i][offside]);
-        helper(matrix,offside+1,msize-2,nsize-2,res);
+        for(int i=0;i<n;i++) res.add(matrix[startm][startn+i]);
+        for(int i=0;i<m-2;i++) res.add(matrix[startm+1+i][startn+n-1]);
+        for(int i=0;i<n;i++) res.add(matrix[startm+m-1][startn+n-1-i]);
+        for(int i=0;i<m-2;i++) res.add(matrix[startm+m-2-i][startn]);
+        helper(matrix,startm+1,startn+1,m-2,n-2,res);
     }
 }
