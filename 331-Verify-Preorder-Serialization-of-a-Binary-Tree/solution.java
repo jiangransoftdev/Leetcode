cb6ee890be4b476b80af67113f4c86534376bf23
jiangran910126 <jiangran0126@gmail.com>
2016-09-27 21:14:04 -0500

    [331. Verify Preorder Serialization of a Binary Tree][Accepted]

--- 331-Verify-Preorder-Serialization-of-a-Binary-Tree/solution.java
+++ 331-Verify-Preorder-Serialization-of-a-Binary-Tree/solution.java
@@ -1,10 +1,10 @@
 public class Solution {
     public boolean isValidSerialization(String preorder) {
-        String[] str=preorder.split(",");
+        String[] nodes=preorder.split(",");
         int diff=1;
-        for(String s:str){
+        for(String node:nodes){
             if(--diff<0) return false;
-            if(!s.equals("#")) diff+=2;
+            if(!node.equals("#")) diff+=2;
         }
         return diff==0;
     }
