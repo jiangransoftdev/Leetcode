805dcd56731c7c4558bec0b1cef421629c45f7b5
jiangran910126 <jiangran0126@gmail.com>
2016-09-29 15:18:32 -0500

    [324. Wiggle Sort II][Accepted]

--- 324-Wiggle-Sort-II/solution.java
+++ 324-Wiggle-Sort-II/solution.java
@@ -1,16 +1,26 @@
 public class Solution {
     public void wiggleSort(int[] nums) {
-        int m=getKth(nums,0,nums.length-1,nums.length/2);
+        int m=getKth(nums,0,nums.length-1,(nums.length+1)/2);
         int mid=nums[m];
-        int i=0,j=0,k=nums.length-1;
-        while(j<=k){
-            if(nums[j]>mid)
-                swap(nums,i++,j++);
-            else if(nums[j]<mid)
-                swap(nums,j,k--);
-            else j++;
+        int n=nums.length;
+	    int i = 0, j = 0, k = n - 1;
+        while (j <= k) {
+            if (nums[vitualIndex(j,n)] > mid)
+                swap(nums,vitualIndex(i++,n), vitualIndex(j++,n));
+            else if (nums[vitualIndex(j,n)] < mid)
+                swap(nums,vitualIndex(j,n), vitualIndex(k--,n));
+            else
+                j++;
         }
     }
+    public int vitualIndex(int i,int n){
+	    return (1+2*i)%(n|1);
+    }
+    public void swap(int[] nums,int i,int j){
+	int tmp=nums[i];
+	nums[i]=nums[j];
+	nums[j]=tmp;
+}
     public int getKth(int[] nums,int low,int high,int k){
         int l=low,h=high;
         int pivot=nums[l];
@@ -20,11 +30,11 @@ public class Solution {
             nums[l]=nums[h];
             while(l<h&&nums[l]<=pivot)
                 l++;
-            nums[h]=num[l];
+            nums[h]=nums[l];
         }
         nums[l]=pivot;
         int m=l-low+1;
-        if(m==k) return m;
+        if(m==k) return l;
         else if(m>k) return getKth(nums,low,l-1,k);
         else return getKth(nums,l+1,high,k-m);
     }
