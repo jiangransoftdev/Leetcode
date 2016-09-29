ba7725d682aef733e50991d3bc5b1ad707adbdf1
jiangran910126 <jiangran0126@gmail.com>
2016-09-29 16:59:36 -0500

    [282. Expression Add Operators][Accepted]

--- 282-Expression-Add-Operators/solution.java
+++ 282-Expression-Add-Operators/solution.java
@@ -1,32 +1,34 @@
 public class Solution {
     public List<String> addOperators(String num, int target) {
         List<String> res=new ArrayList<>();
-        if(num.length()==0) return res;
         helper(num,target,0,0,new StringBuilder(),res);
         return res;
     }
-    public void helper(String num,int target,long cur,long pre,StringBuilder solu,List<String> res){
-        if(cur==target&&num.length()==0){
-            res.add(solu.toString());
+    public void helper(String num,int target,long curnum,long prenum,StringBuilder sb,List<String> res){
+        if(num.length()==0&&curnum==target){
+            res.add(sb.toString());
             return;
         }
+        int len=sb.length();
         for(int i=1;i<=num.length();i++){
-            String curstr=num.substring(0,i);
-            if(curstr.length()>1&&curstr.charAt(0)=='0') return;
-            long curnum=Long.parseLong(curstr);
-            String next=num.substring(i);
-            int len=solu.length();
-            if(len>0){
-                helper(next,target,cur-pre+pre*curnum,pre*curnum,solu.append("*").append(curnum),res);
-                solu.setLength(len);
-                helper(next,target,cur+curnum,curnum,solu.append("+").append(curnum),res);
-                solu.setLength(len);
-                helper(next,target,cur-curnum,-curnum,solu.append("-").append(curnum),res);
-                solu.setLength(len);
+            String next=num.substring(0,i);
+            if(next.length()>1&&next.charAt(0)=='0') break;
+            long cur=Long.parseLong(next);
+            if(sb.length()!=0){
+                sb.append("+"+next);
+                helper(num.substring(i),target,curnum+cur,cur,sb,res);
+                sb.setLength(len);
+                sb.append("-"+next);
+                helper(num.substring(i),target,curnum-cur,-cur,sb,res);
+                sb.setLength(len);
+                sb.append("*"+next);
+                helper(num.substring(i),target,curnum-prenum+prenum*cur,prenum*cur,sb,res);
+                sb.setLength(len);
             }
             else{
-                helper(next,target,curnum,curnum,solu.append(curnum),res);
-                solu.setLength(len);
+                sb.append(next);
+                helper(num.substring(i),target,cur,cur,sb,res);
+                sb.setLength(len);
             }
         }
     }
