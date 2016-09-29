60449613a7c7aa4b14090eda0c4d27e1c55c5e76
jiangran910126 <jiangran0126@gmail.com>
2016-09-28 22:45:03 -0500

    [336. Palindrome Pairs][Accepted]

--- 336-Palindrome-Pairs/solution.java
+++ 336-Palindrome-Pairs/solution.java
@@ -1,28 +1,53 @@
+class TrieNode{
+    TrieNode[] child;
+    int index;
+    List<Integer> list;
+    public TrieNode(){
+        child=new TrieNode[26];
+        index=-1;
+        list=new ArrayList<>();
+    }
+}
 public class Solution {
     public List<List<Integer>> palindromePairs(String[] words) {
         List<List<Integer>> res=new ArrayList<>();
-        Map<String,Integer> map=new HashMap<>();
+        TrieNode root=new TrieNode();
         for(int i=0;i<words.length;i++)
-            map.put(words[i],i);
-        for(int i=0;i<words.length;i++){
-            int l=0,r=0;
-            while(l<=r){
-                String s=words[i].substring(l,r);
-                Integer j=map.get(new StringBuilder(s).reverse().toString());
-                if(j!=null&&i!=j&&isPalindrome(words[i].substring(l==0?r:0,l==0?words[i].length():l)))
-                    res.add(Arrays.asList(l==0?new Integer[]{i,j}:new Integer[]{j,i}));
-                if(r<words[i].length())
-                    r++;
-                else l++;
-            }
-        }
+            addNode(words,i,root);
+        for(int i=0;i<words.length;i++)
+            searchNode(words,i,root,res);
         return res;
     }
-    public boolean isPalindrome(String s){
-        int i=0,j=s.length()-1;
+    public void addNode(String[] words,int i,TrieNode root){
+        String word=words[i];
+        for(int j=word.length()-1;j>=0;j--){
+            int index=word.charAt(j)-'a';
+            if(root.child[index]==null)
+                root.child[index]=new TrieNode();
+            if(isPalindrome(word,0,j))
+                root.list.add(i);
+            root=root.child[index];
+        }
+        root.index=i;
+        root.list.add(i);
+    }
+    public boolean isPalindrome(String s,int i,int j){
         while(i<j){
             if(s.charAt(i++)!=s.charAt(j--)) return false;
         }
         return true;
     }
+    public void searchNode(String[] words,int index,TrieNode root,List<List<Integer>> res){
+        for(int i=0;i<words[index].length();i++){
+            if(root.index>=0&&root.index!=index&&isPalindrome(words[index],i,words[index].length()-1))
+                res.add(Arrays.asList(index,root.index));
+            root=root.child[words[index].charAt(i)-'a'];
+            if(root==null) return;
+        }
+        for(int j:root.list){
+            if(index==j) continue;
+            res.add(Arrays.asList(index,j));
+        }
+    }
+    
 }
