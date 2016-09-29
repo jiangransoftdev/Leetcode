fc2e0cf2853de3d13849999215d065ec5f258dc4
jiangran910126 <jiangran0126@gmail.com>
2016-09-29 00:53:53 -0500

    [316. Remove Duplicate Letters][Accepted]

--- 316-Remove-Duplicate-Letters/solution.java
+++ 316-Remove-Duplicate-Letters/solution.java
@@ -1,18 +1,16 @@
 public class Solution {
     public String removeDuplicateLetters(String s) {
-        if(s.length()<=1) return s;
         int[] count=new int[26];
         char[] schar=s.toCharArray();
         StringBuilder sb=new StringBuilder();
-        for(char c:schar)
-            count[c-'a']++;
         boolean[] visit=new boolean[26];
-        int index=0;
+        for(int i=0;i<s.length();i++)
+            count[s.charAt(i)-'a']++;
         for(char c:schar){
-            index=c-'a';
+            int index=c-'a';
             count[index]--;
             if(visit[index]) continue;
-            while(sb.length()>0&&c<sb.charAt(sb.length()-1)&&count[sb.charAt(sb.length()-1)-'a']!=0){
+            while((sb.length()>0)&&(c<sb.charAt(sb.length()-1))&&count[sb.charAt(sb.length()-1)-'a']>0){
                 visit[sb.charAt(sb.length()-1)-'a']=false;
                 sb.deleteCharAt(sb.length()-1);
             }
