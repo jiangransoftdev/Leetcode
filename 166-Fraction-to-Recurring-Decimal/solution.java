6e14253472f82781115cec8d339a0d1c689a04c6
jiangran910126 <jiangran0126@gmail.com>
2016-09-30 01:01:05 -0500

    [166. Fraction to Recurring Decimal][Accepted]

--- 166-Fraction-to-Recurring-Decimal/solution.java
+++ 166-Fraction-to-Recurring-Decimal/solution.java
@@ -9,12 +9,11 @@ public class Solution {
         StringBuilder sb=new StringBuilder();
         long residue=num%denom;
         while(!map.containsKey(residue)){
-            map.put(residue,i);
+            map.put(residue,i++);
             long digit=residue*10/denom;
             sb.append(digit);
             residue=residue*10-denom*digit;
             if(residue==0) return (sign==-1?"-":"")+String.valueOf((num/denom))+"."+sb.toString();
-            i++;
         }
         sb.insert(map.get(residue),"(");
         return (sign==-1?"-":"")+String.valueOf((num/denom))+"."+sb.toString()+")";
