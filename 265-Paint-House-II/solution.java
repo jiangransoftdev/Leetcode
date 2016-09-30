3b61b88ef256e918899a5aa30cac015768b619b3
jiangran910126 <jiangran0126@gmail.com>
2016-09-30 18:46:24 -0500

    [265. Paint House II][Accepted]

--- 265-Paint-House-II/solution.java
+++ 265-Paint-House-II/solution.java
@@ -1,27 +1,25 @@
 public class Solution {
     public int minCostII(int[][] costs) {
         if(costs.length==0) return 0;
-        int[][] dp=new int[costs.length][costs[0].length];
-        for(int i=0;i<costs[0].length;i++)
-        	dp[0][i]=costs[0][i];
-        for(int i=1;i<costs.length;i++){
-            int[] minarr=new int[costs[0].length];
-            int min=Integer.MAX_VALUE;
-            for(int j=0;j<costs[0].length;j++){
-                minarr[j]=min;
-                min=Math.min(min,dp[i-1][j]);
+        int n=costs.length,k=costs[0].length;
+        int min1=-1,min2=-1;
+        for(int i=0;i<n;i++){
+            int last1=min1,last2=min2;
+            min1=-1;
+            min2=-1;
+            for(int j=0;j<k;j++){
+                if(j!=last1)
+                    costs[i][j]+=last1<0?0:costs[i-1][last1];
+                else
+                    costs[i][j]+=last2<0?0:costs[i-1][last2];
+                if(min1<0||costs[i][j]<costs[i][min1]){
+                    min2=min1;
+                    min1=j;
+                }
+                else if(min2<0||costs[i][j]<costs[i][min2])
+                    min2=j;
             }
-            min=Integer.MAX_VALUE;
-            for(int j=costs[0].length-1;j>=0;j--){
-                minarr[j]=Math.min(min,minarr[j]);
-                min=Math.min(min,dp[i-1][j]);
-            }
-            for(int j=0;j<costs[0].length;j++)
-                dp[i][j]=minarr[j]+costs[i][j];
         }
-        int res=Integer.MAX_VALUE;
-        for(int i=0;i<costs[0].length;i++)
-            res=Math.min(res,dp[costs.length-1][i]);
-        return res;
+        return costs[n-1][min1];
     }
 }
