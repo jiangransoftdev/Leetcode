0738458014db287566f87b3a2392d6f436524a06
jiangran910126 <jiangran0126@gmail.com>
2016-09-30 17:36:52 -0500

    [317. Shortest Distance from All Buildings][Accepted]

--- 317-Shortest-Distance-from-All-Buildings/solution.java
+++ 317-Shortest-Distance-from-All-Buildings/solution.java
@@ -1,27 +1,29 @@
 public class Solution {
     public int shortestDistance(int[][] grid) {
-        if(grid.length==0) return 0;
-        int[] shift={0,1,0,-1,0};
-        int row=grid.length,col=grid[0].length,buildingNum=0;
+        int row=grid.length;
+        if(row==0) return 0;
+        int col=grid[0].length;
+        int[] d={0,-1,0,1,0};
+        int buildNums=0;
         int[][] reach=new int[row][col],distance=new int[row][col];
         for(int i=0;i<row;i++){
             for(int j=0;j<col;j++){
                 if(grid[i][j]==1){
-                    buildingNum++;
+                    buildNums++;
                     boolean[][] visited=new boolean[row][col];
-                    int level=1;
                     Queue<int[]> q=new LinkedList<>();
+                    int level=1;
                     q.offer(new int[]{i,j});
                     while(!q.isEmpty()){
                         int size=q.size();
                         for(int p=0;p<size;p++){
                             int[] cur=q.poll();
                             for(int k=0;k<4;k++){
-                                int newrow=cur[0]+shift[k];
-                                int newcol=cur[1]+shift[k+1];
+                                int newrow=cur[0]+d[k];
+                                int newcol=cur[1]+d[k+1];
                                 if(newrow>=0&&newrow<row&&newcol>=0&&newcol<col&&grid[newrow][newcol]==0&&!visited[newrow][newcol]){
-                                    distance[newrow][newcol]+=level;
                                     reach[newrow][newcol]++;
+                                    distance[newrow][newcol]+=level;
                                     visited[newrow][newcol]=true;
                                     q.offer(new int[]{newrow,newcol});
                                 }
@@ -34,10 +36,8 @@ public class Solution {
         }
         int shortest=Integer.MAX_VALUE;
         for(int i=0;i<row;i++){
-            for(int j=0;j<col;j++){
-                if(grid[i][j]==0&&reach[i][j]==buildingNum)
-                    shortest=Math.min(shortest,distance[i][j]);
-            }
+            for(int j=0;j<col;j++)
+                if(buildNums==reach[i][j]) shortest=Math.min(shortest,distance[i][j]);
         }
         return shortest==Integer.MAX_VALUE?-1:shortest;
     }
