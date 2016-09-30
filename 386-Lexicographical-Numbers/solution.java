e1520621f352ea1017cb531def8f4ea5a6447470
jiangran910126 <jiangran0126@gmail.com>
2016-09-30 02:32:57 -0500

    [386. Lexicographical Numbers][Accepted]

--- 386-Lexicographical-Numbers/solution.java
+++ 386-Lexicographical-Numbers/solution.java
@@ -1,22 +1,16 @@
 public class Solution {
     public List<Integer> lexicalOrder(int n) {
-        List<Integer> res = new ArrayList<>();
-        for(int i=1;i<10;++i){
-          dfs(i, n, res); 
-        }
+        List<Integer> res=new ArrayList<>();
+        for(int i=1;i<10;i++)
+            dfs(i,n,res);
         return res;
     }
-    
-    public void dfs(int cur, int n, List<Integer> res){
-        if(cur>n)
-            return;
-        else{
-            res.add(cur);
-            for(int i=0;i<10;++i){
-                if(10*cur+i>n)
-                    return;
-                dfs(10*cur+i, n, res);
-            }
+    public void dfs(int cur,int n,List<Integer> res){
+        if(cur>n) return;
+        res.add(cur);
+        for(int i=0;i<10;i++){
+            if(cur*10+i>n) break;
+            dfs(cur*10+i,n,res);
         }
     }
 }
