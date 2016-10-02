b050e4f2d98b5f195db68d66046a65848b3ac3d9
jiangran910126 <jiangran0126@gmail.com>
2016-10-02 03:50:29 -0500

    [126. Word Ladder II][Accepted]

--- 126-Word-Ladder-II/solution.java
+++ 126-Word-Ladder-II/solution.java
@@ -1,11 +1,11 @@
 class Node{
     String word;
     int dist;
-    LinkedList<Node> prev;
-    public Node(String s,int dist){
+    List<Node> prev;
+    public Node(int dist,String s){
         this.word=s;
         this.dist=dist;
-        prev=new LinkedList<>();
+        prev=new ArrayList<>();
     }
 }
 public class Solution {
@@ -13,57 +13,55 @@ public class Solution {
         List<List<String>> res=new ArrayList<>();
         Map<String,Node> map=new HashMap<>();
         Queue<String> q=new LinkedList<>();
-        Node startnode=new Node(beginWord,1);
-        map.put(beginWord,startnode);
+        wordList.add(endWord);
         q.offer(beginWord);
+        Node root=new Node(1,beginWord);
+        map.put(beginWord,root);
         while(!q.isEmpty()){
+            int size=q.size();
             String str=q.poll();
             if(str.equals(endWord)){
-                getPath(map.get(str),new ArrayList<>(),res);
+                getPath(map.get(endWord),map,new ArrayList<String>(),res);
                 return res;
             }
-            char[] schar=str.toCharArray();
-            for(int i=0;i<schar.length;i++){
-                char old=schar[i];
+            char[] newchar=str.toCharArray();
+            for(int i=0;i<newchar.length;i++){
+                char old=newchar[i];
                 for(char c='a';c<='z';c++){
-                    schar[i]=c;
-                    String newstr=new String(schar);
+                    newchar[i]=c;
+                    String newstr=new String(newchar);
                     if(wordList.contains(newstr)){
                         if(!map.containsKey(newstr)){
-                        Node node=map.get(str);
-                        Node newnode=new Node(newstr,node.dist+1);
-                        newnode.prev.add(node);
-                        map.put(newstr,newnode);
-                        q.offer(newstr);
-                    }
-                    else{
-                        Node node=map.get(str);
-                        Node newnode=map.get(newstr);
-                        if(newnode.dist==node.dist+1)
+                            Node node=map.get(str);
+                            Node newnode=new Node(node.dist+1,newstr);
                             newnode.prev.add(node);
+                            map.put(newstr,newnode);
+                            q.offer(newstr);
+                        }
+                        else{
+                            Node node=map.get(str);
+                            Node newnode=map.get(newstr);
+                            if(node.dist+1==newnode.dist){
+                                newnode.prev.add(node);
+                            }
+                        }
                     }
-                    }
-                    
                 }
-                schar[i]=old;
+                newchar[i]=old;
             }
         }
         return res;
     }
-    public void getPath(Node end,List<String> solu,List<List<String>> res){
-        if(end==null){
-            res.add(new ArrayList(solu));
+    public void getPath(Node node,Map<String,Node> map,List<String> solu,List<List<String>> res){
+        if(node==null){
+            res.add(solu);
             return;
         }
-        solu.add(0,end.word);
-        LinkedList<Node> list=end.prev;
-        if(!list.isEmpty()){
-            for(Node node:list){
-                getPath(node,new ArrayList(solu),res);
-            }
+        solu.add(0,node.word);
+        if(!node.prev.isEmpty()){
+            for(Node prev:node.prev)
+                getPath(prev,map,new ArrayList(solu),res);
         }
-        else
-            getPath(null,new ArrayList(solu),res);
-        
+        else getPath(null,map,new ArrayList(solu),res);
     }
 }
