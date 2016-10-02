9c52e77ab97f198695f2e6b9159c21c80f662fa9
jiangran910126 <jiangran0126@gmail.com>
2016-10-01 21:42:31 -0500

    [312. Burst Balloons][Accepted]

--- 312-Burst-Balloons/solution.java
+++ 312-Burst-Balloons/solution.java
@@ -3,12 +3,12 @@ public class Solution {
         if(nums.length==0) return 0;
         int[][] dp=new int[nums.length][nums.length];
         for(int k=1;k<=nums.length;k++){
-            for(int i=0;i+k<=nums.length;i++){
-                int start=i,end=i+k-1;
-                for(int j=start;j<=end;j++){
-                    int count=nums[j]*(start-1==-1?1:nums[start-1])*(end+1==nums.length?1:nums[end+1]);
-                    count+=(start==j)?0:dp[start][j-1];
-                    count+=(end==j)?0:dp[j+1][end];
+            for(int start=0;start+k<=nums.length;start++){
+                int end=start+k-1;
+                for(int i=start;i<=end;i++){
+                    int count=nums[i]*(start-1==-1?1:nums[start-1])*(end+1==nums.length?1:nums[end+1]);
+                    count+=(i!=start)?dp[start][i-1]:0;
+                    count+=(i!=end)?dp[i+1][end]:0;
                     dp[start][end]=Math.max(dp[start][end],count);
                 }
             }
