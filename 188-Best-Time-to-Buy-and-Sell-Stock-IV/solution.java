d320643580c191bb1bb354eaf18383b6e6b85b29
jiangran910126 <jiangran0126@gmail.com>
2016-10-01 20:55:15 -0500

    [188. Best Time to Buy and Sell Stock IV][Accepted]

--- 188-Best-Time-to-Buy-and-Sell-Stock-IV/solution.java
+++ 188-Best-Time-to-Buy-and-Sell-Stock-IV/solution.java
@@ -1,22 +1,23 @@
 public class Solution {
-    public int maxProfit(int k, int[] prices) {
+        public int maxProfit(int k, int[] prices) {
+            if(prices.length==0) return 0;
         if(k>prices.length/2){
-            int total=0;
+            int profit=0;
             for(int i=1;i<prices.length;i++){
                 if(prices[i]-prices[i-1]>0)
-                    total+=prices[i]-prices[i-1];
+                    profit+=prices[i]-prices[i-1];
             }
-            return total;
+            return profit;
         }
-        int[][] buy=new int[prices.length+1][k+1];
-        int[][] sell=new int[prices.length+1][k+1];
-        for(int i=1;i<=k;i++) buy[0][i]=Integer.MIN_VALUE;
-        for(int j=1;j<=k;j++){
-            for(int i=1;i<=prices.length;i++){
-                buy[i][j]=Math.max(buy[i-1][j],sell[i-1][j-1]-prices[i-1]);
-                sell[i][j]=Math.max(sell[i-1][j],buy[i-1][j]+prices[i-1]);
+            int[][] buy=new int[prices.length+1][k+1],sell=new int[prices.length+1][k+1];
+            for(int i=1;i<=k;i++)
+                buy[0][i]=Integer.MIN_VALUE;
+            for(int j=1;j<=k;j++){
+                for(int i=1;i<=prices.length;i++){
+                    buy[i][j]=Math.max(buy[i-1][j],sell[i-1][j-1]-prices[i-1]);
+                    sell[i][j]=Math.max(sell[i-1][j],buy[i-1][j]+prices[i-1]);
+                }
             }
+            return sell[prices.length][k];
         }
-        return sell[prices.length][k];
-    }
 }
