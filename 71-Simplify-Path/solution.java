bc56b7e0b23bf194ef76e2883ce30c600a7c75f4
jiangran910126 <jiangran0126@gmail.com>
2016-10-02 20:41:16 -0500

    [71. Simplify Path][Accepted]

--- 71-Simplify-Path/solution.java
+++ 71-Simplify-Path/solution.java
@@ -1,45 +1,36 @@
 public class Solution {
     public String simplifyPath(String path) {
-        if(path.length()==0) return "";
-        StringBuilder sb=new StringBuilder();
-        Stack<String> s=new Stack<>();
-        s.push("/");
+        Stack<String> stack=new Stack<>();
         int i=0;
+        String str="";
+        path=path+"/";
         while(i<path.length()){
-            if(path.charAt(i)=='/'&&s.peek().equals("/"))
-                i++;
-            else{
-                while(i<path.length()&&path.charAt(i)!='/'){
-                    sb.append(path.charAt(i));
+            if(path.charAt(i)!='/'){
+                str=path.charAt(i)+"";
+                while(i+1<path.length()&&path.charAt(i+1)!='/'){
+                    str=str+path.charAt(i+1);
                     i++;
                 }
-                if(sb.toString().equals(".")){
-                    sb.delete(0,sb.length());
-                    continue;
-                }
-                else if(sb.toString().equals("..")){
-                    if(s.size()==1){
-                        sb.delete(0,sb.length());
-                        i++;
-                        continue;
-                    }
-                    else{
-                        s.pop();
-                        s.pop();
-                        sb.delete(0,sb.length());
-                    }
-                }
-                else{
-                    s.push(sb.toString());
-                    s.push("/");
-                    sb.delete(0,sb.length());
-                }
-                i++;
             }
+            else if(str.equals(".")||(i+1<path.length()&&path.charAt(i+1)=='/')){
+                i++;
+                continue;
+            }
+            else if(str.equals("..")){
+                if(!stack.isEmpty()) stack.pop();
+            }
+            else {
+                    stack.push(str);
+                    str="";
+            }
+            i++;
         }
-        if(s.size()>1&&s.peek().equals("/")) s.pop();
-        for(String str:s)
-            sb.append(str);
+        StringBuilder sb=new StringBuilder();
+        for(String s:stack){
+            if(!s.equals(""))
+                sb.append("/"+s);
+        }
+        if(sb.length()==0) return "/";
         return sb.toString();
     }
 }
