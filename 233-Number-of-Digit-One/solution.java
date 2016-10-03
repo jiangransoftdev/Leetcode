d37bb275946293f16d9337450169105f7e0ca8b6
jiangran910126 <jiangran0126@gmail.com>
2016-10-03 16:10:22 -0500

    [233. Number of Digit One][Accepted]

--- 233-Number-of-Digit-One/solution.java
+++ 233-Number-of-Digit-One/solution.java
@@ -1,15 +1,15 @@
 public class Solution {
     public int countDigitOne(int n) {
-        if(n==0) return 0;
-        int res=0,x=n,factor=1;;
+        int q=n,x=1,ans=0;
         do{
-            int digit=x%10;
-            x/=10;
-            res+=x*factor;
-            if(digit==1) res+=n%factor+1;
-            if(digit>1)  res+=factor;
-            factor*=10;
-        }while(x>0);
-        return res;
+            int dig=q%10;
+            q/=10;
+            ans+=q*x;
+            if(dig==1) ans+=n%x+1;
+            if(dig>1) ans+=x;
+            x*=10;
+        }
+        while(q>0);
+        return ans;
     }
 }
