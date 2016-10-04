6545d4f8ee80f9f41140a49562720305d1fa0857
jiangran910126 <jiangran0126@gmail.com>
2016-10-04 02:11:04 -0500

    [68. Text Justification][Accepted]

--- 68-Text-Justification/solution.java
+++ 68-Text-Justification/solution.java
@@ -1,37 +1,33 @@
 public class Solution {
     public List<String> fullJustify(String[] words, int maxWidth) {
         List<String> res=new ArrayList<>();
-        int n=words.length;
-        int i=0;
+        int i=0,n=words.length;
         while(i<n){
-            int len=words[i].length();
-            int j=i+1;
-            while(j<n&&len+words[j].length()+1<=maxWidth){
-                len+=1+words[j].length();
+            int j=i+1,len=words[i].length();
+            while(j<n&&len+1+words[j].length()<=maxWidth){
+                len+=words[j].length()+1;
                 j++;
+                
             }
             String line=words[i];
             if(j==n){
-                for(int k=i+1;k<n;k++){
+                for(int k=i+1;k<j;k++)
                     line+=" "+words[k];
-                }
                 while(line.length()<maxWidth)
                     line+=" ";
             }
             else{
-                int extraWhite=maxWidth-len;
                 int whiteNum=j-i-1;
-                if(whiteNum==0){
+                int extraNum=maxWidth-len;
+                if(whiteNum==0)
                     while(line.length()<maxWidth)
                         line+=" ";
-                }
                 else{
                     for(int k=i+1;k<j;k++){
                         line+=" ";
-                        for(int p=0;p<extraWhite/whiteNum;p++)
+                        for(int m=0;m<extraNum/whiteNum;m++)
                             line+=" ";
-                        if(k-i<=extraWhite%whiteNum)
-                            line+=" ";
+                        if(k-i<=extraNum%whiteNum) line+=" ";
                         line+=words[k];
                     }
                 }
