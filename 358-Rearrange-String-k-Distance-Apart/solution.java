2fddde270362a5bef85af2b6198855967127ed63
jiangran910126 <jiangran0126@gmail.com>
2016-10-04 14:50:41 -0500

    [358. Rearrange String k Distance Apart][Accepted]

--- 358-Rearrange-String-k-Distance-Apart/solution.java
+++ 358-Rearrange-String-k-Distance-Apart/solution.java
@@ -1,28 +1,44 @@
 public class Solution {
     public String rearrangeString(String str, int k) {
-        int[] valid=new int[26],count=new int[26];
-        int n=str.length();
-        for(int i=0;i<n;i++)
-            count[str.charAt(i)-'a']++;
-        StringBuilder sb=new StringBuilder();
-        for(int i=0;i<n;i++){
-            int candidate=findValidMax(count,i,valid);
-            if(candidate==-1) return "";
-            count[candidate]--;
-            valid[candidate]=i+k;
-            sb.append((char)('a'+candidate));
+       StringBuilder rearranged = new StringBuilder();
+        //count frequency of each char
+        Map<Character, Integer> map = new HashMap<>();
+        for (char c : str.toCharArray()) {
+            if (!map.containsKey(c)) {
+                map.put(c, 0);
+            }
+            map.put(c, map.get(c) + 1);
         }
-        return sb.toString();
-    }
-    public int findValidMax(int[] count,int index,int[] valid){
-        int candidate=-1;
-        int max=Integer.MIN_VALUE;
-        for(int i=0;i<count.length;i++){
-            if(count[i]>0&&count[i]>max&&index>=valid[i]){
-                max=count[i];
-                candidate=i;
+        
+        //construct a max heap using self-defined comparator, which holds all Map entries, Java is quite verbose
+        Queue<Map.Entry<Character, Integer>> maxHeap = new PriorityQueue<>(new Comparator<Map.Entry<Character, Integer>>() {
+            public int compare(Map.Entry<Character, Integer> entry1, Map.Entry<Character, Integer> entry2) {
+                return entry2.getValue() - entry1.getValue();
+            }
+        });
+        
+        Queue<Map.Entry<Character, Integer>> waitQueue = new LinkedList<>();
+        maxHeap.addAll(map.entrySet());
+        
+        while (!maxHeap.isEmpty()) {
+            
+            Map.Entry<Character, Integer> current = maxHeap.poll();
+            rearranged.append(current.getKey());
+            current.setValue(current.getValue() - 1);
+            waitQueue.offer(current);
+            
+            if (waitQueue.size() < k) { // intial k-1 chars, waitQueue not full yet
+                continue;
+            }
+            // release from waitQueue if char is already k apart
+            Map.Entry<Character, Integer> front = waitQueue.poll();
+            //note that char with 0 count still needs to be placed in waitQueue as a place holder
+            if (front.getValue() > 0) {
+                maxHeap.offer(front);
             }
         }
-        return candidate;
+        
+        return rearranged.length() == str.length() ? rearranged.toString() : "";
     }
+    
 }
