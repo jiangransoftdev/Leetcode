e671fe749c541b9aba7bc6d5bc766699472ad574
jiangran910126 <jiangran0126@gmail.com>
2016-10-04 15:08:27 -0500

    [313. Super Ugly Number][Accepted]

--- 313-Super-Ugly-Number/solution.java
+++ 313-Super-Ugly-Number/solution.java
@@ -1,18 +1,23 @@
 public class Solution {
     public int nthSuperUglyNumber(int n, int[] primes) {
-        int[] res=new int[n];
-        int[] val=new int[primes.length];
-        int[] idx=new int[primes.length];
-        Arrays.fill(val,1);
-        int next=1;
-        for(int i=0;i<n;i++){
-            res[i]=next;
-            next=Integer.MAX_VALUE;
-            for(int j=0;j<primes.length;j++){
-                if(val[j]==res[i]) val[j]=res[idx[j]++]*primes[j];
-                next=Math.min(next,val[j]);
+        int k=primes.length;
+        int[] index=new int[k],ans=new int[n];
+        ans[0]=1;
+        for(int i=1;i<n;i++){
+            int min=Integer.MAX_VALUE;
+            int pos=-1;
+            for(int j=0;j<k;j++){
+                if(ans[index[j]]*primes[j]<min){
+                    min=ans[index[j]]*primes[j];
+                    pos=j;
+                }
+                else if(ans[index[j]]*primes[j]==min){
+                    index[j]++;
+                }
             }
+            ans[i]=min;
+            index[pos]++;
         }
-        return res[n-1];
+        return ans[n-1];
     }
 }
