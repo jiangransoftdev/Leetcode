38078045315c991246fbb207680ad877238986a5
jiangran910126 <jiangran0126@gmail.com>
2016-10-04 18:05:24 -0500

    [407. Trapping Rain Water II][Accepted]

--- 407-Trapping-Rain-Water-II/solution.java
+++ 407-Trapping-Rain-Water-II/solution.java
@@ -1,72 +1,50 @@
-// class Cell{
-//     int row;
-//     int col;
-//     int height;
-//     public Cell(int row,int col,int height){
-//         this.row=row;
-//         this.col=col;
-//         this.height=height;
-//     }
-// }
-public class Solution {
-    public class Cell {
-        int row;
-        int col;
-        int height;
-        public Cell(int row, int col, int height) {
-            this.row = row;
-            this.col = col;
-            this.height = height;
-        }
+class Cell{
+    int row;
+    int col;
+    int height;
+    public Cell(int row,int col,int height){
+        this.row=row;
+        this.col=col;
+        this.height=height;
     }
-
-    public int trapRainWater(int[][] heights) {
-        if (heights == null || heights.length == 0 || heights[0].length == 0)
-            return 0;
-
-        PriorityQueue<Cell> queue = new PriorityQueue<>(1, new Comparator<Cell>(){
-            public int compare(Cell a, Cell b) {
-                return a.height - b.height;
+}
+public class Solution {
+    int[] d={0,1,0,-1,0};
+    public int trapRainWater(int[][] heightMap) {
+        int m=heightMap.length;
+        if(m==0) return 0;
+        int n=heightMap[0].length;
+        int res=0;
+        PriorityQueue<Cell> pq=new PriorityQueue<>(new Comparator<Cell>(){
+            public int compare(Cell a,Cell b){
+                return a.height-b.height;
             }
         });
-        
-        int m = heights.length;
-        int n = heights[0].length;
-        boolean[][] visited = new boolean[m][n];
-
-        // Initially, add all the Cells which are on borders to the queue.
-        for (int i = 0; i < m; i++) {
-            visited[i][0] = true;
-            visited[i][n - 1] = true;
-            queue.offer(new Cell(i, 0, heights[i][0]));
-            queue.offer(new Cell(i, n - 1, heights[i][n - 1]));
+        boolean[][] visit=new boolean[m][n];
+        for(int i=0;i<m;i++){
+            visit[i][0]=true;
+            visit[i][n-1]=true;
+            pq.offer(new Cell(i,0,heightMap[i][0]));
+            pq.offer(new Cell(i,n-1,heightMap[i][n-1]));
         }
-
-        for (int i = 0; i < n; i++) {
-            visited[0][i] = true;
-            visited[m - 1][i] = true;
-            queue.offer(new Cell(0, i, heights[0][i]));
-            queue.offer(new Cell(m - 1, i, heights[m - 1][i]));
+        for(int i=0;i<n;i++){
+            visit[0][i]=true;
+            visit[m-1][i]=true;
+            pq.offer(new Cell(0,i,heightMap[0][i]));
+            pq.offer(new Cell(m-1,i,heightMap[m-1][i]));
         }
-
-        // from the borders, pick the shortest cell visited and check its neighbors:
-        // if the neighbor is shorter, collect the water it can trap and update its height as its height plus the water trapped
-       // add all its neighbors to the queue.
-        int[][] dirs = new int[][]{{-1, 0}, {1, 0}, {0, -1}, {0, 1}};
-        int res = 0;
-        while (!queue.isEmpty()) {
-            Cell cell = queue.poll();
-            for (int[] dir : dirs) {
-                int row = cell.row + dir[0];
-                int col = cell.col + dir[1];
-                if (row >= 0 && row < m && col >= 0 && col < n && !visited[row][col]) {
-                    visited[row][col] = true;
-                    res += Math.max(0, cell.height - heights[row][col]);
-                    queue.offer(new Cell(row, col, Math.max(heights[row][col], cell.height)));
+        while(!pq.isEmpty()){
+            Cell cur=pq.poll();
+            for(int i=0;i<4;i++){
+                int row=cur.row+d[i];
+                int col=cur.col+d[i+1];
+                if(row>=0&&row<m&&col>=0&&col<n&&!visit[row][col]){
+                    pq.offer(new Cell(row,col,Math.max(cur.height,heightMap[row][col])));
+                    res+=Math.max(0,cur.height-heightMap[row][col]);
+                    visit[row][col]=true;
                 }
             }
         }
-        
         return res;
     }
 }
